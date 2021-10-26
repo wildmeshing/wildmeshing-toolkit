efc5d46f55de60219adcb5ad6736a6745400fbdc
Daniele Panozzo <daniele.panozzo@gmail.com>
2021-10-26 19:13:14 -0400

    hopefully final

--- src/Mesh.h
+++ src/Mesh.h
@@ -14,7 +14,7 @@ namespace wmtk {
         class Tuple {
         public:
             size_t vid;
-            size_t eid;//todo: need edge list and face list?
+            size_t eid;
             size_t fid;
             size_t tid;
 
@@ -22,27 +22,30 @@ namespace wmtk {
 
             void flip_edge(const TetMesh &m);//along face
 
-            void flip_Face(const TetMesh &m);//along tet
+            void flip_face(const TetMesh &m);//along tet
 
             void flip_tetrahedron(const TetMesh &m);//todo: along face?
+
+            size_t get_vertex_attribute_id(const TetMesh &m);
+            size_t get_edge_attribute_id(const TetMesh &m);
+            size_t get_face_attribute_id(const TetMesh &m);
+            size_t get_tetrahedron_attribute_id(const TetMesh &m);
         };
 
         class VertexConnectivity {
         public:
             std::vector<size_t> m_conn_tets;
             bool m_is_removed = false;
-        };
 
-        class EdgeConnectivity {
-        public:
-            std::array<size_t, 2> m_indices;
-            bool m_is_removed = false;
-        };
+            inline size_t &operator[](const size_t index) {
+                assert(index >= 0 && index < m_conn_tets.size());
+                return m_conn_tets[index];
+            }
 
-        class FaceConnectivity {
-        public:
-            std::array<size_t, 3> m_indices;//todo: how to set the order for flip_edge()?
-            bool m_is_removed = false;
+            inline size_t operator[](const size_t index) const {
+                assert(index >= 0 && index < m_conn_tets.size());
+                return m_conn_tets[index];
+            }
         };
 
         class TetrahedronConnectivity {
@@ -61,46 +64,61 @@ namespace wmtk {
             }
         };
 
-        virtual ~TetMesh(){}
-        void split(const Tuple &t);//todo: on one edge?
-        void collapse(const Tuple &t);
-        void swapping(const Tuple &t);
-        void smoothing(const Tuple &t);
+        virtual ~TetMesh(){};
 
+        void split_edge(const Tuple &t)
+        {
+            if (!split_before(t))
+                return;
+            
+            // backup of everything
+
+            // update connectivity
+            // possibly call the resize_attributes
+
+            if (!split_after(t))
+            {
+                // undo changes
+                return;
+            }
+                
+            // call invariants on all entities
+            if (false) // if any invariant fails
+            {
+                // undo changes
+                return;
+            }
+
+        };
+
+        void collapse_edge(const Tuple &t);
+        void swapping_edge(const Tuple &t, int type);
 
     protected:
         // Stores the connectivity of the mesh
         std::vector<VertexConnectivity> m_vertex_connectivity;
-        std::vector<EdgeConnectivity> m_edge_connectivity;
-        std::vector<FaceConnectivity> m_face_connectivity;
         std::vector<TetrahedronConnectivity> m_tetrahedron_connectivity;
 
         //// Split the edge in the tuple
         // Checks if the split should be performed or not (user controlled)
-        virtual bool split_precondition(const Tuple &t);//check edge condition
+        virtual bool split_before(const Tuple &t);//check edge condition
         // This function computes the attributes for the added simplices
         // if it returns false then the operation is undone
-        virtual bool split_postcondition(const Tuple &t);//check tet condition
+        virtual bool split_after(const Tuple &t);//check tet condition
 
         //// Collapse the edge in the tuple
         // Checks if the collapse should be performed or not (user controlled)
-        virtual bool collapse_precondition(const Tuple &t);
+        virtual bool collapse_before(const Tuple &t);
         // If it returns false then the operation is undone (the tuple indexes a vertex and tet that survived)
-        virtual bool collapse_postcondition(const Tuple &t);
+        virtual bool collapse_after(const Tuple &t);
         //todo: quality, inversion, envelope: change v1 pos before this, only need to change partial attributes
 
         //// Swap the edge in the tuple
         // Checks if the swapping should be performed or not (user controlled)
-        virtual bool swapping_precondition(const Tuple &t);
+        virtual bool swapping_before(const Tuple &t);
         // If it returns false then the operation is undone (the tuple indexes TODO)
-        virtual bool swapping_postcondition(const Tuple &t);
+        virtual bool swapping_after(const Tuple &t);
         //quality, inversion
-
-        //// Smooth in the tuple
-        // Checks if the smoothing should be performed or not (user controlled)
-        virtual bool smoothing_precondition(const Tuple &t);
-        // If it returns false then the operation is undone (the tuple indexes TODO)
-        virtual bool smoothing_postcondition(const Tuple &t);
 
         // Invariants that are called on all the new or modified elements after an operation is performed
         virtual bool VertexInvariant(const Tuple &t);
@@ -109,6 +127,7 @@ namespace wmtk {
         virtual bool TetrahedronInvariant(const Tuple &t);
 
         virtual void resize_attributes(size_t v, size_t e, size_t t, size_t tt);
+
         void compact(); // cleans up the deleted vertices or tetrahedra, and fixes the corresponding indices
     };
 
@@ -169,5 +188,6 @@ namespace wmtk {
             m_tetrahedron_attribute.resize(tt);
         }
 
+        void smoothing(const Tuple &t);
     };
 }
