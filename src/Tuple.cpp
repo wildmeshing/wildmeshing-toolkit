62e4059ace809b79cfe49ca72714d4658f7fddf7
Zhongshi <zj495@nyu.edu>
2021-12-16 22:56:33 +0800

    timestamp fix

--- src/Tuple.cpp
+++ src/Tuple.cpp
@@ -13,7 +13,7 @@ TetMesh::Tuple TetMesh::Tuple::init_from_edge(const TetMesh& m, int tid, int loc
 
     int vid = m.m_tet_connectivity[tid][m_local_edges[local_eid][0]];
     int fid = m_map_edge2face[local_eid];
-    return Tuple(vid, local_eid, fid, tid);
+    return Tuple(vid, local_eid, fid, tid, m.m_timestamp);
 }
 
 TetMesh::Tuple TetMesh::Tuple::init_from_face(const TetMesh& m, int tid, int local_fid)
@@ -33,7 +33,7 @@ TetMesh::Tuple TetMesh::Tuple::init_from_face(const TetMesh& m, int tid, int loc
         }
     }
     int fid = m_map_edge2face[local_fid];
-    return Tuple(vid, eid, local_fid, tid);
+    return Tuple(vid, eid, local_fid, tid, m.m_timestamp);
 }
 
 TetMesh::Tuple TetMesh::Tuple::init_from_vertex(const TetMesh& m, int vid)
@@ -44,7 +44,7 @@ TetMesh::Tuple TetMesh::Tuple::init_from_vertex(const TetMesh& m, int vid)
     int j = m.m_tet_connectivity[tid].find(vid);
     int eid = m_map_vertex2edge[j];
     int fid = m_map_edge2face[eid];
-    return Tuple(vid, eid, fid, tid);
+    return Tuple(vid, eid, fid, tid, m.m_timestamp);
 }
 
 TetMesh::Tuple TetMesh::Tuple::init_from_tet(const TetMesh& m, int tid)
@@ -54,7 +54,7 @@ TetMesh::Tuple TetMesh::Tuple::init_from_tet(const TetMesh& m, int tid)
     int vid = m.m_tet_connectivity[tid][0];
     int eid = m_map_vertex2edge[0];
     int fid = m_map_edge2face[eid];
-    return Tuple(vid, eid, fid, tid);
+    return Tuple(vid, eid, fid, tid, m.m_timestamp);
 }
 
 bool TetMesh::Tuple::is_valid(const TetMesh& m) const
@@ -66,7 +66,7 @@ bool TetMesh::Tuple::is_valid(const TetMesh& m) const
 
 void TetMesh::Tuple::update_version_number(const TetMesh& m)
 {
-    assert(m_timestamp < m.m_tet_connectivity[m_tid].timestamp);
+    assert(m_timestamp <= m.m_tet_connectivity[m_tid].timestamp);
     m_timestamp = m.m_tet_connectivity[m_tid].timestamp;
 }
 
@@ -77,7 +77,7 @@ int TetMesh::Tuple::get_version_number()
 
 bool TetMesh::Tuple::is_version_number_valid(const TetMesh& m) const
 {
-    if (m_timestamp != m.m_tet_connectivity[m_tid].timestamp) return false;
+    if (m_timestamp < m.m_tet_connectivity[m_tid].timestamp) return false;
     return true;
 }
 
