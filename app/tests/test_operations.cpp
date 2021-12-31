0c2b4e6851fc2b82a7c3e79a586c0f4319073484
Zhongshi <zj495@nyu.edu>
2021-12-31 08:07:10 -0500

    uninitialized varialbe in Parameters

--- app/tests/test_operations.cpp
+++ app/tests/test_operations.cpp
@@ -30,7 +30,7 @@ TEST_CASE("edge_splitting", "[test_operation]")
 
     tetwild.split_all_edges();
     REQUIRE(tetwild.check_mesh_connectivity_validity());
-    REQUIRE(tetwild.m_vertex_attribute.size() == 1380);
+    REQUIRE(tetwild.m_vertex_attribute.size() == 1374);
 }
 
 
@@ -56,9 +56,9 @@ TEST_CASE("edge_collapsing", "[test_operation]")
 
     tetwild.split_all_edges();
     REQUIRE(tetwild.check_mesh_connectivity_validity());
-    CHECK(tetwild.m_vertex_attribute.size() == 1380);
+    CHECK(tetwild.m_vertex_attribute.size() == 1374);
 
     tetwild.collapse_all_edges();
     REQUIRE(tetwild.check_mesh_connectivity_validity());
-    CHECK(tetwild.get_vertices().size() == 1202);
+    CHECK(tetwild.get_vertices().size() == 1210);
 }
