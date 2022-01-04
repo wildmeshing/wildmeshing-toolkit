4d092ad129619096940dddda206ac9c3e0523075
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2022-01-04 15:43:32 -0800

    Cleanup CMake code.

--- cmake/recipes/fenvelope.cmake
+++ cmake/recipes/fenvelope.cmake
@@ -4,6 +4,14 @@ endif()
 
 message(STATUS "Third-party: creating target 'FastEnvelope'")
 
+# Options for envelope module
+option(FAST_ENVELOPE_WITH_UNIT_TESTS             "Unit test project"                   OFF)
+option(FAST_ENVELOPE_ENABLE_TBB                  "Enable TBB"                          OFF)
+option(FAST_ENVELOPE_WITH_GEOGRAM_PREDICATES     "Use whole Geogram for predicates"    OFF)
+option(FAST_ENVELOPE_WITH_GEOGRAM_PSM_PREDICATES "Use Geogram predicates only"         OFF)
+option(FAST_ENVELOPE_WITH_GMP                    "Use gmp to have rational predicates" OFF)
+option(FAST_ENVELOPE_WITH_TIMER                  "Use timer in the library for debug"  OFF)
+
 include(FetchContent)
 FetchContent_Declare(
     fenvelope
