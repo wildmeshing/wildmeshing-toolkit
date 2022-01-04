43fd2c781e0b6a734f5f97836d61b1cf7cdf66e2
Teseo Schneider <teseo@bluewin.ch>
2022-01-04 15:13:39 -0800

    better find gmp

--- cmake/recipes/gmp.cmake
+++ cmake/recipes/gmp.cmake
@@ -1,14 +1,18 @@
 # Try to find the GNU Multiple Precision Arithmetic Library (GMP)
 # See http://gmplib.org/
 
-if(TARGET gmp)
+if(TARGET gmp::gmp)
     return()
 endif()
 
+# Download precompiled .dll on Windows
+include(gmp_mpfr)
 
-find_package(MPFR 2.3.0)
-find_package(GMP)
+message(STATUS "Third-party: creating target 'gmp::gmp'")
 
-add_library(gmp INTERFACE)
-target_include_directories(gmp INTERFACE ${GMP_INCLUDES} ${MPFR_INCLUDES})
-target_link_libraries(gmp INTERFACE ${GMP_LIBRARIES} ${MPFR_LIBRARIES})
+# Find_package will look for our downloaded lib on Windows, and system-wide on Linux/macOS
+find_package(GMP REQUIRED)
+
+if(NOT TARGET gmp::gmp)
+    message(FATAL_ERROR "Creation of target 'gmp::gmp' failed")
+endif()
