862181608695d6d2650de79972c83a1e6a0308b9
Teseo Schneider <teseo@bluewin.ch>
2022-01-06 18:00:32 -0800

    moving stuff to headers

--- src/wmtk/TriMesh.h
+++ src/wmtk/TriMesh.h
@@ -83,54 +83,13 @@ public:
          */
         std::optional<Tuple> switch_face(const TriMesh& m) const;
 
-        bool is_valid(const TriMesh& m) const
-        {
-            if (m.m_vertex_connectivity[m_vid].m_is_removed ||
-                m.m_tri_connectivity[m_fid].m_is_removed)
-                return false;
-
-            // Condition 3: tuple m_hash check
-            if (m_hash != m.m_tri_connectivity[m_fid].hash) return false;
-
-#ifndef NDEBUG
-            //  Condition 0: Elements exist
-            assert(m_vid < m.m_vertex_connectivity.size());
-            assert(m_eid <= 2);
-            assert(m_fid <= m.m_tri_connectivity.size());
-
-            // Condition 1: tid and vid are consistent
-            const int lvid = m.m_tri_connectivity[m_fid].find(m_vid);
-            assert(lvid == 0 || lvid == 1 || lvid == 2);
-
-            // Condition 2: eid is valid
-            const int v0 = m.m_tri_connectivity[m_fid][0];
-            const int v1 = m.m_tri_connectivity[m_fid][1];
-            const int v2 = m.m_tri_connectivity[m_fid][2];
-            switch (m_eid) {
-            case 0: assert(m_vid == v1 || m_vid == v2); break;
-            case 1: assert(m_vid == v0 || m_vid == v2); break;
-            case 2: assert(m_vid == v0 || m_vid == v1); break;
-            default: assert(false);
-            }
-#endif
-
-            return true;
-        }
+        bool is_valid(const TriMesh& m) const;
 
         /**
          * Positively oriented 3 vertices (represented by Tuples) in a tri.
          * @return std::array<Tuple, 3> each tuple owns a different vertex.
          */
-        std::array<Tuple, 3> oriented_tri_vertices(const TriMesh& m) const
-        {
-            std::array<Tuple, 3> vs;
-            for (int j = 0; j < 3; j++) {
-                vs[j].m_vid = m.m_tri_connectivity[m_fid][j];
-                vs[j].m_eid = (j + 2) % 3;
-                vs[j].m_fid = m_fid;
-            }
-            return vs;
-        }
+        std::array<Tuple, 3> oriented_tri_vertices(const TriMesh& m) const;
 
 
         // TODO: name consistency with 3d
@@ -198,18 +157,7 @@ public:
     TriMesh() {}
     virtual ~TriMesh() {}
 
-    inline void create_mesh(size_t n_vertices, const std::vector<std::array<size_t, 3>>& tris)
-    {
-        m_vertex_connectivity.resize(n_vertices);
-        m_tri_connectivity.resize(tris.size());
-        size_t hash_cnt = 0;
-        for (int i = 0; i < tris.size(); i++) {
-            m_tri_connectivity[i].m_indices = tris[i];
-            m_tri_connectivity[i].hash = hash_cnt;
-            hash_cnt++;
-            for (int j = 0; j < 3; j++) m_vertex_connectivity[tris[i][j]].m_conn_tris.push_back(i);
-        }
-    }
+    void create_mesh(size_t n_vertices, const std::vector<std::array<size_t, 3>>& tris);
 
     /**
      * Generate a vector of Tuples from global vertex index and __local__ edge index
@@ -218,34 +166,7 @@ public:
      * eid assigned counter clockwise as in the ilustrated example
      * @return vector of Tuples
      */
-    std::vector<Tuple> get_vertices() const
-    {
-        // TODO: move to cpp
-        const TriMesh& m = *this;
-        const size_t n_vertices = m_vertex_connectivity.size();
-        std::vector<Tuple> all_vertices_tuples;
-        all_vertices_tuples.resize(n_vertices);
-        for (size_t i = 0; i < n_vertices; i++) {
-            const std::vector<size_t>& v_conn_fids = m_vertex_connectivity[i].m_conn_tris;
-            size_t fid = *min_element(v_conn_fids.begin(), v_conn_fids.end());
-
-            // get the 3 vid
-            const std::array<size_t, 3> f_conn_verts = m_tri_connectivity[fid].m_indices;
-            assert(i == f_conn_verts[0] || i == f_conn_verts[1] || i == f_conn_verts[2]);
-            size_t eid;
-            // eid is the same as the lvid
-            if (i == f_conn_verts[0]) eid = 2;
-            if (i == f_conn_verts[1])
-                eid = 0;
-            else
-                eid = 1;
-
-            Tuple v_tuple = Tuple(i, eid, fid, m);
-            assert(v_tuple.is_valid(m));
-            all_vertices_tuples[i] = v_tuple;
-        }
-        return all_vertices_tuples;
-    }
+    std::vector<Tuple> get_vertices() const;
 
     /**
      * Generate a vector of Tuples from global face index
@@ -253,52 +174,14 @@ public:
      * local eid assigned counter clockwise as in the ilustrated example
      * @return vector of Tuples
      */
-    std::vector<Tuple> get_faces() const
-    {
-        // TODO: move to cpp
-        const TriMesh& m = *this;
-        std::vector<Tuple> all_faces_tuples;
-        all_faces_tuples.resize(m.m_tri_connectivity.size());
-        for (size_t i = 0; i < m.m_tri_connectivity.size(); i++) {
-            // get the 3 vid
-            const std::array<size_t, 3>& f_conn_verts = m.m_tri_connectivity[i].m_indices;
-            size_t vid = f_conn_verts[0];
-            Tuple f_tuple = Tuple(vid, 2, i, m);
-            assert(f_tuple.is_valid(m));
-            all_faces_tuples[i] = f_tuple;
-        }
-        return all_faces_tuples;
-    }
+    std::vector<Tuple> get_faces() const;
 
     /**
      * Generate a vector of Tuples for each edge
      * @note ensures the fid assigned is the smallest between faces adjacent to the edge
      * @return vector of Tuples
      */
-    std::vector<Tuple> get_edges() const
-    {
-        // TODO: move to cpp
-        const TriMesh& m = *this;
-        std::vector<Tuple> all_edges_tuples;
-        all_edges_tuples.reserve(m.m_tri_connectivity.size() * 3 / 2);
-        for (size_t i = 0; i < m.m_tri_connectivity.size(); i++) {
-            for (int j = 0; j < 3; j++) {
-                size_t vid = m.m_tri_connectivity[i].m_indices[j];
-                size_t eid = (j + 2) % 3;
-                Tuple e_tuple = Tuple(vid, eid, i, m);
-                assert(e_tuple.is_valid(m));
-                Tuple e_tuple2 = e_tuple.switch_face(m).value_or(e_tuple);
-                assert(e_tuple2.is_valid(m));
-                // return itself if it is a boundary triangle
-                size_t fid2 = e_tuple2.get_fid();
-                if (fid2 < i)
-                    continue;
-                else
-                    all_edges_tuples.push_back(e_tuple);
-            }
-        }
-        return all_edges_tuples;
-    }
+    std::vector<Tuple> get_edges() const;
 
     template <typename T>
     using vector = tbb::concurrent_vector<T>;
@@ -307,27 +190,8 @@ private:
     vector<VertexConnectivity> m_vertex_connectivity;
     vector<TriangleConnectivity> m_tri_connectivity;
 
-    size_t get_next_empty_slot_t()
-    {
-        // TODO: move to cpp
-        m_tri_connectivity.emplace_back();
-        resize_attributes(
-            m_vertex_connectivity.size(),
-            m_tri_connectivity.size() * 3,
-            m_tri_connectivity.size());
-        return m_tri_connectivity.size() - 1;
-    }
-
-    size_t get_next_empty_slot_v()
-    {
-        // TODO: move to cpp
-        m_vertex_connectivity.emplace_back();
-        resize_attributes(
-            m_vertex_connectivity.size(),
-            m_tri_connectivity.size() * 3,
-            m_tri_connectivity.size());
-        return m_vertex_connectivity.size() - 1;
-    }
+    size_t get_next_empty_slot_t();
+    size_t get_next_empty_slot_v();
 
 protected:
     virtual bool split_before(const Tuple& t) { return true; }
@@ -370,21 +234,8 @@ public:
 
     bool check_link_condition(const Tuple& t) const; // DP: should be private
     bool check_mesh_connectivity_validity() const; // DP: should be private
-    bool check_manifold(const Tuple& t) const
-    {
-        // TODO: move to cpp
-        auto v1_conn_tris = m_vertex_connectivity[t.get_vid()].m_conn_tris;
-        auto v2_conn_tris = m_vertex_connectivity[t.switch_vertex(*this).get_vid()].m_conn_tris;
+    bool check_manifold(const Tuple& t) const;
 
-        size_t fid1 = t.get_fid();
-        size_t fid2 = switch_face(t).value_or(t).get_fid();
-
-        vector_erase(v1_conn_tris, fid1);
-        vector_erase(v1_conn_tris, fid2);
-        vector_erase(v2_conn_tris, fid1);
-        vector_erase(v2_conn_tris, fid2);
-        return (v1_conn_tris.size() + v2_conn_tris.size() > 0);
-    }
     /**
      * Split an edge
      *
