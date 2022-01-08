dbebbf52b2612b004bfc06dcf6903ab8c66bb2dc
Qingnan Zhou <qzhou@adobe.com>
2022-01-07 21:34:09 -0500

    Making gcc happy.

--- tests/test_io.cpp
+++ tests/test_io.cpp
@@ -22,26 +22,26 @@ TEST_CASE("io", "[io][mshio]")
 
         // Edge visualization.
         msh.add_edge_vertices(vertices.size(), [&](size_t i) { return vertices[i]; });
-        msh.add_edges(3, [&](size_t i) { return std::array<size_t, 2>{0, i + 1}; });
-        msh.add_edge_vertex_attribute<1>("ev index", [&](size_t i) {return i; } );
+        msh.add_edges(3, [&](size_t i) { return std::array<size_t, 2>{{0, i + 1}}; });
+        msh.add_edge_vertex_attribute<1>("ev index", [&](size_t i) { return i; });
         msh.add_edge_attribute<1>("e index", [&](size_t i) { return i; });
 
         // Face visualization.
         msh.add_face_vertices(vertices.size(), [&](size_t i) { return vertices[i]; });
         msh.add_faces(4, [&](size_t i) -> std::array<size_t, 3> {
-            if (i == 0) return {1, 2, 3};
-            if (i == 1) return {2, 0, 3};
-            if (i == 2) return {0, 1, 3};
-            if (i == 3) return {0, 2, 1};
+            if (i == 0) return {{1, 2, 3}};
+            if (i == 1) return {{2, 0, 3}};
+            if (i == 2) return {{0, 1, 3}};
+            if (i == 3) return {{0, 2, 1}};
             throw std::runtime_error("Invalid index");
         });
-        msh.add_face_vertex_attribute<1>("fv index", [&](size_t i) { return i; } );
+        msh.add_face_vertex_attribute<1>("fv index", [&](size_t i) { return i; });
         msh.add_face_attribute<1>("f index", [&](size_t i) { return i; });
 
         // Tet visualization.
         msh.add_tet_vertices(vertices.size(), [&](size_t i) { return vertices[i]; });
         msh.add_tets(tets.size(), [&](size_t i) { return tets[i]; });
-        msh.add_tet_vertex_attribute<1>("tv index", [&](size_t i) { return i; } );
+        msh.add_tet_vertex_attribute<1>("tv index", [&](size_t i) { return i; });
         msh.add_tet_attribute<1>("t index", [&](size_t i) { return i; });
 
         // Save and validate.
