3b94df530bc6d9f5fea006d1559491cef82a9972
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2022-01-09 18:31:09 -0800

    Fix CMake wmtk_data dependency.

--- cmake/recipes/wmtk_data.cmake
+++ cmake/recipes/wmtk_data.cmake
@@ -11,13 +11,12 @@ include(FetchContent)
 set(WMT_DATA_ROOT "${PROJECT_SOURCE_DIR}/data/" CACHE PATH "Where should the toolkit download and look for test data?")
 
 ExternalProject_Add(
-    wmt_data
+    wmtk_data_download
     PREFIX "${FETCHCONTENT_BASE_DIR}/wmtk-test-data"
     SOURCE_DIR ${WMT_DATA_ROOT}
 
     GIT_REPOSITORY https://github.com/wildmeshing/data.git
     GIT_TAG e276819f96333ac22c1fdad43f37b977f4e4c470
-    GIT_SHALLOW FALSE
 
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
@@ -28,4 +27,7 @@ ExternalProject_Add(
 # Create a dummy target for convenience
 add_library(wmtk_data INTERFACE)
 add_library(wmtk::data ALIAS wmtk_data)
+
+add_dependencies(wmtk_data wmtk_data_download)
+
 target_compile_definitions(wmtk_data INTERFACE  WMT_DATA_DIR=\"${WMT_DATA_ROOT}\")
