e6610a28d15a8591e4fd2067551670a319008c25
Zhongshi <zj495@nyu.edu>
2022-01-12 21:42:37 +0800

    refactor and add g-d

--- src/wmtk/utils/TetraQualityUtils.cpp
+++ src/wmtk/utils/TetraQualityUtils.cpp
@@ -3,6 +3,7 @@
 #include "AMIPS.h"
 #include "EnergyHarmonicTet.hpp"
 #include "Logger.hpp"
+#include "spdlog/sinks/ansicolor_sink.h"
 
 #include <igl/doublearea.h>
 #include <spdlog/fmt/ostr.h>
@@ -26,77 +27,156 @@ std::array<size_t, 4> wmtk::orient_preserve_tet_reorder(
     auto newconn = conn;
     for (auto j = 0; j < 4; j++) newconn[j] = conn[reorder[vl_id][j]];
     return newconn;
+}
+
+auto newton_direction = [](auto& compute_energy,
+                           auto& compute_jacobian,
+                           auto& compute_hessian,
+                           auto& assembles,
+                           const Eigen::Vector3d& pos) -> Eigen::Vector3d {
+    auto total_energy = 0.;
+    Eigen::Vector3d total_jac = Eigen::Vector3d::Zero();
+    Eigen::Matrix3d total_hess = Eigen::Matrix3d::Zero();
+
+    // E = \sum_i E_i(x)
+    // J = \sum_i J_i(x)
+    // H = \sum_i H_i(x)
+    auto local_id = 0;
+    for (auto& T : assembles) {
+        for (auto j = 0; j < 3; j++) {
+            T[j] = pos[j]; // only filling the front point.
+        }
+        auto jac = decltype(total_jac)();
+        auto hess = decltype(total_hess)();
+        total_energy += compute_energy(T);
+        compute_jacobian(T, jac);
+        compute_hessian(T, hess);
+        total_jac += jac;
+        total_hess += hess;
+        assert(!std::isnan(total_energy));
+    }
+    Eigen::Vector3d x = total_hess.ldlt().solve(total_jac);
+    wmtk::logger().trace("energy {}", total_energy);
+    if (total_jac.isApprox(total_hess * x)) // a hacky PSD trick. TODO: change this.
+        return -x;
+    else {
+        wmtk::logger().trace("gradient descent instead.");
+        return -total_jac;
+    }
 };
 
-Eigen::Vector3d wmtk::newton_direction_from_stack(std::vector<std::array<double, 12>>& assembles)
+
+auto gradient_direction = [](auto& compute_energy,
+                             auto& compute_jacobian,
+                             auto& assembles,
+                             const Eigen::Vector3d& pos) -> Eigen::Vector3d {
+    Eigen::Vector3d total_jac = Eigen::Vector3d::Zero();
+
+    for (auto& T : assembles) {
+        for (auto j = 0; j < 3; j++) {
+            T[j] = pos[j]; // only filling the front point.
+        }
+        auto jac = decltype(total_jac)();
+        compute_jacobian(T, jac);
+        total_jac += jac;
+    }
+    return total_jac;
+};
+
+auto linesearch = [](auto&& energy_from_point,
+                     const Eigen::Vector3d& pos,
+                     const Eigen::Vector3d& dir,
+                     const int& max_iter) {
+    auto lr = 0.8;
+    auto old_energy = energy_from_point(pos);
+    wmtk::logger().trace("dir {}", dir);
+    for (auto iter = 1; iter <= max_iter; iter++) {
+        Eigen::Vector3d newpos = pos + std::pow(lr, iter) * dir;
+        wmtk::logger().trace("pos {}, dir {}, [{}]", pos, dir, std::pow(lr, iter));
+        auto new_energy = energy_from_point(newpos);
+        wmtk::logger().trace("iter {}, {}, [{}]", iter, new_energy, newpos);
+        if (new_energy < old_energy) return newpos; // TODO: armijo conditions.
+    }
+    return pos;
+};
+
+Eigen::Vector3d wmtk::newton_method_from_stack(
+    std::vector<std::array<double, 12>>& assembles,
+    std::function<double(const std::array<double, 12>&)> compute_energy,
+    std::function<void(const std::array<double, 12>&, Eigen::Vector3d&)> compute_jacobian,
+    std::function<void(const std::array<double, 12>&, Eigen::Matrix3d&)> compute_hessian)
 {
     assert(!assembles.empty());
     auto& T = assembles.front();
     Eigen::Vector3d old_pos(T[0], T[1], T[2]);
-    auto newton_direction = [&assembles](auto& pos) -> Eigen::Vector3d {
-        auto total_energy = 0.;
-        Eigen::Vector3d total_jac = Eigen::Vector3d::Zero();
-        Eigen::Matrix3d total_hess = Eigen::Matrix3d::Zero();
 
-        // E = \sum_i E_i(x)
-        // J = \sum_i J_i(x)
-        // H = \sum_i H_i(x)
-        auto local_id = 0;
-        for (auto& T : assembles) {
-            for (auto j = 0; j < 3; j++) {
-                T[j] = pos[j]; // only filling the front point.
-            }
-            auto jac = decltype(total_jac)();
-            auto hess = decltype(total_hess)();
-            total_energy += wmtk::AMIPS_energy(T);
-            wmtk::AMIPS_jacobian(T, jac);
-            wmtk::AMIPS_hessian(T, hess);
-            total_jac += jac;
-            total_hess += hess;
-            assert(!std::isnan(total_energy));
-        }
-        Eigen::Vector3d x = total_hess.ldlt().solve(total_jac);
-        logger().trace("energy {}", total_energy);
-        if (total_jac.isApprox(total_hess * x)) // a hacky PSD trick. TODO: change this.
-            return -x;
-        else {
-            logger().trace("gradient descent instead.");
-            return -total_jac;
-        }
-    };
-    auto compute_energy = [&assembles](const Eigen::Vector3d& pos) -> double {
+    auto energy_from_point = [&assembles, &compute_energy](const Eigen::Vector3d& pos) -> double {
         auto total_energy = 0.;
         for (auto& T : assembles) {
             for (auto j = 0; j < 3; j++) {
                 T[j] = pos[j]; // only filling the front point x,y,z.
             }
-            total_energy += wmtk::AMIPS_energy(T);
+            total_energy += compute_energy(T);
         }
         return total_energy;
     };
-    auto linesearch = [&compute_energy](
-                          const Eigen::Vector3d& pos,
-                          const Eigen::Vector3d& dir,
-                          const int& max_iter) {
-        auto lr = 0.8;
-        auto old_energy = compute_energy(pos);
-        logger().trace("dir {}", dir);
-        for (auto iter = 1; iter <= max_iter; iter++) {
-            Eigen::Vector3d newpos = pos + std::pow(lr, iter) * dir;
-            logger().trace("pos {}, dir {}, [{}]", pos, dir, std::pow(lr, iter));
-            auto new_energy = compute_energy(newpos);
-            logger().trace("iter {}, {}, [{}]", iter, new_energy, newpos);
-            if (new_energy < old_energy) return newpos; // TODO: armijo conditions.
+
+    auto compute_new_valid_pos =
+        [&energy_from_point, &assembles, &compute_energy, &compute_jacobian, &compute_hessian](
+            const Eigen::Vector3d& old_pos) {
+            auto current_pos = old_pos;
+            auto line_search_iters = 12;
+            auto newton_iters = 10;
+            for (auto iter = 0; iter < newton_iters; iter++) {
+                auto dir = newton_direction(
+                    compute_energy,
+                    compute_jacobian,
+                    compute_hessian,
+                    assembles,
+                    current_pos);
+                auto newpos = linesearch(energy_from_point, current_pos, dir, line_search_iters);
+                if ((newpos - current_pos).norm() < 1e-9) // barely moves
+                {
+                    break;
+                }
+                current_pos = newpos;
+            }
+            return current_pos;
+        };
+    return compute_new_valid_pos(old_pos);
+}
+
+Eigen::Vector3d wmtk::gradient_descent_from_stack(
+    std::vector<std::array<double, 12>>& assembles,
+    std::function<double(const std::array<double, 12>&)> compute_energy,
+    std::function<void(const std::array<double, 12>&, Eigen::Vector3d&)> compute_jacobian)
+{
+    assert(!assembles.empty());
+    auto& T = assembles.front();
+    Eigen::Vector3d old_pos(T[0], T[1], T[2]);
+
+    auto energy_from_point = [&assembles, &compute_energy](const Eigen::Vector3d& pos) -> double {
+        auto total_energy = 0.;
+        for (auto& T : assembles) {
+            for (auto j = 0; j < 3; j++) {
+                T[j] = pos[j]; // only filling the front point x,y,z.
+            }
+            total_energy += compute_energy(T);
         }
-        return pos;
+        return total_energy;
     };
-    auto compute_new_valid_pos = [&linesearch, &newton_direction](const Eigen::Vector3d& old_pos) {
+
+    auto compute_new_valid_pos = [&energy_from_point,
+                                  &assembles,
+                                  &compute_energy,
+                                  &compute_jacobian](const Eigen::Vector3d& old_pos) {
         auto current_pos = old_pos;
         auto line_search_iters = 12;
         auto newton_iters = 10;
         for (auto iter = 0; iter < newton_iters; iter++) {
-            auto dir = newton_direction(current_pos);
-            auto newpos = linesearch(current_pos, dir, line_search_iters);
+            Eigen::Vector3d dir =
+                -gradient_direction(compute_energy, compute_jacobian, assembles, current_pos);
+            auto newpos = linesearch(energy_from_point, current_pos, dir, line_search_iters);
             if ((newpos - current_pos).norm() < 1e-9) // barely moves
             {
                 break;
@@ -106,7 +186,7 @@ Eigen::Vector3d wmtk::newton_direction_from_stack(std::vector<std::array<double,
         return current_pos;
     };
     return compute_new_valid_pos(old_pos);
-};
+}
 
 double wmtk::harmonic_energy(const Eigen::MatrixXd& verts)
 {
--- src/wmtk/utils/TetraQualityUtils.hpp
+++ src/wmtk/utils/TetraQualityUtils.hpp
@@ -5,14 +5,22 @@
 namespace wmtk {
 
 /**
- * Newton method with regard to AMIPS_energy.
+ * Newton method or gradient descent.
  *
  * @param stack with flattened 4x3 vertices positions, with the mover vertex always on the front.
  * This is the same convention with AMIPS_energy
  * @return Descend direction as computed from Newton method, (or gradient descent if Newton fails).
  */
-Eigen::Vector3d newton_direction_from_stack(std::vector<std::array<double, 12>>& stack);
+Eigen::Vector3d newton_method_from_stack(
+    std::vector<std::array<double, 12>>& stack,
+    std::function<double(const std::array<double, 12>&)> energy,
+    std::function<void(const std::array<double, 12>&, Eigen::Vector3d&)> jacobian,
+    std::function<void(const std::array<double, 12>&, Eigen::Matrix3d&)> hessian);
 
+Eigen::Vector3d gradient_descent_from_stack(
+    std::vector<std::array<double, 12>>& stack,
+    std::function<double(const std::array<double, 12>&)> energy,
+    std::function<void(const std::array<double, 12>&, Eigen::Vector3d&)> jacobian);
 /**
  * Reorders indices in a tetrahedron such that v0 is on the front. Using the tetra symmetry to
  * preserve orientation. Assumes v0 in tetra.
@@ -25,7 +33,7 @@ std::array<size_t, 4> orient_preserve_tet_reorder(const std::array<size_t, 4>& t
 
 /**
  * @brief Harmonic Triangulation energy: trace of Laplacian operator
- * 
+ *
  */
 double harmonic_energy(const Eigen::MatrixXd& verts);
 } // namespace wmtk
