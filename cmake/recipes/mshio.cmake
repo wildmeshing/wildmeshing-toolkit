ce628a15073c3e4ec3840ac85bc9fa8cdb1729d6
yfzhou42 <yfzhou42@gmail.com>
2022-01-13 12:57:51 -0500

    make fix

--- cmake/recipes/mshio.cmake
+++ cmake/recipes/mshio.cmake
@@ -7,7 +7,7 @@ message(STATUS "Third-party (external): creating target 'mshio::mshio'")
 FetchContent_Declare(
     mshio
     GIT_REPOSITORY https://github.com/qnzhou/MshIO.git
-    GIT_TAG        5fadb581700254f75e42269aacbd9800fd5064f2
+    GIT_TAG        995c28d6b615c74f4d5e27ac1185c040d5b57a33
     GIT_SHALLOW TRUE
 )
 
