24379fce77856f8666a17fe9fa3f319f37ab5f6c
Zhongshi <zj495@nyu.edu>
2022-01-14 09:43:38 +0800

    remove and update

--- cmake/recipes/fenvelope.cmake
+++ cmake/recipes/fenvelope.cmake
@@ -17,6 +17,5 @@ FetchContent_Declare(
     fenvelope
     GIT_REPOSITORY https://github.com/wangbolun300/fast-envelope.git
     GIT_TAG 5d5d5ac99b14400b2757e043fbc1bd9eacd0cced
-    GIT_SHALLOW TRUE
 )
 FetchContent_MakeAvailable(fenvelope)
