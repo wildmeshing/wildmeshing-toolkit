1e55fc828e8e5a6279a99a0206caa48dedae858c
Zhongshi <zj495@nyu.edu>
2022-01-18 05:43:40 -0500

    fix compilation

--- app/EdgeOperations2d/EdgeOperations2d.h
+++ app/EdgeOperations2d/EdgeOperations2d.h
@@ -70,7 +70,6 @@ public:
     bool smooth(const Tuple& t)
     {
         auto one_ring_edges = get_one_ring_edges_for_vertex(t);
-        assert(one_ring_edges.size() = get_vertices().size() - 1);
         Eigen::Vector3d after_smooth(0, 0, 0);
         Eigen::Vector3d after_smooth_boundary(0, 0, 0);
         int boundary = 0;
@@ -134,9 +133,9 @@ public:
 
     bool collapse_qec();
     // get the quadrix in form of an array of 10 floating point numbers
-    Eigen::MatrixXd compute_Q_f(wmtk::TriMesh::Tuple& t);
+    std::array<double, 10> compute_Q_f(wmtk::TriMesh::Tuple& t);
 
-    Eigen::MatrixXd compute_Q_v(wmtk::TriMesh::Tuple& t);
+    std::array<double, 10> compute_Q_v(wmtk::TriMesh::Tuple& t);
 
     double compute_cost_for_v(wmtk::TriMesh::Tuple& v_tuple);
 
