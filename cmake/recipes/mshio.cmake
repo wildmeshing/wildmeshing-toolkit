e56c117b26482536e77964e620c7d487a57d21fe
Qingnan Zhou <qzhou@adobe.com>
2022-01-18 10:52:05 -0500

    Fix IO errors.

--- cmake/recipes/mshio.cmake
+++ cmake/recipes/mshio.cmake
@@ -7,7 +7,7 @@ message(STATUS "Third-party (external): creating target 'mshio::mshio'")
 FetchContent_Declare(
     mshio
     GIT_REPOSITORY https://github.com/qnzhou/MshIO.git
-    GIT_TAG        5fadb581700254f75e42269aacbd9800fd5064f2
+    GIT_TAG        dbfe01f072a90d067a25c5e962ea1f87e34c4fd3
 )
 
 FetchContent_MakeAvailable(mshio)
