f6bb1ed2db9109302bdf0dc7284c07d22beb9871
Teseo Schneider <teseo@bluewin.ch>
2022-01-26 23:50:29 -0800

    better target for remeshing

--- app/Remeshing/app/main.cpp
+++ app/Remeshing/app/main.cpp
@@ -25,7 +25,7 @@ void run_remeshing(std::string input, double len, std::string output, UniformRem
     auto duration = duration_cast<milliseconds>(stop - start);
 
     m.consolidate_mesh();
-    m.write_triangle_mesh(fmt::format("{}_{}.obj", output, len));
+    m.write_triangle_mesh(output);
     wmtk::logger().info("runtime {}", duration.count());
     wmtk::logger().info("current_memory {}", getCurrentRSS() / (1024. * 1024));
     wmtk::logger().info("peak_memory {}", getPeakRSS() / (1024. * 1024));
@@ -41,10 +41,15 @@ int main(int argc, char** argv)
     // output
     // ep
     const std::string root(WMT_DATA_DIR);
-    const std::string path = root + argv[1];
+    const std::string path = argv[1];
     Eigen::MatrixXd V;
     Eigen::MatrixXi F;
     bool ok = igl::read_triangle_mesh(path, V, F);
+    Eigen::VectorXi SVI, SVJ;
+    Eigen::MatrixXd temp_V = V; // for STL file
+    igl::remove_duplicate_vertices(temp_V, 0, V, SVI, SVJ);
+    for (int i = 0; i < F.rows(); i++)
+        for (int j : {0, 1, 2}) F(i, j) = SVJ[F(i, j)];
     wmtk::logger().info("Before_vertices#: {} \n Before_tris#: {}", V.rows(), F.rows());
 
 
@@ -66,15 +71,18 @@ int main(int argc, char** argv)
         return 1;
     } else {
         UniformRemeshing m(v);
-        m.create_mesh(v.size(), tri, envelope_size);
+        m.create_mesh(v.size(), tri, envelope_size, atoi(argv[4]));
         assert(m.check_mesh_connectivity_validity());
         std::vector<double> properties = m.average_len_valen();
         wmtk::logger().info(
             "edgelen: avg max min valence:avg max min before remesh is: {}",
             properties);
-
+        igl::Timer timer;
+        timer.start();
         run_remeshing(path, properties[0] * 5, std::string(argv[2]), m);
         //run_remeshing(path, properties[0] / 2, std::string(argv[2]), m);
+        timer.stop();
+        logger().info("Took {}", timer.getElapsedTimeInSec());
     }
 
 
