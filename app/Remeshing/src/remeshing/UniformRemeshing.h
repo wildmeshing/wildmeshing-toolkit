7a9ec8b3c56cc65907405ef14f7e14b5d0936f98
JcDai <jiacheng_dai@qq.com>
2022-05-03 16:17:23 -0400

    add morton to remeshing

--- app/Remeshing/src/remeshing/UniformRemeshing.h
+++ app/Remeshing/src/remeshing/UniformRemeshing.h
@@ -15,6 +15,7 @@
 #include <tbb/task_group.h>
 #include <fastenvelope/FastEnvelope.h>
 #include <wmtk/utils/EnableWarnings.hpp>
+#include <tbb/parallel_sort.h>
 // clang-format on
 
 #include <Eigen/Core>
@@ -76,7 +77,8 @@ public:
             m_envelope.init(V, F, 0.0);
 
 
-        partition_mesh();
+        // partition_mesh();
+        partition_mesh_morton();
         for (auto v : frozen_verts) vertex_attrs[v].freeze = true;
         for (auto e : get_edges()) {
             if (is_boundary_edge(e)) {
@@ -117,12 +119,106 @@ public:
         return true;
     }
 
-
     void partition_mesh()
     {
         auto m_vertex_partition_id = partition_TriMesh(*this, NUM_THREADS);
         for (auto i = 0; i < m_vertex_partition_id.size(); i++)
             vertex_attrs[i].partition_id = m_vertex_partition_id[i];
+    }
+
+    void partition_mesh_morton(){
+        wmtk::logger().info("Number of parts: {} by morton", NUM_THREADS);
+
+        tbb::task_arena arena(NUM_THREADS);
+
+        arena.execute([&] {
+
+            std::vector<Eigen::Vector3d> V_v(vertex_attrs.size());
+
+            tbb::parallel_for(
+                tbb::blocked_range<int>(0, V_v.size()),
+                [&](tbb::blocked_range<int> r) {
+                    for (int i = r.begin(); i < r.end(); i++) {
+                        V_v[i] = vertex_attrs[i].pos;
+                    }
+                });
+
+            struct sortstruct
+            {
+                int order;
+                Resorting::MortonCode64 morton;
+            };
+
+            std::vector<sortstruct> list_v;
+            list_v.resize(V_v.size());
+            const int multi = 1000;
+            // since the morton code requires a correct scale of input vertices,
+            //  we need to scale the vertices if their coordinates are out of range
+            std::vector<Eigen::Vector3d> V = V_v; // this is for rescaling vertices
+            Eigen::Vector3d vmin, vmax;
+            vmin = V.front();
+            vmax = V.front();
+
+            for (size_t j = 0; j < V.size(); j++) {
+                for (int i = 0; i < 3; i++) {
+                    vmin(i) = std::min(vmin(i), V[j](i));
+                    vmax(i) = std::max(vmax(i), V[j](i));
+                }
+            }
+
+            Eigen::Vector3d center = (vmin + vmax) / 2;
+
+            tbb::parallel_for(tbb::blocked_range<int>(0, V.size()), [&](tbb::blocked_range<int> r) {
+                for (int i = r.begin(); i < r.end(); i++) {
+                    V[i] = V[i] - center;
+                }
+            });
+
+            Eigen::Vector3d scale_point =
+                vmax - center; // after placing box at origin, vmax and vmin are symetric.
+
+            double xscale, yscale, zscale;
+            xscale = fabs(scale_point[0]);
+            yscale = fabs(scale_point[1]);
+            zscale = fabs(scale_point[2]);
+            double scale = std::max(std::max(xscale, yscale), zscale);
+            if (scale > 300) {
+                tbb::parallel_for(
+                    tbb::blocked_range<int>(0, V.size()),
+                    [&](tbb::blocked_range<int> r) {
+                        for (int i = r.begin(); i < r.end(); i++) {
+                            V[i] = V[i] / scale;
+                        }
+                    });
+            }
+
+            tbb::parallel_for(tbb::blocked_range<int>(0, V.size()), [&](tbb::blocked_range<int> r) {
+                for (int i = r.begin(); i < r.end(); i++) {
+                    list_v[i].morton = Resorting::MortonCode64(
+                        int(V[i][0] * multi),
+                        int(V[i][1] * multi),
+                        int(V[i][2] * multi));
+                    list_v[i].order = i;
+                }
+            });
+
+            const auto morton_compare = [](const sortstruct& a, const sortstruct& b) {
+                return (a.morton < b.morton);
+            };
+
+            tbb::parallel_sort(list_v.begin(), list_v.end(), morton_compare);
+
+            int interval = list_v.size() / NUM_THREADS + 1;
+
+            tbb::parallel_for(
+                tbb::blocked_range<int>(0, list_v.size()),
+                [&](tbb::blocked_range<int> r) {
+                    for (int i = r.begin(); i < r.end(); i++) {
+                        vertex_attrs[list_v[i].order].partition_id = i / interval;
+                    }
+                });
+            
+        });
     }
 
     Eigen::Vector3d smooth(const Tuple& t);
