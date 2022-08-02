e7235710953d2d1f0ea6813999c03eb5ffb28930
Daniele Panozzo <daniele.panozzo@gmail.com>
2022-08-01 22:58:34 -0400

    removed duplicate 3D delaunay, added 2D delaunay and tests (#240)

--- src/wmtk/utils/Delaunay.hpp
+++ src/wmtk/utils/Delaunay.hpp
@@ -7,9 +7,11 @@
 namespace wmtk {
 using Point3D = std::array<double, 3>;
 using Tetrahedron = std::array<size_t, 4>;
+using Point2D = std::array<double, 2>;
+using Triangle = std::array<size_t, 3>;
 
 /**
- * Compute the Delaunay tetrahedralization of the input points.
+ * Compute the Delaunay tetrahedralization of the input 3D points.
  * @param[in] points
  *
  * @returns A tuple of (vertices, tets).
@@ -21,6 +23,20 @@ using Tetrahedron = std::array<size_t, 4>;
  */
 auto delaunay3D(const std::vector<Point3D>& points)
     -> std::pair<std::vector<Point3D>, std::vector<Tetrahedron>>;
-auto delaunay3D_conn(const std::vector<Point3D>& points)
-    -> std::vector<Tetrahedron>;
+
+
+/**
+ * Compute the Delaunay triangulation of the input 2D points.
+ * @param[in] points
+ *
+ * @returns A tuple of (vertices, tets).
+ *
+ * Example:
+ *     std::vector<Point2D> points;
+ *     // Populate points.
+ *     auto [vertices, triangles] = delaunay2D(points);
+ */
+auto delaunay2D(const std::vector<Point2D>& points)
+    -> std::pair<std::vector<Point2D>, std::vector<Triangle>>;
+
 } // namespace wmtk
--- tests/test_delaunay.cpp
+++ tests/test_delaunay.cpp
@@ -5,6 +5,7 @@
 #include <limits>
 
 namespace {
+    // TODO: this should use a predicate instead
 inline double compute_tet_volume(
     const wmtk::Point3D& p,
     const wmtk::Point3D& q,
@@ -19,7 +20,24 @@ inline double compute_tet_volume(
         p[2] * r[0] * s[1] - p[2] * r[1] * s[0] - q[0] * r[1] * s[2] + q[0] * r[2] * s[1] +
         q[1] * r[0] * s[2] - q[1] * r[2] * s[0] - q[2] * r[0] * s[1] + q[2] * r[1] * s[0]);
 }
+// TODO: this should use a predicate instead
+inline double compute_tri_area(
+    const wmtk::Point2D& a,
+    const wmtk::Point2D& b,
+    const wmtk::Point2D& c)
+{
+    auto a0=a[0];
+    auto a1=a[1];
+    auto b0=b[0];
+    auto b1=b[1];
+    auto c0=c[0];
+    auto c1=c[1];
+
+    return (-(a1*b0) + a0*b1 + a1*c0 - b1*c0 - a0*c1 + b0*c1)/2.0;
+}
+
 } // namespace
+
 
 TEST_CASE("Delaunay3D", "[delaunay][3d]")
 {
@@ -166,5 +184,99 @@ TEST_CASE("Delaunay3D", "[delaunay][3d]")
         REQUIRE(vertices.size() == N * N * N);
         REQUIRE(tets.size() == (N - 1) * (N - 1) * (N - 1) * 6);
         validate(vertices, tets);
+    }
+}
+
+TEST_CASE("Delaunay2D", "[delaunay][2d]")
+{
+    using namespace wmtk;
+
+    auto validate = [](const auto& vertices, const auto& triangles) {
+        constexpr double EPS = std::numeric_limits<double>::epsilon();
+        const size_t num_vertices = vertices.size();
+        const size_t num_triangles = triangles.size();
+
+        for (size_t i = 0; i < num_triangles; i++) {
+            const auto& tri = triangles[i];
+            REQUIRE(tri[0] < num_vertices);
+            REQUIRE(tri[1] < num_vertices);
+            REQUIRE(tri[2] < num_vertices);
+
+            // tri must be positive oriented and non-degenerate.
+            REQUIRE(
+                compute_tri_area( 
+                    vertices[tri[0]],
+                    vertices[tri[1]],
+                    vertices[tri[2]]) > EPS);
+        }
+    };
+
+    SECTION("Simple")
+    {
+        std::vector<Point2D> points{{{0, 0}}, {{1, 0}}, {{0, 1}}, {{1, 1}}};
+
+        auto [vertices, triangles] = delaunay2D(points);
+        REQUIRE(vertices.size() == 4);
+        REQUIRE(triangles.size() == 2);
+        validate(vertices, triangles);
+    }
+    SECTION("Insufficient points should not fail")
+    {
+        std::vector<Point2D> points{{{1, 0}}, {{0, 1}}};
+
+        auto [vertices, triangles] = delaunay2D(points);
+        REQUIRE(triangles.size() == 0);
+        validate(vertices, triangles);
+    }
+    SECTION("Coplanar pts")
+    {
+        std::vector<Point2D> points{{{0, 0}}, {{1, 0}}, {{2, 0}}, {{3, 0}}};
+
+        auto [vertices, triangles] = delaunay2D(points);
+        REQUIRE(vertices.size() == 4);
+        REQUIRE(triangles.size() == 0);
+        validate(vertices, triangles);
+    }
+    SECTION("Duplicate pts")
+    {
+        std::vector<Point2D>
+            points{{{0, 0}}, {{1, 0}}, {{0, 1}}, {{1, 1}}, {{0, 1}}, {{0, 1}}};
+
+        auto [vertices, triangles] = delaunay2D(points);
+        REQUIRE(vertices.size() == 6); // duplicate pts are kept in the output.
+        REQUIRE(triangles.size() == 2);
+        validate(vertices, triangles);
+    }
+    SECTION("Square with centroid")
+    {
+        std::vector<Point2D> points{
+            {{0, 0}},
+            {{1, 0}},
+            {{1, 1}},
+            {{0, 1}},
+            {{0.5, 0.5}}
+        };
+
+        auto [vertices, triangles] = delaunay2D(points);
+        REQUIRE(vertices.size() == 5);
+        REQUIRE(triangles.size() == 4);
+        validate(vertices, triangles);
+    }
+    SECTION("Regular grid")
+    {
+        constexpr size_t N = 10;
+        std::vector<Point2D> points;
+        points.reserve(N * N);
+        for (size_t i = 0; i < N; i++) {
+            for (size_t j = 0; j < N; j++) {
+                double x = i;
+                double y = j;
+                points.push_back({{x, y}});
+            }
+        }
+        auto [vertices, triangles] = delaunay2D(points);
+        REQUIRE(vertices.size() == N * N);
+        REQUIRE(triangles.size() == (N - 1) * (N - 1) * 2);
+        validate(vertices, triangles);
     }
 }
