c5aa0c115756a861369d7a92f952b41da9ad3a42
Michael Tao <mtao@dgp.toronto.edu>
2023-04-26 17:57:48 -0400

    some minor comments

--- src/wmtk/TriMeshEasy.h
+++ src/wmtk/TriMeshEasy.h
@@ -5,22 +5,40 @@
 
 namespace wmtk {
 
+    // Fast prototyping using TriMesh
+    //
+    // 
+    // TriMeshEasy easy_mesh;
+    // AttributeCollection<Eigen::Vector3d> p;
+    // easy_mesh.vertex_attrs = p;
+    // easy_mesh.split_edge_after = [&](ExecuteReturnData& ret_data) {
+    //    const Tuple& t = ret_data.tuple;
+    //    const Tuple& c = t.switch_vertex(easy_mesh);;
+    //    const Tuple& ot = t.switch_vertex(easy_mesh).switch_edge(easy_mesh).switch_face(easy_mesh).switch_vertex(easy_mesh);
+    //
+    //    
+    //    p[c.vid(easy_mesh)] = .5 * (p[t.vid(easy_mesh)] + p[ot.vid(easy_mesh)]);
+    //    
+    // };
+    // ExecutePass<TriMeshEasy, ExecutionPolicy::kSeq> exec; 
+    // for(const auto& op_ptr: easy_mesh.getOperations()) { exec.add_operation(op_ptr); }
+    //
 
 class TriMeshEasy : public TriMesh
 {
 public:
-    using ExecuteReturnValue = TriMeshOperation::ExecuteReturnData;
+    using ExecuteReturnData = TriMeshOperation::ExecuteReturnData;
     std::vector<std::shared_ptr<TriMeshOperation>> getOperations() const;
 
 
     std::function<bool(const Tuple&)> collapse_edge_before;
-    std::function<bool(ExecuteReturnValue&)> collapse_edge_after;
+    std::function<bool(ExecuteReturnData&)> collapse_edge_after;
     std::function<bool(const Tuple&)> split_edge_before;
-    std::function<bool(ExecuteReturnValue&)> split_edge_after;
+    std::function<bool(ExecuteReturnData&)> split_edge_after;
     std::function<bool(const Tuple&)> swap_edge_before;
-    std::function<bool(ExecuteReturnValue&)> swap_edge_after;
+    std::function<bool(ExecuteReturnData&)> swap_edge_after;
     std::function<bool(const Tuple&)> vertex_smooth_before;
-    std::function<bool(ExecuteReturnValue&)> vertex_smooth_after;
-    std::function<bool(ExecuteReturnValue&)> invariants;
+    std::function<bool(ExecuteReturnData&)> vertex_smooth_after;
+    std::function<bool(ExecuteReturnData&)> invariants;
 };
 } // namespace wmtk
