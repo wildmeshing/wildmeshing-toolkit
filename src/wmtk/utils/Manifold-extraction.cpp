4ca9c630901f39ec41335f0599dcab5ae92ce121
Yifei <yz6994@nyu.edu>
2023-04-26 18:19:38 -0400

    change some comments

--- src/wmtk/utils/Manifold-extraction.cpp
+++ src/wmtk/utils/Manifold-extraction.cpp
@@ -3,13 +3,11 @@
 namespace wmtk {
     auto pntgen2d(const size_t nb_points, const double range) -> std::vector<wmtk::Point2D>{
         std::vector<Point2D> points(nb_points);
-        //auto points = std::make_unique<std::vector<Point2D>>(nb_points);
         std::random_device rd{};
-        std::mt19937 gen(rd());
-        // seed of random gen is 10  //std::mt19937 gen(10);
+        std::mt19937 gen(rd()); //std::mt19937 gen(10);
         std::uniform_real_distribution<double> dis(0.0, range);
         for (size_t i = 0; i < nb_points; ++i) {
-            // generate 3 random doubles between 0 and the given range
+            // generate 2 random doubles between 0 and the given range
             for (auto j = 0; j < 2; j++) {
                 points[i][j] = dis(gen);  //(std::rand() % 10 + 1);
             }
@@ -20,8 +18,7 @@ namespace wmtk {
     auto pntgen3d(size_t nb_points, double range) -> std::vector<wmtk::Point3D>{
         std::vector<Point3D> points(nb_points);
         std::random_device rd{};
-        std::mt19937 gen(rd());
-        // seed of random gen is 10  //std::mt19937 gen(10);
+        std::mt19937 gen(rd()); //std::mt19937 gen(10);
         std::uniform_real_distribution<double> dis(0.0, range);
         for (size_t i = 0; i < nb_points; ++i) {
             // generate 3 random doubles between 0 and the given range
@@ -37,7 +34,7 @@ namespace wmtk {
         std::srand(10);
         for (size_t i = 0 ; i < nb_triangles; i++){
             size_t tag = std::rand() % 2;
-            if (tag == 1) tagass.push_back(i);
+            if (tag == 1) tagass.push_back(i); // for every tagged triangle, add its index to the end
         }
         return tagass;
     }
--- src/wmtk/utils/Manifold-extraction.hpp
+++ src/wmtk/utils/Manifold-extraction.hpp
@@ -6,8 +6,6 @@
 #include <cstdlib>
 #include <random>
 #include <map>
-#include <memory>
-
 
 namespace wmtk {
 using Point3D = std::array<double, 3>;
@@ -16,12 +14,26 @@ using Point2D = std::array<double, 2>;
 using Triangle = std::array<size_t, 3>;
 
 /**
- * Generate 100 random points in 2D square domain
+ * Generate nb_points random points in 2D square domain, from 0 to range
  *
  * @returns A vector of Point2D type.
  *
  */
 auto pntgen2d(const size_t nb_points, const double range) -> std::vector<Point2D>;
+
+/**
+ * Generate nb_points random points in 3D square domain, from 0 to range
+ *
+ * @returns A vector of Point3D type.
+ *
+ */
 auto pntgen3d(size_t nb_points, double range) -> std::vector<Point3D>;
+
+/**
+ * Randomly assign tags to the triangles of size nb_triangles.
+ *
+ * @returns A vector of size_t type, only keeping the indecies of the tagged ones, prob=50%
+ *
+ */
 auto tagassign(size_t nb_triangles) -> std::vector<size_t>;
 } // namespace wmtk
