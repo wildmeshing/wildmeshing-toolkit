4c37730b7437fd3ee275d1ef459d3e7a8fd8718a
Daniel Zint <daniel.zint@mail.de>
2023-05-20 21:36:45 -0400

    update icp toolkit

--- cmake/recipes/ipc-toolkit.cmake
+++ cmake/recipes/ipc-toolkit.cmake
@@ -2,6 +2,6 @@ include(FetchContent)
 FetchContent_Declare(
     ipc_toolkit
     GIT_REPOSITORY https://github.com/ipc-sim/ipc-toolkit.git
-    GIT_TAG 6aaf0ca84917d595a16a1d37dc1e6cd98ca0467c
+    GIT_TAG fda38f65f69e42c6811d2a133d12c31c06e1f2e9
 )
 FetchContent_MakeAvailable(ipc_toolkit)
