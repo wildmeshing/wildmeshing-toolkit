e2a1f1a350495a5b929707b12c8d1f7df3de673d
Daniel Zint <daniel.zint@mail.de>
2023-06-22 14:15:27 -0400

    Cleaning up ipc-toolkit.cmake

--- cmake/recipes/ipc-toolkit.cmake
+++ cmake/recipes/ipc-toolkit.cmake
@@ -1,3 +1,9 @@
+if(TARGET ipc::toolkit)
+    return()
+endif()
+
+
+message(STATUS "Third-party (external): creating target 'ipc::toolkit'")
 include(FetchContent)
 FetchContent_Declare(
     ipc_toolkit
@@ -5,3 +11,5 @@ FetchContent_Declare(
     GIT_TAG fda38f65f69e42c6811d2a133d12c31c06e1f2e9
 )
 FetchContent_MakeAvailable(ipc_toolkit)
+
+set_target_properties(ipc_toolkit PROPERTIES FOLDER third_party)
