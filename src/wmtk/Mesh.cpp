97c422da4fb6e5863d0bfe32af4cbd9dea9e3515
Michael Tao <michael.tao@nyu.edu>
2023-06-22 21:09:44 -0400

    moving some impls from mesh header to cpp

--- src/wmtk/Mesh.cpp
+++ src/wmtk/Mesh.cpp
@@ -29,4 +29,78 @@ TetMesh::TetMesh()
 {}
 
 
+void trimesh_topology_initialization(
+    Eigen::Ref<const RowVectors3l>& F,
+    Eigen::Ref<const RowVectors3l>& FE,
+    Eigen::Ref<const RowVectors3l>& FF,
+    Eigen::Ref<const VectorXl>& VF,
+    Eigen::Ref<const VectorXl>& EF)
+{
+    std::vector<std::vector<long>> TTT;
+    FV.resize(F.rows(), F.cols());
+    FE.resize(F.rows(), F.cols());
+    FF.resize(F.rows(), F.cols());
+    VF.resize(F.rows(), 1);
+    EF.resize(F.rows(), 1);
+    TTT.resize(F.rows(), std::vector<long>(4));
+    for (int f = 0; f < F.rows(); ++f) {
+        for (int i = 0; i < F.cols(); ++i) {
+            // v1 v2 f ei
+            long v1 = std::static_cast<long>(F(f, i));
+            long v2 = std::static_cast<long>(F(f, (i + 1) % F.cols()));
+            if (v1 > v2) std::swap(v1, v2);
+            std::vector<long> r(4);
+            r[0] = v1;
+            r[1] = v2;
+            r[2] = f;
+            r[3] = i;
+            TTT[f] = r;
+            FV(f, i) = v1;
+        }
+    }
+    std::sort(TTT.begin(), TTT.end());
+
+    // iterate over TTT to initialize topology
+    // assumption is the same edge is always next to each other in the sorted TTT
+    int unique_edges = 0;
+    long v01 = TTT[0][0];
+    long v02 = TTT[0][1];
+    long f0 = TTT[0][2];
+    long e0 = TTT[0][3];
+    FE(f0, e0) = unique_edges;
+    VF(v01, 0) = f0;
+    VF(v02, 0) = f0;
+    EF(unique_edges, 0) = f0;
+
+    for (int i = 1; i < TTT.size(); ++i) {
+        int va1 = TTT[i][0];
+        int va2 = TTT[i][1];
+        int fa = TTT[i][2];
+        int eia = TTT[i][3];
+
+        int vb1 = TTT[i - 1][0];
+        int vb2 = TTT[i - 1][1];
+        int fb = TTT[i - 1][2];
+        int eib = TTT[i - 1][3];
+        if (va1 == vb1 & va2 == vb2) {
+            // same edge
+            FF(fa, eia) = fb;
+            FF(fb, eib) = fa;
+            continue;
+        } else {
+            unique_edges++;
+            FE(fa, eia) = unique_edges;
+            VF(va1, 0) = fa;
+            VF(va2, 0) = fa;
+            EF(unique_edges, 0) = fa;
+            FF(fa, eia) = -1;
+        }
+    }
+}
+
+void tetmesh_topology_initialization(
+    Eigen::Ref<const RowVectors3d>& V,
+    Eigen::Ref<const RowVectors4l>& F,
+    TetMesh& mesh)
+{}
 } // namespace wmtk
--- src/wmtk/Mesh.hpp
+++ src/wmtk/Mesh.hpp
@@ -6,14 +6,15 @@
 
 
 namespace wmtk {
-using Matl3 = Eigen::Matrix<long, Eigen::Dynamic, 3>;
-using Matl1 = Eigen::Matrix<long, Eigen::Dynamic, 1>;
-using Matl4 = Eigen::Matrix<long, Eigen::Dynamic, 4>;
 
 
 class Mesh
 {
 public:
+    using RowVectors3l = Eigen::Matrix<long, Eigen::Dynamic, 3>;
+    using VectorXl = Eigen::Matrix<long, Eigen::Dynamic, 1>;
+    using RowVectors4l = Eigen::Matrix<long, Eigen::Dynamic, 4>;
+    using RowVectors3d = Eigen::Matrix<double, Eigen::Dynamic, 3>;
     template <typename T>
     friend class Accessor;
     Mesh();
@@ -212,12 +213,12 @@ public:
     Tuple switch_tuple(const Tuple& tuple, const PrimitiveType& type) const override;
     bool is_ccw(const Tuple& tuple) const override;
     void initialize(
-        Eigen::Ref<const Matl3>& FV,
-        Eigen::Ref<const Matl3>& FE,
-        Eigen::Ref<const Matl3>& FF,
-        Eigen::Ref<const Matl1>& VF,
-        Eigen::Ref<const Matl1>& EF,
-        Eigen::Ref<const Matl4>& seam) const;
+        Eigen::Ref<const RowVectors3l>& FV,
+        Eigen::Ref<const RowVectors3l>& FE,
+        Eigen::Ref<const RowVectors3l>& FF,
+        Eigen::Ref<const VectorXl>& VF,
+        Eigen::Ref<const VectorXl>& EF,
+        Eigen::Ref<const RowVectors4l>& seam) const;
 };
 
 class TetMesh : public Mesh
@@ -239,13 +240,13 @@ public:
     Tuple switch_tuple(const Tuple& tuple, const PrimitiveType& type) const override;
     bool is_ccw(const Tuple& tuple) const override;
     void initialize(
-        Eigen::Ref<const Matl4>& TV,
-        Eigen::Ref<const Matl4>& TE,
-        Eigen::Ref<const Matl4>& TF,
-        Eigen::Ref<const Matl4>& TT,
-        Eigen::Ref<const Matl1>& VT,
-        Eigen::Ref<const Matl1>& ET,
-        Eigen::Ref<const Matl1>& FT) const;
+        Eigen::Ref<const RowVectors4l>& TV,
+        Eigen::Ref<const RowVectors4l>& TE,
+        Eigen::Ref<const RowVectors4l>& TF,
+        Eigen::Ref<const RowVectors4l>& TT,
+        Eigen::Ref<const VectorXl>& VT,
+        Eigen::Ref<const VectorXl>& ET,
+        Eigen::Ref<const VectorXl>& FT) const;
 };
 
 template <typename T>
@@ -270,77 +271,14 @@ Mesh::register_attribute_with_accessor(const std::string& name, PrimitiveType pt
  */
 
 void trimesh_topology_initialization(
-    Eigen::Ref<const MaxtrixXi>& F,
-    Eigen::Ref<const Matl3>& FE,
-    Eigen::Ref<const Matl3>& FF,
-    Eigen::Ref<const Matl1>& VF,
-    Eigen::Ref<const Matl1>& EF)
-{
-    std::vector<std::vector<long>> TTT;
-    FV.resize(F.rows(), F.cols());
-    FE.resize(F.rows(), F.cols());
-    FF.resize(F.rows(), F.cols());
-    VF.resize(F.rows(), 1);
-    EF.resize(F.rows(), 1);
-    TTT.resize(F.rows(), std::vector<long>(4));
-    for (int f = 0; f < F.rows(); ++f) {
-        for (int i = 0; i < F.cols(); ++i) {
-            // v1 v2 f ei
-            long v1 = std::static_cast<long>(F(f, i));
-            long v2 = std::static_cast<long>(F(f, (i + 1) % F.cols()));
-            if (v1 > v2) std::swap(v1, v2);
-            std::vector<long> r(4);
-            r[0] = v1;
-            r[1] = v2;
-            r[2] = f;
-            r[3] = i;
-            TTT[f] = r;
-            FV(f, i) = v1;
-        }
-    }
-    std::sort(TTT.begin(), TTT.end());
-
-    // iterate over TTT to initialize topology
-    // assumption is the same edge is always next to each other in the sorted TTT
-    int unique_edges = 0;
-    long v01 = TTT[0][0];
-    long v02 = TTT[0][1];
-    long f0 = TTT[0][2];
-    long e0 = TTT[0][3];
-    FE(f0, e0) = unique_edges;
-    VF(v01, 0) = f0;
-    VF(v02, 0) = f0;
-    EF(unique_edges, 0) = f0;
-
-    for (int i = 1; i < TTT.size(); ++i) {
-        int va1 = TTT[i][0];
-        int va2 = TTT[i][1];
-        int fa = TTT[i][2];
-        int eia = TTT[i][3];
-
-        int vb1 = TTT[i - 1][0];
-        int vb2 = TTT[i - 1][1];
-        int fb = TTT[i - 1][2];
-        int eib = TTT[i - 1][3];
-        if (va1 == vb1 & va2 == vb2) {
-            // same edge
-            FF(fa, eia) = fb;
-            FF(fb, eib) = fa;
-            continue;
-        } else {
-            unique_edges++;
-            FE(fa, eia) = unique_edges;
-            VF(va1, 0) = fa;
-            VF(va2, 0) = fa;
-            EF(unique_edges, 0) = fa;
-            FF(fa, eia) = -1;
-        }
-    }
-}
+    Eigen::Ref<const RowVectors3l>& F,
+    Eigen::Ref<const RowVectors3l>& FE,
+    Eigen::Ref<const RowVectors3l>& FF,
+    Eigen::Ref<const VectorXl>& VF,
+    Eigen::Ref<const VectorXl>& EF);
 
 void tetmesh_topology_initialization(
-    const Eigen::MatrixXd& V,
-    const Eigen::MatrixXi& F,
-    const TetMesh& mesh)
-{}
+    Eigen::Ref<const RowVectors3d>& V,
+    Eigen::Ref<const RowVectors4l>& F,
+    TetMesh& mesh);
 } // namespace wmtk
