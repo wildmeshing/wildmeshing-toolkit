35f2dfa87c62a8c1809994959c77a1910d5bbe4e
Teseo Schneider <teseo@bluewin.ch>
2023-06-24 09:33:51 -0700

    tetmesh

--- src/wmtk/utils/tetmesh_topology_initialization.h
+++ src/wmtk/utils/tetmesh_topology_initialization.h
@@ -7,10 +7,11 @@ namespace wmtk {
 
 
 /**
- * @brief Given the mesh connectivity in matrix format, finds unique edges and faces and their relations
+ * @brief Given the mesh connectivity in matrix format, finds unique edges and faces and their
+ * relations
  */
 
-std::tuple<RowVectors6l,RowVectors4l,RowVectors4l,VectorXl,VectorXl,VectorXl> tetmesh_topology_initialization(
-    Eigen::Ref<const RowVectors4l> T);
-    
-}
+std::tuple<RowVectors6l, RowVectors4l, RowVectors4l, VectorXl, VectorXl, VectorXl>
+tetmesh_topology_initialization(Eigen::Ref<const RowVectors4l> T);
+// returns TE, TF, TT, VT, ET, FT
+} // namespace wmtk
