5aafc6a3081a02818c1376a62bc14586c6673857
Michael Tao <michael.tao@nyu.edu>
2023-06-24 12:42:01 -0400

    clang-format pass

--- src/wmtk/Accessor.cpp
+++ src/wmtk/Accessor.cpp
@@ -6,15 +6,15 @@
 namespace wmtk {
 
 template <typename T, bool IsConst>
-Accessor<T,IsConst>::Accessor(MeshType& mesh, const MeshAttributeHandle<T>& handle)
+Accessor<T, IsConst>::Accessor(MeshType& mesh, const MeshAttributeHandle<T>& handle)
     : m_mesh(mesh)
     , m_handle(handle)
 {}
 
 template <typename T, bool IsConst>
-auto Accessor<T,IsConst>::attributes() -> MeshAttributes<T>& 
+auto Accessor<T, IsConst>::attributes() -> MeshAttributes<T>&
 {
-    if constexpr(!IsConst) {
+    if constexpr (!IsConst) {
         return m_mesh.get_mesh_attributes(m_handle);
     } else {
         assert(false);
@@ -23,55 +23,55 @@ auto Accessor<T,IsConst>::attributes() -> MeshAttributes<T>&
     }
 }
 template <typename T, bool IsConst>
-auto Accessor<T,IsConst>::attributes() const-> const MeshAttributes<T>& 
+auto Accessor<T, IsConst>::attributes() const -> const MeshAttributes<T>&
 {
     return m_mesh.get_mesh_attributes(m_handle);
 }
 
 template <typename T, bool IsConst>
-auto Accessor<T,IsConst>::vector_attribute(const long index) const -> ConstMapResult
+auto Accessor<T, IsConst>::vector_attribute(const long index) const -> ConstMapResult
 {
     return attributes().vector_attribute(m_handle.m_base_handle, index);
 }
 template <typename T, bool IsConst>
-auto Accessor<T,IsConst>::vector_attribute(const long index) -> MapResult
+auto Accessor<T, IsConst>::vector_attribute(const long index) -> MapResult
 {
     return attributes().vector_attribute(m_handle.m_base_handle, index);
 }
 
 template <typename T, bool IsConst>
-T Accessor<T,IsConst>::scalar_attribute(const long index) const
+T Accessor<T, IsConst>::scalar_attribute(const long index) const
 {
     return attributes().scalar_attribute(m_handle.m_base_handle, index);
 }
 
 template <typename T, bool IsConst>
-T& Accessor<T,IsConst>::scalar_attribute(const long index)
+T& Accessor<T, IsConst>::scalar_attribute(const long index)
 {
     return attributes().scalar_attribute(m_handle.m_base_handle, index);
 }
 template <typename T, bool IsConst>
-auto Accessor<T,IsConst>::vector_attribute(const Tuple& t) const -> ConstMapResult
+auto Accessor<T, IsConst>::vector_attribute(const Tuple& t) const -> ConstMapResult
 {
     long index = m_mesh.id(t, m_handle.m_primitive_type);
     return vector_attribute(index);
 }
 template <typename T, bool IsConst>
-auto Accessor<T,IsConst>::vector_attribute(const Tuple& t) -> MapResult
+auto Accessor<T, IsConst>::vector_attribute(const Tuple& t) -> MapResult
 {
     long index = m_mesh.id(t, m_handle.m_primitive_type);
     return vector_attribute(index);
 }
 
 template <typename T, bool IsConst>
-T Accessor<T,IsConst>::scalar_attribute(const Tuple& t) const
+T Accessor<T, IsConst>::scalar_attribute(const Tuple& t) const
 {
     long index = m_mesh.id(t, m_handle.m_primitive_type);
     return scalar_attribute(index);
 }
 
 template <typename T, bool IsConst>
-T& Accessor<T,IsConst>::scalar_attribute(const Tuple& t)
+T& Accessor<T, IsConst>::scalar_attribute(const Tuple& t)
 {
     long index = m_mesh.id(t, m_handle.m_primitive_type);
     return scalar_attribute(index);
@@ -104,10 +104,10 @@ T& Accessor<T,IsConst>::scalar_attribute(const Tuple& t)
 //{
 //     return !attributes()s_copy.empty();
 // }
-template class Accessor<char,true>;
-template class Accessor<long,true>;
-template class Accessor<double,true>;
-template class Accessor<char,false>;
-template class Accessor<long,false>;
-template class Accessor<double,false>;
+template class Accessor<char, true>;
+template class Accessor<long, true>;
+template class Accessor<double, true>;
+template class Accessor<char, false>;
+template class Accessor<long, false>;
+template class Accessor<double, false>;
 } // namespace wmtk
