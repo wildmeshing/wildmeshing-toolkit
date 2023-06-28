4197b67d9c36eb7f526a066793120798f3ec9e51
zlyfunction <zhuleyi001@126.com>
2023-06-28 16:47:06 -0400

    reorganize collapse/merge

--- src/wmtk/TriMeshOperation.cpp
+++ src/wmtk/TriMeshOperation.cpp
@@ -4,34 +4,57 @@
 #include "SimplicialComplex.hpp"
 namespace wmtk
 {
+namespace {
+    constexpr PrimitiveType PV = PrimitiveType::Vertex;
+    constexpr PrimitiveType PE = PrimitiveType::Edge;
+    constexpr PrimitiveType PF = PrimitiveType::Face;
+}
 class TriMesh::TriMeshOperationState
 {
     public:
     TriMeshOperationState(TriMesh &m, const Tuple &operating_tuple);
-    void delete_simplices(const std::vector<Simplex>& simplicess);
+    void delete_simplices();
+    
+    void merge();
 
     std::array<Accessor<char>, 3> flag_accessors;
-
     Accessor<long> ff_accessor;
     Accessor<long> fe_accessor;
     Accessor<long> fv_accessor;
     Accessor<long> vf_accessor;
     Accessor<long> ef_accessor;
 
-    Tuple edge_tuple;
 
+    //           C
     //         /  \ 
-    //      A /    \ B
+    //    F1  /    \  F2 
     //       /      \ 
     //      /        \ 
-    //      ----------
+    //     A----------B
     //      \        /
     //       \      /
-    //      C \    / D
+    //    F1' \    / F2'
     //         \  /
+    //          C'
 
-    // the neighbors are stored in the order of A, B, C, D if they exist
-    std::vector<std::array<std::optional<Tuple>, 2>> edge_neighbors;
+    struct SimplexSet
+    {
+        long V_C_id;
+        long E_AB_id;
+        long E_BC_id;
+        long E_AC_id;
+        long F_ABC_id;
+        long F1_id;
+        long F2_id;
+    };
+
+    // common simplicies
+    Simplex V_A, V_B, E_AB;
+    long V_A_id, V_B_id, E_AB_id;
+    // other simplicies    
+    std::vector<SimplexSet> SimplexSets;
+
+    std::vector<std::vector<long>> simplices_to_delete; // size 3 for vertex, edge, face
     TriMesh &m_mesh;
 };
 
@@ -45,132 +68,131 @@ TriMesh::TriMeshOperationState::TriMeshOperationState(TriMesh &m, const Tuple &o
     , fv_accessor(m.create_accessor<long>(m.m_fv_handle))
     , vf_accessor(m.create_accessor<long>(m.m_ef_handle))
     , ef_accessor(m.create_accessor<long>(m.m_vf_handle))
-    , edge_tuple(operating_tuple)
     , m_mesh(m)
+    , V_A(PV, operating_tuple)
+    , V_B(PV, m.switch_tuple(operating_tuple, PV))
+    , E_AB(PE, operating_tuple)
 {
-    auto get_ears = [&](Tuple e_tuple) -> std::array<std::optional<Tuple>, 2> {
-        std::array<std::optional<Tuple>, 2> ears;
-        Tuple t1_edge = m_mesh.switch_tuple(e_tuple, PrimitiveType::Edge);
-        Tuple t2_edge = m_mesh.switch_tuple(e_tuple, PrimitiveType::Vertex);
-        t2_edge = m_mesh.switch_tuple(t2_edge, PrimitiveType::Edge);
+    V_A_id = m_mesh.id(V_A.tuple(), PV);
+    V_B_id = m_mesh.id(V_B.tuple(), PV);
+    E_AB_id = m_mesh.id(E_AB.tuple(), PE);
+    simplices_to_delete.resize(3);
 
-        ears[0] = m_mesh.is_boundary(t1_edge)
-                      ? std::nullopt
-                      : std::optional<Tuple>(m_mesh.switch_tuple(t1_edge, PrimitiveType::Face));
-        ears[1] = m_mesh.is_boundary(t2_edge)
-                      ? std::nullopt
-                      : std::optional<Tuple>(m_mesh.switch_tuple(t2_edge, PrimitiveType::Face));
-        return ears;
+    auto sw = [this](const Tuple& tt, const PrimitiveType& ptype) { return this->m_mesh.switch_tuple(tt, ptype); };
+
+    auto get_simplexset = [&](Tuple he) -> SimplexSet {
+        SimplexSet ret;
+        Simplex V_C(PV, sw(sw(he, PE), PV));
+        Simplex E_AB(PE, he), E_BC(PE, sw(sw(he, PV), PE)), E_AC(PE, sw(he, PE));
+        Simplex FA(PF, he);
+        
+        ret.V_C_id = m_mesh.id(V_C.tuple(), PV);
+        ret.E_AB_id = m_mesh.id(E_AB.tuple(), PE);
+        ret.E_BC_id = m_mesh.id(E_BC.tuple(), PE);
+        ret.E_AC_id = m_mesh.id(E_AC.tuple(), PE);
+        ret.F_ABC_id = m_mesh.id(FA.tuple(), PF);
+
+        for (long j = 0; j < 3; j++)
+        {
+            if (fe_accessor.vector_attribute(ret.F_ABC_id)[j] == ret.E_AC_id)
+            {
+                ret.F1_id = ff_accessor.vector_attribute(ret.F_ABC_id)[j];
+            }
+            if (fe_accessor.vector_attribute(ret.F_ABC_id)[j] == ret.E_BC_id)
+            {
+                ret.F2_id = ff_accessor.vector_attribute(ret.F_ABC_id)[j];
+            }
+        }
+        return ret;
     };
-    edge_neighbors.emplace_back(get_ears(edge_tuple));
-
-    if (!m_mesh.is_boundary(edge_tuple)) {
-        Tuple other_face = m_mesh.switch_tuple(edge_tuple, PrimitiveType::Face);
-        edge_neighbors.emplace_back(get_ears(other_face));
+ 
+    SimplexSets.emplace_back(get_simplexset(operating_tuple));
+    if (!m_mesh.is_boundary(operating_tuple))
+    {
+        SimplexSets.emplace_back(get_simplexset(sw(operating_tuple, PF)));
     }
 };
 
-void TriMesh::TriMeshOperationState::delete_simplices(
-    const std::vector<wmtk::Simplex>& simplices)
+void TriMesh::TriMeshOperationState::delete_simplices()
 {
-    for (const Simplex& simplex : simplices) {
-        flag_accessors[simplex.dimension()].scalar_attribute(simplex.tuple()) = 0;
+    for (int d = 0; d < 3; d++)
+    {
+        for (long& simplex_id : simplices_to_delete[d]) {
+            flag_accessors[d].scalar_attribute(simplex_id) = 0;
+        }
     }
 }
 
-void TriMesh::collapse_edge(const Tuple& t)
+
+void TriMesh::TriMeshOperationState::merge() 
 {
-    // TODO: check link_cond before collapse
-
-    constexpr PrimitiveType PV = PrimitiveType::Vertex;
-    constexpr PrimitiveType PE = PrimitiveType::Edge;
-    constexpr PrimitiveType PF = PrimitiveType::Face;
-    auto sw = [this](const Tuple& tt, const PrimitiveType& ptype) { return this->switch_tuple(tt, ptype); };
-    
-    bool is_t_boudanry = is_boundary(t);
-    TriMeshOperationState state(*this, t);
-
-    Simplex V_A(PV, t), V_B(PV, sw(t, PV));
-    long V_A_id = id(V_A.tuple(), PV);
-    long V_B_id = id(V_B.tuple(), PV);
-    // get faces in open_star(B)
-    auto star_B_f = SimplicialComplex::open_star(V_B, *this).get_simplices(PF);
-    std::vector<Simplex> faces_in_open_star_B = std::vector<Simplex>(star_B_f.begin(), star_B_f.end());
-    std::vector<long> faces_in_open_star_B_id;
-    for (const Simplex& simplex : faces_in_open_star_B) 
+    simplices_to_delete[0].push_back(V_B_id);
+    simplices_to_delete[1].push_back(E_AB_id);
+    for (int set_ind = 0; set_ind < SimplexSets.size(); set_ind++)
     {
-        faces_in_open_star_B_id.push_back(id(simplex.tuple(), PF));
-    }
-
-    std::vector<Simplex> simplices_to_delete;
-    auto merge_he = [&](const Tuple &input_he, int ear_ind) {
-        Simplex V_C(PV, sw(sw(input_he, PE), PV));
-        Simplex E_AB(PE, input_he), E_BC(PE, sw(sw(input_he, PV), PE)), E_AC(PE, sw(input_he, PE));
-        Simplex FA(PF, input_he);
-        
-        long V_C_id = id(V_C.tuple(), PV);
-        long E_AB_id = id(E_AB.tuple(), PE);
-        long E_BC_id = id(E_BC.tuple(), PE);
-        long E_AC_id = id(E_AC.tuple(), PE);
-        long FA_id = id(FA.tuple(), PF);
-        long f1_id = state.edge_neighbors[ear_ind][0].has_value() ? id(state.edge_neighbors[ear_ind][0].value(), PF) : -1;
-        long f2_id = state.edge_neighbors[ear_ind][1].has_value() ? id(state.edge_neighbors[ear_ind][1].value(), PF) : -1;
-        if (f1_id == -1 && f2_id == -1)
+        if (SimplexSets[set_ind].F1_id == -1 && SimplexSets[set_ind].F2_id == -1)
         {
             return; // TODO: throw exception
         }
 
         // change VF for V_A,V_C
-        state.vf_accessor.scalar_attribute(V_A_id) = (f1_id == -1) ? f2_id : f1_id;
-        state.vf_accessor.scalar_attribute(V_C_id) = state.vf_accessor.scalar_attribute(V_A_id);
+        vf_accessor.scalar_attribute(V_A_id) = (SimplexSets[set_ind].F1_id == -1) ? SimplexSets[set_ind].F2_id : SimplexSets[set_ind].F1_id;
+        vf_accessor.scalar_attribute(SimplexSets[set_ind].V_C_id) = vf_accessor.scalar_attribute(V_A_id);
         // change EF for E_AC
-        state.ef_accessor.scalar_attribute(E_AC_id) = state.vf_accessor.scalar_attribute(V_A_id);
-        
+        ef_accessor.scalar_attribute(SimplexSets[set_ind].E_AC_id) = vf_accessor.scalar_attribute(V_A_id);
         // change FF and FE for  F1,F2
-        auto change_ff_fe = [this, &state, &E_AC_id, &FA_id](long fid_one, long fid_another) {
+        auto change_ff_fe = [set_ind, this](long fid_one, long fid_other) {
             if (fid_one != -1)
             {
                 long index = 0;
-                while (index < 2 && state.ff_accessor.vector_attribute(fid_one)[index] != FA_id)
+                while (index < 2 && this->ff_accessor.vector_attribute(fid_one)[index] != this->SimplexSets[set_ind].F_ABC_id)
                 {
                     index++;
                 }
-                assert(state.ff_accessor.vector_attribute(fid_one)[index] == FA_id); // assert find FA in FF(f1)
-                state.ff_accessor.vector_attribute(fid_one)[index] = fid_another;
-                state.fe_accessor.vector_attribute(fid_one)[index] = E_AC_id;
+                assert(this->ff_accessor.vector_attribute(fid_one)[index] == this->SimplexSets[set_ind].F_ABC_id); // assert find F_ABC in FF(f_one)
+                this->ff_accessor.vector_attribute(fid_one)[index] = fid_other;
+                this->fe_accessor.vector_attribute(fid_one)[index] = this->SimplexSets[set_ind].E_AC_id;
             }
         };
-        change_ff_fe(f1_id, f2_id);
-        change_ff_fe(f2_id, f1_id);
+        change_ff_fe(SimplexSets[set_ind].F1_id, SimplexSets[set_ind].F2_id);
+        change_ff_fe(SimplexSets[set_ind].F2_id, SimplexSets[set_ind].F1_id);
 
-        simplices_to_delete.push_back(V_B);
-        simplices_to_delete.push_back(E_BC);
-        simplices_to_delete.push_back(E_AB);
-        simplices_to_delete.push_back(FA);
-    };
-    
-    // merge
-    merge_he(t, 0);
-    if (!is_t_boudanry)
-    {
-        merge_he(sw(t, PF), 1);
+        simplices_to_delete[1].push_back(SimplexSets[set_ind].E_BC_id);
+        simplices_to_delete[2].push_back(SimplexSets[set_ind].F_ABC_id);
     }
+};
+
+void TriMesh::collapse_edge(const Tuple& t)
+{
+    // TODO: check link_cond before collapse
+    TriMeshOperationState state(*this, t);
+
+    // get faces in open_star(B)
+    auto star_B_f = SimplicialComplex::open_star(state.V_B, *this).get_simplices(PF);
+    std::vector<long> faces_in_open_star_B_id;
+    for (const Simplex& simplex : star_B_f) 
+    {
+        faces_in_open_star_B_id.push_back(id(simplex.tuple(), PF));
+    }
+
+    // merge
+    state.merge();
 
     // change FV for open_star_faces(V_B)
     for (long &f : faces_in_open_star_B_id)
     {
         for (long index = 0; index < 3; index++)
         {
-            if (state.fv_accessor.vector_attribute(f)[index] == V_B_id)
+            if (state.fv_accessor.vector_attribute(f)[index] == state.V_B_id)
             {
-                state.fv_accessor.vector_attribute(f)[index] = V_A_id;
+                state.fv_accessor.vector_attribute(f)[index] = state.V_A_id;
                 break;
             }
         }
     }
 
     // delete simplices
-    state.delete_simplices(simplices_to_delete);
+    state.delete_simplices();
 }
 
 } // namespace wmtk
