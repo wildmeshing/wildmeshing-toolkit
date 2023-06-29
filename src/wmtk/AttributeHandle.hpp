efeaf61d81016591f82bf9032e1e6308a3d0baef
Michael Tao <michael.tao@nyu.edu>
2023-06-28 22:17:08 -0400

    cleaned up mesh attribute handle visibility

--- src/wmtk/AttributeHandle.hpp
+++ src/wmtk/AttributeHandle.hpp
@@ -1,13 +1,20 @@
 #pragma once
 #include "Primitive.hpp"
 namespace wmtk {
+template <typename T>
+class MeshAttributes;
+template <typename T, bool IsConst>
+class Accessor;
 
-struct AttributeHandle
+class AttributeHandle
 {
-public:
+protected:
+    template <typename T>
+    friend class MeshAttributes;
     long index = -1;
     long stride = -1;
 
+public:
     bool valid() const { return index >= 0 && stride >= 0; }
 
     bool operator==(const AttributeHandle& other) const
@@ -17,11 +24,17 @@ public:
 };
 
 template <typename T>
-struct MeshAttributeHandle
+class MeshAttributeHandle
 {
+private:
+    friend class Mesh;
+    friend class MeshAttributes<T>;
+    template <typename U, bool IsConst>
+    friend class Accessor;
     AttributeHandle m_base_handle;
     PrimitiveType m_primitive_type = PrimitiveType::Invalid;
 
+public:
     bool valid() const
     {
         return m_base_handle.valid() && m_primitive_type != PrimitiveType::Invalid;
