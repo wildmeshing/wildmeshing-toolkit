1bc28fc9dceb5ed053ff7f20b6911797a09f6eaf
zlyfunction <zhuleyi001@126.com>
2023-07-26 15:43:00 -0400

    use correct link-cond with boundary support in 2d

--- src/wmtk/operations/TriMeshCollapseEdgeOperation.cpp
+++ src/wmtk/operations/TriMeshCollapseEdgeOperation.cpp
@@ -21,7 +21,7 @@ bool TriMeshCollapseEdgeOperation::before() const
     if(!m_mesh.is_valid(m_input_tuple)) {
         return false;
     }
-    return SimplicialComplex::link_cond(m_input_tuple, m_mesh);
+    return SimplicialComplex::link_cond_bd_2d(m_input_tuple, m_mesh);
 }
 
 std::string TriMeshCollapseEdgeOperation::name() const
