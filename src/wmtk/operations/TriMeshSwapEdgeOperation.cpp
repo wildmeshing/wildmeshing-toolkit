67f46d767f4a3d5622221456cc5caec97c4a8c1a
Daniel Zint <daniel.zint@mail.de>
2023-08-07 15:45:38 +0200

    check if swap is outdated

--- src/wmtk/operations/TriMeshSwapEdgeOperation.cpp
+++ src/wmtk/operations/TriMeshSwapEdgeOperation.cpp
@@ -16,6 +16,9 @@ std::string TriMeshSwapEdgeOperation::name() const
 
 bool TriMeshSwapEdgeOperation::before() const
 {
+    if (m_mesh.is_outdated(m_input_tuple)) {
+        return false;
+    }
     if (m_mesh.is_boundary(m_input_tuple)) {
         return false;
     }
@@ -102,34 +105,6 @@ bool TriMeshSwapEdgeOperation::execute()
     m_output_tuple = m_mesh.switch_vertex(m_mesh.switch_edge(coll_ret));
 
     return true;
-
-    // bool is_ccw_input = m_mesh.is_ccw(m_input_tuple);
-    // Tuple split_result;
-    // TriMeshSplitEdgeOperation split(m_mesh, m_input_tuple);
-    // if (!split()) {
-    //     return false;
-    // }
-    // split_result = split.return_tuple();
-    //
-    //// TODO: navigate collapse
-    // Tuple collapse_input = m_mesh.switch_tuple(
-    //     m_mesh.switch_tuple(
-    //         m_mesh.switch_tuple(split_result, PrimitiveType::Vertex),
-    //         PrimitiveType::Edge),
-    //     PrimitiveType::Face);
-    // TriMeshCollapseEdgeOperation collapse(m_mesh, collapse_input);
-    // if (!collapse()) {
-    //     return false;
-    // }
-    // m_output_tuple = collapse.return_tuple();
-    // if (!is_ccw_input) {
-    //     m_output_tuple = m_mesh.switch_tuple(m_output_tuple, PrimitiveType::Edge);
-    // } else {
-    //     m_output_tuple = m_mesh.switch_tuple(
-    //         m_mesh.switch_tuple(m_output_tuple, PrimitiveType::Face),
-    //         PrimitiveType::Vertex);
-    // }
-    // return true;
 }
 
 
