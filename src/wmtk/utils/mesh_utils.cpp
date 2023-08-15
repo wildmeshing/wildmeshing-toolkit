cf7e362673deba0b8d00c915ae342bd723a74998
Daniel Zint <daniel.zint@mail.de>
2023-08-15 10:22:11 +0200

    split mesh_utils in header and cpp file

---
+++ src/wmtk/utils/mesh_utils.cpp
@@ -0,0 +1,38 @@
+#include "mesh_utils.hpp"
+
+namespace wmtk::mesh_utils {
+Eigen::Vector3d
+compute_face_normal_area_weighted(const Mesh& m, const Accessor<double>& pos, const Tuple& f)
+{
+    const Tuple v0 = f;
+    const Tuple v1 = m.switch_vertex(f);
+    const Tuple v2 = m.switch_vertex(m.switch_edge(f));
+
+    const Eigen::Vector3d p0 = pos.vector_attribute(v0);
+    const Eigen::Vector3d p1 = pos.vector_attribute(v1);
+    const Eigen::Vector3d p2 = pos.vector_attribute(v2);
+    return ((p0 - p2).cross(p1 - p2));
+}
+
+Eigen::Vector3d compute_face_normal(const Mesh& m, const Accessor<double>& pos, const Tuple& f)
+{
+    return compute_face_normal_area_weighted(m, pos, f).normalized();
+}
+
+Eigen::Vector3d compute_vertex_normal(const Mesh& m, const Accessor<double>& pos, const Tuple& v)
+{
+    const SimplicialComplex closed_star = SimplicialComplex::closed_star(m, Simplex::vertex(v));
+
+    Eigen::Vector3d n = Eigen::Vector3d::Zero();
+    for (const Simplex& f : closed_star.get_faces()) {
+        Tuple t = f.tuple();
+        if (!m.is_ccw(t)) {
+            t = m.switch_vertex(t);
+        }
+        n += compute_face_normal_area_weighted(m, pos, t);
+    }
+    n.normalize();
+    return n;
+}
+
+} // namespace wmtk::mesh_utils
--- src/wmtk/utils/mesh_utils.hpp
+++ src/wmtk/utils/mesh_utils.hpp
@@ -26,40 +26,20 @@ inline MeshAttributeHandle<typename Mat::Scalar> set_matrix_attribute(
     return handle;
 }
 
-inline Eigen::Vector3d
-compute_face_normal_area_weighted(const Mesh& m, const Accessor<double>& pos, const Tuple& f)
-{
-    const Tuple v0 = f;
-    const Tuple v1 = m.switch_vertex(f);
-    const Tuple v2 = m.switch_vertex(m.switch_edge(f));
+/**
+ * @brief compute area vector of face
+ */
+Eigen::Vector3d
+compute_face_normal_area_weighted(const Mesh& m, const Accessor<double>& pos, const Tuple& f);
 
-    const Eigen::Vector3d p0 = pos.vector_attribute(v0);
-    const Eigen::Vector3d p1 = pos.vector_attribute(v1);
-    const Eigen::Vector3d p2 = pos.vector_attribute(v2);
-    return ((p0 - p2).cross(p1 - p2));
-}
+/**
+ * @brief compute the normalized face normal based on the vertex positions
+ */
+Eigen::Vector3d compute_face_normal(const Mesh& m, const Accessor<double>& pos, const Tuple& f);
 
-inline Eigen::Vector3d
-compute_face_normal(const Mesh& m, const Accessor<double>& pos, const Tuple& f)
-{
-    return compute_face_normal_area_weighted(m, pos, f).normalized();
-}
-
-inline Eigen::Vector3d
-compute_vertex_normal(const Mesh& m, const Accessor<double>& pos, const Tuple& v)
-{
-    const SimplicialComplex closed_star = SimplicialComplex::closed_star(m, Simplex::vertex(v));
-
-    Eigen::Vector3d n = Eigen::Vector3d::Zero();
-    for (const Simplex& f : closed_star.get_faces()) {
-        Tuple t = f.tuple();
-        if (!m.is_ccw(t)) {
-            t = m.switch_vertex(t);
-        }
-        n += compute_face_normal_area_weighted(m, pos, t);
-    }
-    n.normalize();
-    return n;
-}
+/**
+ * @brief compute the normalized vertex normal from the incident area weighted face normals
+ */
+Eigen::Vector3d compute_vertex_normal(const Mesh& m, const Accessor<double>& pos, const Tuple& v);
 
 } // namespace wmtk::mesh_utils
