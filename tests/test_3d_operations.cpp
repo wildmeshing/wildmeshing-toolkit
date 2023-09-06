41ff2ba4cd9a2c0d49ea148bf6346b92668680ab
JcDai <jiacheng_dai@qq.com>
2023-09-06 15:10:25 -0400

    draft 3d operation tests

---
+++ tests/test_3d_operations.cpp
@@ -0,0 +1,41 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <numeric>
+#include <wmtk/Accessor.hpp>
+#include <wmtk/TetMeshOperationExecutor.hpp>
+#include <wmtk/operations/OperationFactory.hpp>
+
+#include <wmtk/utils/Logger.hpp>
+#include "tools/DEBUG_TetMesh.hpp"
+#include "tools/TetMesh_examples.hpp"
+
+using namespace wmtk;
+using namespace wmtk::tests_3d;
+
+using TM = TetMesh;
+using MapResult = typename Eigen::Matrix<long, Eigen::Dynamic, 1>::MapType;
+using TMOE = decltype(std::declval<DEBUG_TetMesh>().get_tmoe(
+    wmtk::Tuple(),
+    std::declval<Accessor<long>&>()));
+
+constexpr PrimitiveType PV = PrimitiveType::Vertex;
+constexpr PrimitiveType PE = PrimitiveType::Edge;
+constexpr PrimitiveType PF = PrimitiveType::Face;
+constexpr PrimitiveType PT = PrimitiveType::Tetrahedron;
+
+TEST_CASE("get_split_simplices_to_delete", "[operations][split][3d]")
+{
+    SECTION("single_tet")
+    {
+        const DEBUG_TetMesh m = single_tet();
+        const Tuple edge = m.edge_tuple_between_v1_v2(1, 2, 0);
+
+        std::array<std::vector<long>, 4> ids_to_delete =
+            TMOE::get_split_simplices_to_delete(edge, m);
+
+        REQUIRE(ids_to_delete[0].size() == 0);
+        REQUIRE(ids_to_delete[1].size() == 1);
+        REQUIRE(ids_to_delete[2].size() == 2);
+        REQUIRE(ids_to_delete[3].size() == 1);
+    }
+}
