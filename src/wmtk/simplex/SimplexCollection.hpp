e67c69c5495533d4cb9563421bdecbaf6eb00da4
Daniel Zint <daniel.zint@mail.de>
2023-09-13 12:26:28 -0400

    some function declarations for the simplex collection

--- src/wmtk/simplex/SimplexCollection.hpp
+++ src/wmtk/simplex/SimplexCollection.hpp
@@ -1,7 +1,38 @@
 #pragma once
 
+#include <vector>
+#include "Simplex.hpp"
+
 namespace wmtk::simplex {
 class SimplexCollection
 {
+public:
+    /**
+     * @brief Return const reference to the simplex vector.
+     */
+    const std::vector<Simplex>& simplex_vector() const { return m_simplices; }
+    /**
+     * @brief Return vector of all simplices of the requested type.
+     */
+    std::vector<Simplex> simplex_vector(const PrimitiveType& ptype) const;
+
+    void add(const Simplex& simplex);
+
+    /**
+     * @brief Sort simplex vector and remove duplicates.
+     */
+    void sort_and_clean();
+
+    static SimplexCollection get_union(
+        const SimplexCollection& collection_a,
+        const SimplexCollection& collection_b);
+    // useful: std::sort, std::set_union/difference/intersection
+
+    static SimplexCollection get_intersection(
+        const SimplexCollection& collection_a,
+        const SimplexCollection& collection_b);
+
+protected:
+    std::vector<Simplex> m_simplices;
 };
 } // namespace wmtk::simplex
