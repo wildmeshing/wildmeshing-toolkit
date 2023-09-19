e0288271ed4c89f1173d9b130750cb1b08c97509
yunfanzhou <yfzhou42@gmail.com>
2023-09-19 11:17:13 -0400

    add triangle inversion invariants

--- src/wmtk/invariants/TriangleInversionInvariant.cpp
+++ src/wmtk/invariants/TriangleInversionInvariant.cpp
@@ -10,15 +10,19 @@ TriangleInversionInvariant::TriangleInversionInvariant(
     : MeshInvariant(m)
     , m_uv_coordinate_handle(uv_coordinate)
 {}
-bool TriangleInversionInvariant::before(const Tuple& t) const
+bool TriangleInversionInvariant::after(PrimitiveType type, const std::vector<Tuple>& t) const
 {
+    if (type != PrimitiveType::Face) return true;
+    // assume conterclockwise
     ConstAccessor<double> accessor = mesh().create_accessor(m_uv_coordinate_handle);
+    for (auto& tuple : t) {
+        Eigen::Vector2d p0 = accessor.const_vector_attribute(tuple);
+        Eigen::Vector2d p1 = accessor.const_vector_attribute(mesh().switch_vertex(tuple));
+        Eigen::Vector2d p2 =
+            accessor.const_vector_attribute(mesh().switch_vertex(mesh().switch_edge(tuple)));
 
-    Eigen::Vector2d p0 = accessor.const_vector_attribute(t);
-    Eigen::Vector2d p1 = accessor.const_vector_attribute(mesh().switch_vertex(t));
-    Eigen::Vector2d p2 =
-        accessor.const_vector_attribute(mesh().switch_vertex(mesh().switch_edge(t)));
-
-    return (triangle_2d_area<double>(p0, p1, p2) < 0);
+        if (triangle_2d_area<double>(p0, p1, p2) < 0) return false;
+    }
+    return true;
 }
 } // namespace wmtk
--- src/wmtk/invariants/TriangleInversionInvariant.hpp
+++ src/wmtk/invariants/TriangleInversionInvariant.hpp
@@ -8,11 +8,9 @@ class TriangleInversionInvariant : public MeshInvariant
 {
 public:
     // NOTE: this takes in the threshold squared rather than the threshold itself
-    TriangleInversionInvariant(
-        const Mesh& m,
-        const MeshAttributeHandle<double>& uv_coordinate);
+    TriangleInversionInvariant(const Mesh& m, const MeshAttributeHandle<double>& uv_coordinate);
     using MeshInvariant::MeshInvariant;
-    bool before(const Tuple& t) const override;
+    bool after(PrimitiveType type, const std::vector<Tuple>& t) const override;
 
 private:
     const MeshAttributeHandle<double> m_uv_coordinate_handle;
