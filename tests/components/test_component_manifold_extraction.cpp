99313304e1b8e6dc7c1444d51901f05285b6a422
Yifei <yz6994@nyu.edu>
2023-09-23 12:00:07 -0400

    figured out how to use igl::vertex_components

--- tests/components/test_component_manifold_extraction.cpp
+++ tests/components/test_component_manifold_extraction.cpp
@@ -4,19 +4,20 @@
 #include <wmtk_components/delaunay/internal/delaunay_3d.hpp>
 #include <wmtk_components/manifold_extraction/internal/Manifold-extraction.hpp>
 #include <paraviewo/VTUWriter.hpp>
+#include <igl/vertex_components.h>
+#include <igl/adjacency_matrix.h>
 
 
 TEST_CASE("Manifold-Extraction2D", "[components][man-ext2d]"){
     
     using namespace wmtk;
-    unsigned int nb_points = 20;
+    unsigned int nb_points = 20; // 20
     unsigned int nb_triangles;
     unsigned int nb_vertices;
     double range = 10.0;
-    size_t tagass_loop = 100;
-    size_t pntgen_loop = 10;
+    size_t tagass_loop = 1; // 100
+    size_t pntgen_loop = 1; // 10
     std::vector<std::vector<size_t>> tag(tagass_loop);
-
     for (size_t i = 0; i < pntgen_loop; i++){ // test for 10 iterations, each with 10 more vertices, so 20~110
         std::vector<Eigen::Vector2d> points;
         points.reserve(nb_points);
@@ -27,26 +28,49 @@ TEST_CASE("Manifold-Extraction2D", "[components][man-ext2d]"){
         wmtk::components::internal::delaunay_2d(points, vertices, faces);
         nb_vertices = vertices.rows();
         nb_triangles = faces.rows();
+        // std::cout << "Man-ext 2D whole triangles: " << std::endl << faces << std::endl;
         for (size_t j = 0 ; j < tagass_loop; j++){ // assign 100 sets of different tags for all triangles
             tag[j] = tagassign(nb_triangles); // assign tags to triangles, only keep the inside ones
             // std::vector<std::vector<Triangle>> components = findConnectedComponents(triangles, tagass_arr[j]);
+            size_t nb_in = tag[j].size();
+            int max_vertex = -1;
+            Eigen::MatrixXi faces_in;
+            faces_in.resize(nb_in, 3);
+            for (size_t k = 0; k < nb_in; k++){
+                for (size_t k2 = 0; k2 < 3; k2++) {
+                    int curr = faces(tag[j][k], k2);
+                    if (curr > max_vertex) {max_vertex = curr;}
+                    faces_in(k, k2) = curr;
+                }
+            }
+            Eigen::MatrixXi C;
+            C.resize(max_vertex + 1, 1); // since the vertices are indexed from 0
+            std::cout << "Man-ext 2D test case, j = " << j << std::endl;
+            std::cout << "num of tagged tri: " << nb_in << ", tags: ";
+            for (size_t k = 0; k < nb_in; k++){ std::cout << tag[j][k] << " ";}
+            std::cout << std::endl;
+            std::cout << faces_in << std::endl;
+            igl::vertex_components(faces_in, C);
+            std::cout << "Shape of C: row = " << C.rows() << ", col = " << C.cols() << std::endl << "Components:  ";
+            for (int k = 0; k < max_vertex + 1; k++){std::cout << C(k, 0) << " ";}
+            std::cout << std::endl;
         }
-
+            
         if (false) {
             paraviewo::VTUWriter writer;
             writer.write_mesh("manifold_extraction_2d_random_test.vtu", vertices, faces);
         }
 
-        SECTION("Points Number && Delaunay check"){
-            REQUIRE(points.size() == nb_points);
-            REQUIRE(vertices.rows() == nb_vertices);
-        }
+        // SECTION("Points Number && Delaunay check"){
+        REQUIRE(points.size() == nb_points);
+        REQUIRE(vertices.rows() == nb_vertices);
+        // }
 
-        SECTION("Tag assignment map num check"){
-            for (size_t j = 0; j < tagass_loop; j++){
-                REQUIRE(tag[j].size() <= nb_triangles); // NOTE: <= here since we only keep those tagged "inside"
-            }
+        // SECTION("Tag assignment map num check"){
+        for (size_t j = 0; j < tagass_loop; j++){
+            REQUIRE(tag[j].size() <= nb_triangles); // NOTE: <= here since we only keep those tagged "inside"
         }
+        // }
         nb_points += 10;
         range += 10.0;
     }
@@ -91,16 +115,16 @@ TEST_CASE("Manifold-Extraction3D", "[components][man-ext3d]"){
             writer.write_mesh("manifold_extraction_3d_random_test.vtu", vertices, faces);
         }
 
-        SECTION("Points Number && Delaunay check"){
-            REQUIRE(points.size() == nb_points);
-            REQUIRE(vertices.rows() == nb_vertices);
-        }
+        // SECTION("Points Number && Delaunay check"){
+        REQUIRE(points.size() == nb_points);
+        REQUIRE(vertices.rows() == nb_vertices);
+        // }
 
-        SECTION("Tag assignment map num check"){
-            for (size_t j = 0; j < tagass_loop; j++){
-                REQUIRE(tag[j].size() <= nb_triangles); // NOTE: <= here since we only keep those tagged "inside"
-            }
+        // SECTION("Tag assignment map num check"){
+        for (size_t j = 0; j < tagass_loop; j++){
+            REQUIRE(tag[j].size() <= nb_triangles); // NOTE: <= here since we only keep those tagged "inside"
         }
+        // }
         nb_points += 10;
         range += 10.0;
     }
