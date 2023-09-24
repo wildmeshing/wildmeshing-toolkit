5fd8125566d6b1402d543d54645813d26c035539
Yifei <yz6994@nyu.edu>
2023-09-23 20:37:21 -0400

    finish connect components compute (with rare bugs)

--- tests/components/test_component_manifold_extraction.cpp
+++ tests/components/test_component_manifold_extraction.cpp
@@ -4,8 +4,8 @@
 #include <wmtk_components/delaunay/internal/delaunay_3d.hpp>
 #include <wmtk_components/manifold_extraction/internal/Manifold-extraction.hpp>
 #include <paraviewo/VTUWriter.hpp>
-#include <igl/vertex_components.h>
 #include <igl/adjacency_matrix.h>
+#include <igl/connected_components.h>
 
 
 auto tagassign(size_t nb_triangles, double prob) -> std::vector<size_t>{
@@ -28,10 +28,12 @@ TEST_CASE("Manifold-Extraction2D", "[components][man-ext2d]"){
     unsigned int nb_triangles;
     unsigned int nb_vertices;
     double range = 10.0;
-    size_t tagass_loop = 3; // 100
-    size_t pntgen_loop = 2; // 10
+    size_t tagass_loop = 100; // 100
+    size_t pntgen_loop = 10; // 10
     double prob = 0.2;
+    paraviewo::VTUWriter writer;
     std::vector<std::vector<size_t>> tag(tagass_loop);
+
     for (size_t i = 0; i < pntgen_loop; i++){ // test for 10 iterations, each with 10 more vertices, so 20~110
         std::vector<Eigen::Vector2d> points;
         points.reserve(nb_points);
@@ -55,28 +57,88 @@ TEST_CASE("Manifold-Extraction2D", "[components][man-ext2d]"){
         for (size_t j = 0 ; j < tagass_loop; j++){
             tag[j] = tagassign(nb_triangles, prob); // assign tags to triangles, only keep the inside ones
             size_t nb_in = tag[j].size();
-            int max_vertex = -1;
             Eigen::MatrixXi faces_in;
+            bool vertices_in_bool[nb_vertices]; for (int k = 0; k < (int)nb_vertices; k++) {vertices_in_bool[k] = false;}
+            int nb_vertices_in = 0;
             faces_in.resize(nb_in, 3);
             for (size_t k = 0; k < nb_in; k++){
                 for (size_t k2 = 0; k2 < 3; k2++) {
+                    // tag[j][k] is triangle index
                     faces_in(k, k2) = faces(tag[j][k], k2);
+                    vertices_in_bool[faces(tag[j][k], k2)] = true;
                 }
             }
+            // for ()
+
             Eigen::MatrixXi C;
+            Eigen::MatrixXi arg2;
             Eigen::SparseMatrix<int> adj;
             igl::adjacency_matrix(faces_in,adj); // A is the adjacency matrix 
-            igl::vertex_components(adj, C); // C is the list of component ids
-            std::cout << "Case " << j << ", tagged tri num: " << nb_in << ", tags: ";
-            for (size_t k = 0; k < nb_in; k++){ std::cout << tag[j][k] << " ";}
+            igl::connected_components(adj, C, arg2);
+
+            std::cout << "Case " << j << ", tagged tri num: " << nb_in << "/" << nb_triangles;
+            // std::cout << "\ntags: ";
+            // for (size_t k = 0; k < nb_in; k++){ std::cout << tag[j][k] << " ";}
             // std::cout << std::endl << faces_in;
-            std::cout << "\nComponents: ";
-            for (int k = 0; k < C.rows() ; k++){std::cout << C(k, 0) << " ";} std::cout << "\n";
+
+            // std::cout << "\nComponents: ";
+            // for (int k = 0; k < C.rows() ; k++){std::cout << C(k, 0) << " ";}
+
+            // std::cout << "\nsize of each cc: ";
+            // for (int k = 0; k < arg2.rows() ; k++){std::cout << arg2(k, 0) << " ";}
+            
+            // store as a map each component and a vector of its vertices
+            std::map<int, std::vector<int>> cc_in;
+            for (int k = 0; k < (int)nb_vertices; k++){
+                if (vertices_in_bool[k]){ // vertex k must be in inside
+                    nb_vertices_in ++;
+                    // if the component that current vertex is in isn't stored in the map
+                    if (cc_in.count(C(k, 0)) == 0){
+                        // create a new pair
+                        cc_in.insert({C(k, 0), {k}});
+                    }
+                    else {
+                        //otherwise, append current vertex to the end of this component
+                        cc_in[C(k, 0)].push_back(k);
+                    }
+                }
+            }
+            // Eigen::MatrixXd vertices_in;
+            // vertices_in.resize(nb_vertices_in, 3);
+            // int temp = 0;
+            // for (int k = 0; k < (int)nb_vertices; k++){
+            //     if (vertices_in_bool[k]){ // vertex k must be in inside
+            //     for (int temp2 = 0; temp2 < 3; temp2 ++)
+            //         vertices_in(temp, temp2) = vertices(k, temp2);
+            //     }
+            //     temp++;
+            // }
+            
+            std::cout << "\nconnected components inside num: " << cc_in.size();
+            std::cout << "\nvertices inside num: " << nb_vertices_in << "\n";
+            // std::cout << "Connect components inside: \n";
+            // for (int k = 0; k < (int)nb_vertices; k++){
+            //     if (vertices_in[k]){
+            //         std::cout << C(k, 0) << " component has " << arg2(C(k, 0)) << " vertices: ";
+            //         for (int l: cc_in[C(k, 0)]) std::cout << l << " ";
+            //         std::cout << std::endl;
+            //     }
+            // }
+
+            int sum = 0;
+            for (int k = 0; k < arg2.rows() ; k++){
+                if (arg2(k, 0) >= 3){
+                    sum += arg2(k, 0);
+                }
+            }
+            REQUIRE(sum == nb_vertices_in);
+            // if (false){
+            //     writer.write_mesh("manifold_extraction_2d_random_test.vtu", vertices_in, faces_in);
+            // }
         }
             
         if (false) {
-            paraviewo::VTUWriter writer;
-            writer.write_mesh("manifold_extraction_2d_random_test.vtu", vertices, faces);
+            writer.write_mesh("manifold_extraction_2d.vtu", vertices, faces);
         }
 
         REQUIRE(points.size() == nb_points);
@@ -85,6 +147,7 @@ TEST_CASE("Manifold-Extraction2D", "[components][man-ext2d]"){
         for (size_t j = 0; j < tagass_loop; j++){
             REQUIRE(tag[j].size() <= nb_triangles); // NOTE: <= since we only keep those tagged "inside"
         }
+
         nb_points += 10;
         range += 10.0;
     }
