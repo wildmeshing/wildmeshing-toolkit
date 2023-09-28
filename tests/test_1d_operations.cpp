e68073438fa056d865abd9a0567ec2c2b246ab82
zlyfunction <zhuleyi001@126.com>
2023-09-28 14:30:08 -0400

    add split and collapse test, need fix collapse

--- tests/test_1d_operations.cpp
+++ tests/test_1d_operations.cpp
@@ -34,6 +34,7 @@ TEST_CASE("simplices_to_delete_for_split_1D", "[operations][1D]")
         auto executor = m.get_emoe(edge, hash_accessor);
 
         executor.split_edge();
+        REQUIRE(m.is_connectivity_valid());
         const auto& ids_to_delete = executor.simplex_ids_to_delete;
         REQUIRE(ids_to_delete[0].size() == 0);
         REQUIRE(ids_to_delete[1].size() == 1);
@@ -52,6 +53,7 @@ TEST_CASE("simplices_to_delete_for_split_1D", "[operations][1D]")
         auto executor = m.get_emoe(edge, hash_accessor);
 
         executor.split_edge();
+        REQUIRE(m.is_connectivity_valid());
         const auto& ids_to_delete = executor.simplex_ids_to_delete;
         REQUIRE(ids_to_delete[0].size() == 0);
         REQUIRE(ids_to_delete[1].size() == 1);
@@ -73,6 +75,7 @@ TEST_CASE("simplices_to_delete_for_collapse_1D", "[operations][1D]")
         auto executor = m.get_emoe(edge, hash_accessor);
 
         executor.collapse_edge();
+        REQUIRE(m.is_connectivity_valid());
         const auto& ids_to_delete = executor.simplex_ids_to_delete;
         REQUIRE(ids_to_delete[0].size() == 1);
         REQUIRE(ids_to_delete[0][0] == 3);
@@ -97,5 +100,192 @@ TEST_CASE("simplices_to_delete_for_collapse_1D", "[operations][1D]")
         REQUIRE(ids_to_delete[0][0] == 1);
         REQUIRE(ids_to_delete[1].size() == 1);
         REQUIRE(ids_to_delete[1][0] == edge_id);
+    }
+}
+
+
+TEST_CASE("collapse_edge_1D", "[operations][1D]")
+{
+    SECTION("multiple_lines")
+    {
+        DEBUG_EdgeMesh m = multiple_lines();
+        REQUIRE(m.is_connectivity_valid());
+
+        const long edge_id = 2;
+        Tuple edge = m.tuple_from_edge_id(edge_id);
+        REQUIRE(m.is_valid_slow(edge));
+        Accessor<long> hash_accessor = m.get_cell_hash_accessor();
+        auto executor = m.get_emoe(edge, hash_accessor);
+
+        executor.collapse_edge();
+        CHECK(m.is_connectivity_valid());
+    }
+
+    SECTION("single_line")
+    {
+        DEBUG_EdgeMesh m = single_line();
+        REQUIRE(m.is_connectivity_valid());
+
+        const long edge_id = 0;
+        Tuple edge = m.tuple_from_edge_id(edge_id);
+        REQUIRE(m.is_valid_slow(edge));
+        Accessor<long> hash_accessor = m.get_cell_hash_accessor();
+        auto executor = m.get_emoe(edge, hash_accessor);
+
+        executor.collapse_edge();
+        CHECK(m.is_connectivity_valid());
+    }
+
+    SECTION("self_loop")
+    {
+        DEBUG_EdgeMesh m = self_loop();
+        REQUIRE(m.is_connectivity_valid());
+
+        const long edge_id = 0;
+        Tuple edge = m.tuple_from_edge_id(edge_id);
+        REQUIRE(m.is_valid_slow(edge));
+        Accessor<long> hash_accessor = m.get_cell_hash_accessor();
+        auto executor = m.get_emoe(edge, hash_accessor);
+
+        executor.collapse_edge();
+        CHECK(m.is_connectivity_valid());
+    }
+
+    SECTION("two_line_loop")
+    {
+        DEBUG_EdgeMesh m = two_line_loop();
+        REQUIRE(m.is_connectivity_valid());
+
+        const long edge_id = 0;
+        Tuple edge = m.tuple_from_edge_id(edge_id);
+        REQUIRE(m.is_valid_slow(edge));
+        Accessor<long> hash_accessor = m.get_cell_hash_accessor();
+        auto executor = m.get_emoe(edge, hash_accessor);
+
+        executor.collapse_edge();
+        CHECK(m.is_connectivity_valid());
+    }
+
+    SECTION("self_loop")
+    {
+        DEBUG_EdgeMesh m = self_loop();
+        REQUIRE(m.is_connectivity_valid());
+
+        const long edge_id = 0;
+        Tuple edge = m.tuple_from_edge_id(edge_id);
+        REQUIRE(m.is_valid_slow(edge));
+        Accessor<long> hash_accessor = m.get_cell_hash_accessor();
+        auto executor = m.get_emoe(edge, hash_accessor);
+
+        executor.collapse_edge();
+        CHECK(m.is_connectivity_valid());
+    }
+
+    SECTION("loop_lines")
+    {
+        DEBUG_EdgeMesh m = loop_lines();
+        REQUIRE(m.is_connectivity_valid());
+
+        const long edge_id = 0;
+        Tuple edge = m.tuple_from_edge_id(edge_id);
+        REQUIRE(m.is_valid_slow(edge));
+        Accessor<long> hash_accessor = m.get_cell_hash_accessor();
+        auto executor = m.get_emoe(edge, hash_accessor);
+
+        executor.collapse_edge();
+        CHECK(m.is_connectivity_valid());
+    }
+}
+
+TEST_CASE("split_edge_1D", "[operations][1D]")
+{
+    SECTION("multiple_lines")
+    {
+        DEBUG_EdgeMesh m = multiple_lines();
+        REQUIRE(m.is_connectivity_valid());
+
+        const long edge_id = 2;
+        Tuple edge = m.tuple_from_edge_id(edge_id);
+        REQUIRE(m.is_valid_slow(edge));
+        Accessor<long> hash_accessor = m.get_cell_hash_accessor();
+        auto executor = m.get_emoe(edge, hash_accessor);
+
+        executor.split_edge();
+        CHECK(m.is_connectivity_valid());
+    }
+
+    SECTION("single_line")
+    {
+        DEBUG_EdgeMesh m = single_line();
+        REQUIRE(m.is_connectivity_valid());
+
+        const long edge_id = 0;
+        Tuple edge = m.tuple_from_edge_id(edge_id);
+        REQUIRE(m.is_valid_slow(edge));
+        Accessor<long> hash_accessor = m.get_cell_hash_accessor();
+        auto executor = m.get_emoe(edge, hash_accessor);
+
+        executor.split_edge();
+        CHECK(m.is_connectivity_valid());
+    }
+
+    SECTION("self_loop")
+    {
+        DEBUG_EdgeMesh m = self_loop();
+        REQUIRE(m.is_connectivity_valid());
+
+        const long edge_id = 0;
+        Tuple edge = m.tuple_from_edge_id(edge_id);
+        REQUIRE(m.is_valid_slow(edge));
+        Accessor<long> hash_accessor = m.get_cell_hash_accessor();
+        auto executor = m.get_emoe(edge, hash_accessor);
+
+        executor.split_edge();
+        CHECK(m.is_connectivity_valid());
+    }
+
+    SECTION("two_line_loop")
+    {
+        DEBUG_EdgeMesh m = two_line_loop();
+        REQUIRE(m.is_connectivity_valid());
+
+        const long edge_id = 0;
+        Tuple edge = m.tuple_from_edge_id(edge_id);
+        REQUIRE(m.is_valid_slow(edge));
+        Accessor<long> hash_accessor = m.get_cell_hash_accessor();
+        auto executor = m.get_emoe(edge, hash_accessor);
+
+        executor.split_edge();
+        CHECK(m.is_connectivity_valid());
+    }
+
+    SECTION("self_loop")
+    {
+        DEBUG_EdgeMesh m = self_loop();
+        REQUIRE(m.is_connectivity_valid());
+
+        const long edge_id = 0;
+        Tuple edge = m.tuple_from_edge_id(edge_id);
+        REQUIRE(m.is_valid_slow(edge));
+        Accessor<long> hash_accessor = m.get_cell_hash_accessor();
+        auto executor = m.get_emoe(edge, hash_accessor);
+
+        executor.split_edge();
+        CHECK(m.is_connectivity_valid());
+    }
+
+    SECTION("loop_lines")
+    {
+        DEBUG_EdgeMesh m = loop_lines();
+        REQUIRE(m.is_connectivity_valid());
+
+        const long edge_id = 0;
+        Tuple edge = m.tuple_from_edge_id(edge_id);
+        REQUIRE(m.is_valid_slow(edge));
+        Accessor<long> hash_accessor = m.get_cell_hash_accessor();
+        auto executor = m.get_emoe(edge, hash_accessor);
+
+        executor.split_edge();
+        CHECK(m.is_connectivity_valid());
     }
 }
