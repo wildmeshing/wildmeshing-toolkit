0191bc1287e6e9cd7d788a01bb0edcd206d16ce0
zlyfunction <zhuleyi001@126.com>
2023-09-28 13:30:36 -0400

    add test simplices_to_delete_for_split_1D

--- tests/test_1d_operations.cpp
+++ tests/test_1d_operations.cpp
@@ -20,10 +20,41 @@ using EMOE = decltype(std::declval<DEBUG_EdgeMesh>().get_emoe(
 constexpr PrimitiveType PV = PrimitiveType::Vertex;
 constexpr PrimitiveType PE = PrimitiveType::Edge;
 
-TEST_CASE("edge_mesh", "[operations][1D]")
+TEST_CASE("simplices_to_delete_for_split_1D", "[operations][1D]")
 {
     SECTION("single line")
     {
-        REQUIRE(true);
+        DEBUG_EdgeMesh m = single_line();
+        REQUIRE(m.is_connectivity_valid());
+
+        const long edge_id = 0;
+        Tuple edge = m.tuple_from_edge_id(edge_id);
+        REQUIRE(m.is_valid_slow(edge));
+        Accessor<long> hash_accessor = m.get_cell_hash_accessor();
+        auto executor = m.get_emoe(edge, hash_accessor);
+
+        executor.split_edge();
+        const auto& ids_to_delete = executor.simplex_ids_to_delete;
+        REQUIRE(ids_to_delete[0].size() == 0);
+        REQUIRE(ids_to_delete[1].size() == 1);
+        REQUIRE(ids_to_delete[1][0] == edge_id);
+    }
+
+    SECTION("self loop")
+    {
+        DEBUG_EdgeMesh m = single_line();
+        REQUIRE(m.is_connectivity_valid());
+
+        const long edge_id = 0;
+        Tuple edge = m.tuple_from_edge_id(edge_id);
+        REQUIRE(m.is_valid_slow(edge));
+        Accessor<long> hash_accessor = m.get_cell_hash_accessor();
+        auto executor = m.get_emoe(edge, hash_accessor);
+
+        executor.split_edge();
+        const auto& ids_to_delete = executor.simplex_ids_to_delete;
+        REQUIRE(ids_to_delete[0].size() == 0);
+        REQUIRE(ids_to_delete[1].size() == 1);
+        REQUIRE(ids_to_delete[1][0] == edge_id);
     }
 }
