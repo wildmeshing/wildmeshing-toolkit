8b8320bb74cfb7d730e2bc39314f2482f3596688
zhouyuan chen <867442167@qq.com>
2023-09-30 22:22:09 -0400

    clean the code, reconstruct the EdgeMeshReader

--- src/wmtk/io/EdgeMeshReader.cpp
+++ src/wmtk/io/EdgeMeshReader.cpp
@@ -10,64 +10,11 @@ void EdgeMeshReader::read(Eigen::Matrix<long, -1, -1>& E, Eigen::MatrixXd& V)
 {
     std::vector<std::pair<long, long>> edges;
     std::vector<std::vector<double>> vertices;
-    char* line_buffer = nullptr;
-    std::fstream f(m_filename);
-    if (!f) {
-        throw std::runtime_error("can't open file!");
-    }
-    if (m_type == OBJ) {
-        std::string buffer;
-        while (!f.eof()) {
-            getline(f, buffer);
-            // here is for debugging
-            // spdlog::info("lineinfo: {}", buffer[0]);
-            if (buffer[0] != 'v' && buffer[0] != 'l') continue;
-            // read obj
-            line_buffer = new char[buffer.size()];
-            strcpy(line_buffer, buffer.c_str());
-            if (line_buffer[0] == 'v') {
-                std::vector<double> positions;
-                char* token = strtok(line_buffer, " ");
-                while (token != NULL) {
-                    if (isdigit(token[0]) || token[0] == '-' || token[0] == '.') {
-                        positions.push_back(std::atof(token));
-                    }
-                    token = strtok(NULL, " ");
-                }
-                vertices.push_back(positions);
-            } else if (line_buffer[0] == 'l') {
-                line_buffer = new char[buffer.size()];
-                strcpy(line_buffer, buffer.c_str());
-                std::vector<long> segment;
-                char* token = strtok(line_buffer, " ");
-                while (token != NULL) {
-                    if (isdigit(token[0])) {
-                        segment.push_back(std::atol(token));
-                    }
-                    token = strtok(NULL, " ");
-                }
-                if (segment.size() > 1) {
-                    long v0, v1;
-                    v0 = segment[0];
-                    for (size_t i = 1; i < segment.size(); ++i) {
-                        v1 = segment[i];
-                        std::pair<long, long> edge;
-                        edge.first = v0;
-                        edge.second = v1;
-                        edges.push_back(edge);
-                        v0 = v1;
-                    }
-                }
-            }
-            if (line_buffer) delete[] line_buffer;
-        }
-    } else if (m_type == OFF) {
-        throw std::runtime_error(".off has not implemented!");
-        abort();
-        std::string buffer;
-        while (getline(f, buffer)) {
-            // read off
-        }
+
+    switch (m_type) {
+    case OBJ: read_obj(edges, vertices); break;
+    case OFF: read_off(edges, vertices); break;
+    default: throw std::runtime_error("Unknown data type!"); break;
     }
 
     E.resize(edges.size(), 2);
@@ -82,5 +29,60 @@ void EdgeMeshReader::read(Eigen::Matrix<long, -1, -1>& E, Eigen::MatrixXd& V)
         V(i, 1) = vertices[i][1];
         V(i, 2) = vertices[i][2];
     }
+}
+
+void EdgeMeshReader::read_obj(
+    std::vector<std::pair<long, long>>& edges,
+    std::vector<std::vector<double>>& vertices)
+{
+    std::ifstream f(m_filename);
+    if (!f.is_open()) {
+        throw std::runtime_error("can't open file!");
+    }
+    std::string buffer;
+    while (!f.eof()) {
+        getline(f, buffer);
+        std::vector<std::string> tokens;
+        // here is for debugging
+        // spdlog::info("lineinfo: {}", buffer[0]);
+        // read obj
+        if (buffer[0] == 'v') {
+            std::vector<double> positions;
+            std::stringstream line(buffer);
+            std::string token;
+            getline(line, token, ' ');
+            while (getline(line, token, ' ')) {
+                // spdlog::info("lineinfo: {}", token);
+                positions.push_back(std::atof(token.c_str()));
+            }
+            vertices.push_back(positions);
+        } else if (buffer[0] == 'l') {
+            std::vector<long> segment;
+            std::stringstream line(buffer);
+            std::string token;
+            getline(line, token, ' ');
+            while (getline(line, token, ' ')) {
+                segment.push_back(std::atol(token.c_str()));
+            }
+            if (segment.size() > 1) {
+                long v0, v1;
+                v0 = segment[0];
+                for (size_t i = 1; i < segment.size(); ++i) {
+                    v1 = segment[i];
+                    std::pair<long, long> edge;
+                    edge.first = v0;
+                    edge.second = v1;
+                    edges.push_back(edge);
+                    v0 = v1;
+                }
+            }
+        }
+    }
+}
+void EdgeMeshReader::read_off(
+    std::vector<std::pair<long, long>>& edges,
+    std::vector<std::vector<double>>& vertices)
+{
+    throw std::runtime_error("OFF is not supported!");
 }
 } // namespace wmtk
--- src/wmtk/io/EdgeMeshReader.hpp
+++ src/wmtk/io/EdgeMeshReader.hpp
@@ -11,6 +11,12 @@ public:
     enum file_type { OBJ, OFF };
     EdgeMeshReader(const std::string& filename, const file_type type);
     void read(Eigen::Matrix<long, -1, -1>& E, Eigen::MatrixXd& V);
+    void read_obj(
+        std::vector<std::pair<long, long>>& edges,
+        std::vector<std::vector<double>>& vertices);
+    void read_off(
+        std::vector<std::pair<long, long>>& edges,
+        std::vector<std::vector<double>>& vertices);
 
     std::string m_filename;
     file_type m_type;
