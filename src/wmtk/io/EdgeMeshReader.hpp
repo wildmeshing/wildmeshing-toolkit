2c3f359703bfc94589179689d2dfc75a778262e1
zhouyuan chen <867442167@qq.com>
2023-09-30 23:57:16 -0400

    input .obj files completed, can read a full .obj edge files.

--- src/wmtk/io/EdgeMeshReader.hpp
+++ src/wmtk/io/EdgeMeshReader.hpp
@@ -10,7 +10,23 @@ class EdgeMeshReader
 public:
     enum file_type { OBJ, OFF };
     EdgeMeshReader(const std::string& filename, const file_type type);
-    void read(Eigen::Matrix<long, -1, -1>& E, Eigen::MatrixXd& V);
+    void read(
+        Eigen::Matrix<long, -1, -1>& edges,
+        Eigen::MatrixXd& vertices,
+        Eigen::MatrixXd& vertices_w,
+        Eigen::MatrixXd& vertices_texture,
+        Eigen::MatrixXd& vertices_normal,
+        Eigen::MatrixXd& vertices_parameter);
+    void read_obj(
+        std::vector<std::pair<long, long>>& edges,
+        std::vector<std::vector<double>>& vertices,
+        std::vector<double>& vertices_w,
+        std::vector<std::vector<double>>& vertices_texture,
+        std::vector<std::vector<double>> vertices_normal,
+        std::vector<std::vector<double>>& vertices_parameter);
+    void read_off(
+        std::vector<std::pair<long, long>>& edges,
+        std::vector<std::vector<double>>& vertices);
 
     std::string m_filename;
     file_type m_type;
