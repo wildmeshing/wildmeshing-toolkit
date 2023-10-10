c2f52b38272631373e11ea9f71582e34e1aeaccd
Daniel Zint <daniel.zint@mail.de>
2023-10-10 17:42:46 -0400

    Remove boundary hack. It is not required in the invariant.

--- src/wmtk/invariants/MinIncidentValenceInvariant.cpp
+++ src/wmtk/invariants/MinIncidentValenceInvariant.cpp
@@ -12,14 +12,8 @@ bool wmtk::MinIncidentValenceInvariant::before(const Tuple& t) const
 {
     const Tuple v0 = t;
     const Tuple v1 = mesh().switch_vertex(t);
-    long val0 = static_cast<long>(SimplicialComplex::vertex_one_ring(mesh(), v0).size());
-    long val1 = static_cast<long>(SimplicialComplex::vertex_one_ring(mesh(), v1).size());
-    if (mesh().is_boundary_vertex(v0)) {
-        ++val0;
-    }
-    if (mesh().is_boundary_vertex(v1)) {
-        ++val1;
-    }
+    const long val0 = static_cast<long>(SimplicialComplex::vertex_one_ring(mesh(), v0).size());
+    const long val1 = static_cast<long>(SimplicialComplex::vertex_one_ring(mesh(), v1).size());
 
     return val0 >= m_min_valence && val1 >= m_min_valence;
 }
