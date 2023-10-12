5946b9a7cd4f6039d04195a2ae794d6d2e2e4625
zlyfunction <zhuleyi001@126.com>
2023-10-12 12:34:29 -0400

    add files

---
+++ src/wmtk/operations/edge_mesh/EdgeOperationData.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <array>
+#include <vector>
+#include <wmtk/Tuple.hpp>
+
+namespace wmtk::operations::edge_mesh {
+struct EdgeOperationData
+{
+};
+} // namespace wmtk::operations::edge_mesh
