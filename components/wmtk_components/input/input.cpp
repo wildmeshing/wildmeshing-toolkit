793d4062e856ecf7b7870bcb732ac88ad0e6c325
zhouyuan chen <867442167@qq.com>
2023-10-17 15:15:35 -0400

    add basic test

--- components/wmtk_components/input/input.cpp
+++ components/wmtk_components/input/input.cpp
@@ -1,8 +1,11 @@
 #include "input.hpp"
 
+#include <igl/edges.h>
 #include <igl/read_triangle_mesh.h>
+#include <wmtk/EdgeMesh.hpp>
 #include <wmtk/PointMesh.hpp>
 #include <wmtk/TriMesh.hpp>
+#include <wmtk/io/EdgeMeshReader.hpp>
 #include <wmtk/io/HDF5Writer.hpp>
 #include <wmtk/io/MeshReader.hpp>
 #include <wmtk/utils/mesh_utils.hpp>
@@ -11,6 +14,7 @@
 
 namespace wmtk {
 namespace components {
+
 void input(const nlohmann::json& j, std::map<std::string, std::filesystem::path>& files)
 {
     using namespace internal;
@@ -52,11 +56,63 @@ void input(const nlohmann::json& j, std::map<std::string, std::filesystem::path>
         files[options.name] = cached_mesh_file;
         break;
     }
-    case 1:
-        // edge mesh
-        throw "not implemented";
-        assert(false);
+    case 1: {
+        // for debugging
+        // spdlog::info("file_name:{}", options.file.string());
+        EdgeMesh mesh;
+        if (options.file.extension() == ".hdf5") {
+            MeshReader reader(options.file);
+            reader.read(mesh);
+        } else if (options.file.extension() == ".obj" || options.file.extension() == ".off") {
+            Eigen::MatrixXd V;
+            Eigen::Matrix<long, -1, -1> E;
+            Eigen::MatrixXd W;
+            Eigen::MatrixXd VT;
+            Eigen::MatrixXd VN;
+            Eigen::MatrixXd VP;
+            // read E
+            // assume only have l and v
+            EdgeMeshReader::file_type type;
+            if (options.file.extension() == ".obj") {
+                type = EdgeMeshReader::OBJ;
+            } else if (options.file.extension() == ".off") {
+                type = EdgeMeshReader::OFF;
+            }
+            // EdgeMeshReader reader(, );
+            EdgeMeshReader reader(options.file.string(), EdgeMeshReader::file_type::OBJ);
+            reader.read(E, V, W, VT, VN, VP);
+            mesh.initialize(E);
+            // for debugging
+            // spdlog::info("E:{} V:{}", E.rows(), V.rows());
+
+            Eigen::Matrix<long, -1, -1> vertices_tags;
+
+            // I thought this block could be removed, since we will add tags in embedding part.
+            vertices_tags.resize(V.rows(), 1);
+            vertices_tags.setZero();
+            mesh_utils::set_matrix_attribute(
+                vertices_tags,
+                "vertices_tags",
+                PrimitiveType::Vertex,
+                mesh);
+
+            mesh_utils::set_matrix_attribute(V, "position", PrimitiveType::Vertex, mesh);
+        } else {
+            throw std::runtime_error(std::string("Unknown file type: ") + options.file.string());
+        }
+
+        const std::filesystem::path cache_dir = "cache";
+        std::filesystem::create_directory(cache_dir);
+
+        const std::filesystem::path cached_mesh_file = cache_dir / (options.name + ".hdf5");
+
+        HDF5Writer writer(cached_mesh_file);
+        mesh.serialize(writer);
+
+        files["file"] = options.file;
+        files[options.name] = cached_mesh_file;
         break;
+    }
     case 2: {
         // triangle mesh
         TriMesh mesh;
---
+++ tests/components/test_component_embedding.cpp
@@ -0,0 +1,35 @@
+#include <igl/is_edge_manifold.h>
+#include <catch2/catch_test_macros.hpp>
+#include <nlohmann/json.hpp>
+#include <wmtk_components/embedding/embedding.hpp>
+#include <wmtk_components/embedding/internal/Embedding.hpp>
+#include <wmtk_components/embedding/internal/EmbeddingOptions.hpp>
+#include <wmtk_components/input/input.hpp>
+
+using json = nlohmann::json;
+using namespace wmtk;
+const std::filesystem::path data_dir = WMTK_DATA_DIR;
+
+TEST_CASE("embedding_function", "[components][embedding][2D]")
+{
+    // spdlog::warn("EdgeMesh has not been merged and used!");
+    // EdgeMesh has not been merged
+    std::map<std::string, std::filesystem::path> files;
+    json component_json = {
+        {"type", "input"},
+        {"name", "input_mesh"},
+        {"cell_dimension", 1},
+        {"file", data_dir / "test_edgemesh.obj"}};
+    wmtk::components::input(component_json, files);
+
+    json mesh_embedding_json = {
+        {"input_file", "file"},
+        {"type", "embedding"},
+        {"input", "input_mesh"},
+        {"output", "output_mesh"},
+        {"tag_name", "vertices_tags"},
+        {"input_tag_value", 1},
+        {"embedding_tag_value", 0},
+        {"resolute_level", 0}};
+    REQUIRE_NOTHROW(wmtk::components::embedding(mesh_embedding_json, files));
+}
