c060f20a17f12eff13a8481d3c09f7dc8bcc9ac1
zlyfunction <zhuleyi001@126.com>
2023-10-18 22:44:57 -0400

    add implementation and test

--- src/wmtk/autogen/is_ccw.cpp
+++ src/wmtk/autogen/is_ccw.cpp
@@ -1,15 +1,17 @@
 #include "is_ccw.hpp"
 #include <wmtk/Tuple.hpp>
+#include <wmtk/autogen/edge_mesh/is_ccw.hpp>
 #include <wmtk/autogen/tet_mesh/is_ccw.hpp>
 #include <wmtk/autogen/tri_mesh/is_ccw.hpp>
+
 namespace wmtk::autogen {
 bool is_ccw(PrimitiveType pt, const Tuple& t)
 {
     switch (pt) {
     case PrimitiveType::Face: return tri_mesh::is_ccw(t);
     case PrimitiveType::Tetrahedron: return tet_mesh::is_ccw(t);
+    case PrimitiveType::Edge: return edge_mesh::is_ccw(t);
     case PrimitiveType::Vertex:
-    case PrimitiveType::Edge:
     default: throw "notimplemented";
     }
     return false;
@@ -21,8 +23,8 @@ bool tuple_is_valid_for_ccw(PrimitiveType pt, const Tuple& t)
     switch (pt) {
     case PrimitiveType::Face: return tri_mesh::tuple_is_valid_for_ccw(t);
     case PrimitiveType::Tetrahedron: return tet_mesh::tuple_is_valid_for_ccw(t);
+    case PrimitiveType::Edge: return edge_mesh::tuple_is_valid_for_ccw(t);
     case PrimitiveType::Vertex:
-    case PrimitiveType::Edge:
     default: throw "notimplemented";
     }
     return false;
