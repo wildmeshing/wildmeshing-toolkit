3d9b93fa83d5deabedb736e370680b36b142d741
zlyfunction <zhuleyi001@126.com>
2023-10-19 00:11:05 -0400

    add TODO in function transport_tuple for case that base_pt > pt

--- src/wmtk/multimesh/utils/transport_tuple.cpp
+++ src/wmtk/multimesh/utils/transport_tuple.cpp
@@ -11,8 +11,14 @@ Tuple transport_tuple(
     const Tuple& source,
     PrimitiveType primitive_type)
 {
-    std::vector<PrimitiveType> operations =
-        find_local_switch_sequence(base_source, base_target, base_primitive_type);
-    return local_switch_tuples(primitive_type, source, operations);
+    if (base_primitive_type <= primitive_type) {
+        std::vector<PrimitiveType> operations =
+            find_local_switch_sequence(base_source, base_target, base_primitive_type);
+        return local_switch_tuples(primitive_type, source, operations);
+    } else {
+        // TODO: implement this
+        // in top_level_cofaces find the one with the same
+        return Tuple();
+    }
 }
 } // namespace wmtk::multimesh::utils
