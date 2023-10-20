d2a6819459a3f53fdd92724c9bec5c594a57536f
yunfanzhou <yfzhou42@gmail.com>
2023-10-20 12:22:49 -0400

    Differentiable extension of the persimplexfunction

--- src/wmtk/function/DifferentiablePerSimplexFunction.cpp
+++ src/wmtk/function/DifferentiablePerSimplexFunction.cpp
@@ -4,27 +4,30 @@ namespace wmtk {
 namespace function {
 DifferentiablePerSimplexFunction::DifferentiablePerSimplexFunction(
     const Mesh& mesh,
-    const Simplex::Type& simplex_type)
+    const PrimitiveType& simplex_type,
+    const attribute::MeshAttributeHandle<double>& variable_attribute_handle)
     : PerSimplexFunction(mesh, simplex_type)
+    , m_attribute_handle(variable_attribute_handle)
 {}
 
 DifferentiablePerSimplexFunction::~DifferentiablePerSimplexFunction() = default;
 
-const MeshAttributeHandle<double>& DifferentiablePerSimplexFunction::get_simplex_attribute_handle()
+const MeshAttributeHandle<double>& DifferentiablePerSimplexFunction::get_variable_attribute_handle()
     const
 {
     return m_attribute_handle;
 }
 
-void DifferentiablePerSimplexFunction::assert_function_type(const Simplex::type& s_type) const
+void DifferentiablePerSimplexFunction::assert_function_type(const PrimitiveType& s_type) const
 {
-    if (m_attribute_handle.get_attribute_type() != s_type) {
+    if (get_variable_attribute_handle().primitive_type() != s_type) {
         throw std::runtime_error("Differentiation of the DifferentiableFunction must be taken wrt "
                                  "the attribute of the simplex type ");
     }
-    if (get_simplex_type() > s_type || get_simplex_type() == s_type) {
-        throw std::runtime_error("The DifferentiableFunction must be defined on the cofaces of the "
-                                 "simplex type ");
+    if (get_simplex_type() < s_type) {
+        throw std::runtime_error(
+            "The DifferentiableFunction must be defined on the cofaces of higher dimension to the "
+            "simplex type ");
     }
 }
 } // namespace function
--- src/wmtk/function/DifferentiablePerSimplexFunction.hpp
+++ src/wmtk/function/DifferentiablePerSimplexFunction.hpp
@@ -1,24 +1,44 @@
 #pragma once
+#include <wmtk/Primitive.hpp>
 #include <wmtk/attribute/AttributeHandle.hpp>
+#include <wmtk/attribute/MeshAttributes.hpp>
 #include "PerSimplexFunction.hpp"
 namespace wmtk {
 namespace function {
 class DifferentiablePerSimplexFunction : public PerSimplexFunction
 {
 public:
-    DifferentiablePerSimplexFunction(const Mesh& mesh, const Simplex::Type& simplex_type);
+    /**
+     * @brief Construct a new DifferentiablePerSimplexFunction object where the function is defined
+     * over simplices of simplex_type. And the differentiation is taken wrt the
+     * attribute_handle.primitive_type()
+     *
+     * @param mesh
+     * @param simplex_type
+     */
+    DifferentiablePerSimplexFunction(
+        const Mesh& mesh,
+        const PrimitiveType& simplex_type,
+        const attribute::MeshAttributeHandle<double>& variable_attribute_handle);
     virtual ~DifferentiablePerSimplexFunction();
 
 public:
-    virtual Eigen::VectorXd get_gradient(const Simplex& s) const;
-    virtual Eigen::MatrixXd get_hessian(const Simplex& s) const;
+    virtual Eigen::VectorXd get_gradient(const Simplex& s) const = 0;
+    virtual Eigen::MatrixXd get_hessian(const Simplex& s) const = 0;
 
-    const MeshAttributeHandle<double>& get_simplex_attribute_handle() const;
+    const attribute::MeshAttributeHandle<double>& get_variable_attribute_handle() const;
 
-    void assert_function_type(const Simplex::type& s_type) const;
+    /**
+     * @brief the function should be defined on the simplex of type A and the differntiation is
+     * taken wrt simplex type B. The definition mandate (1) A should be coface with B, (2) the type
+     * of the attribute_handle should be of the same type as B.
+     *
+     * @param s_type simplex Type B as defined above
+     */
+    void assert_function_type(const PrimitiveType& s_type) const;
 
 private:
-    const MeshAttributeHandle<double> m_attribute_handle;
+    const attribute::MeshAttributeHandle<double> m_attribute_handle;
 };
 } // namespace function
 } // namespace wmtk
