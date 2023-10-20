0bf9576ecd2eb71cfa4dd3e31c90ceef80b38fa6
yunfanzhou <yfzhou42@gmail.com>
2023-10-20 12:21:51 -0400

    define persimplexfunction

--- src/wmtk/function/PerSimplexFunction.cpp
+++ src/wmtk/function/PerSimplexFunction.cpp
@@ -3,16 +3,16 @@
 namespace wmtk {
 namespace function {
 
-PerSimplexFunction::PerSimplexFunction(const Mesh& mesh, const Simplex::Type& simplex_type)
+PerSimplexFunction::PerSimplexFunction(const Mesh& mesh, const PrimitiveType& simplex_type)
     : m_mesh(mesh)
     , m_simplex_type(simplex_type)
 {}
-const Mesh& mesh() const
+const Mesh& PerSimplexFunction::mesh() const
 {
     return m_mesh;
 }
 
-const Simplex::Type& PerSimplexFunction::get_simplex_type() const
+const PrimitiveType& PerSimplexFunction::get_simplex_type() const
 {
     return m_simplex_type;
 }
--- src/wmtk/function/PerSimplexFunction.hpp
+++ src/wmtk/function/PerSimplexFunction.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <wmtk/Mesh.hpp>
-#include <wmtk/MeshAttributeHandle.hpp>
+#include <wmtk/Primitive.hpp>
 #include <wmtk/Simplex.hpp>
 #include <wmtk/Tuple.hpp>
 namespace wmtk {
@@ -8,17 +8,17 @@ namespace function {
 class PerSimplexFunction
 {
 public:
-    PerSimplexFunction(const Mesh& mesh, const Simplex::Type& simplex_type);
+    PerSimplexFunction(const Mesh& mesh, const PrimitiveType& simplex_type);
     virtual ~PerSimplexFunction();
 
 public:
     const Mesh& mesh() const;
     virtual double get_value(const Simplex& s) const = 0;
-    const Simplex::Type& get_simplex_type() const;
+    const PrimitiveType& get_simplex_type() const;
 
 private:
     const Mesh& m_mesh;
-    const Simplex::Type m_simplex_type;
+    const PrimitiveType m_simplex_type;
 };
 } // namespace function
 
