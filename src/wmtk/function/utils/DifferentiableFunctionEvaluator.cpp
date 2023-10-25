669b591862415fbaddc170d9abd68c6ca82e06e9
yunfanzhou <yfzhou42@gmail.com>
2023-10-25 18:10:17 -0400

    restricting get_gradient and hessian to only the differnetiableFunctionEvaluator

--- src/wmtk/function/utils/DifferentiableFunctionEvaluator.cpp
+++ src/wmtk/function/utils/DifferentiableFunctionEvaluator.cpp
@@ -1,34 +1,78 @@
 #include "DifferentiableFunctionEvaluator.hpp"
+#include <wmtk/simplex/top_level_cofaces.hpp>
+#include <wmtk/simplex/upper_level_cofaces.hpp>
+#include <wmtk/simplex/utils/tuple_vector_to_homogeneous_simplex_vector.hpp>
+
+
 namespace wmtk::function::utils {
-
 DifferentiableFunctionEvaluator::DifferentiableFunctionEvaluator(
-    const function::DifferentiableFunction& function,
+    const function::LocallyDifferentiableFunction& function,
     Accessor<double>& accessor,
-    const Tuple& tuple)
-    : FunctionEvaluator(function, accessor, tuple)
-{}
-
-auto DifferentiableFunctionEvaluator::function() const -> const function::DifferentiableFunction&
+    const Simplex& simplex)
+    : FunctionEvaluator(function, accessor, simplex)
 {
-    return static_cast<const DifferentiableFunction&>(FunctionEvaluator::function());
+    m_upper_level_cofaces = compute_upper_level_cofaces();
+}
+
+auto DifferentiableFunctionEvaluator::function() const
+    -> const function::LocallyDifferentiableFunction&
+{
+    return static_cast<const LocallyDifferentiableFunction&>(FunctionEvaluator::function());
+}
+
+auto DifferentiableFunctionEvaluator::get_value(double v) -> double
+{
+    store(v);
+    return get_value();
+}
+
+auto DifferentiableFunctionEvaluator::get_value() const -> double
+{
+    return function().get_value_sum(
+        wmtk::simplex::utils::tuple_vector_to_homogeneous_simplex_vector(
+            upper_level_cofaces(),
+            function_simplex_type()));
 }
 
 auto DifferentiableFunctionEvaluator::get_gradient() const -> Vector
 {
-    return function().get_gradient_sum(tuple(), top_level_cofaces());
+    return function().get_gradient_sum(
+        wmtk::simplex::utils::tuple_vector_to_homogeneous_simplex_vector(
+            upper_level_cofaces(),
+            function_simplex_type()));
 }
+
 auto DifferentiableFunctionEvaluator::get_hessian() const -> Matrix
 {
-    return function().get_hessian_sum(tuple(), top_level_cofaces());
+    return function().get_hessian_sum(
+        wmtk::simplex::utils::tuple_vector_to_homogeneous_simplex_vector(
+            upper_level_cofaces(),
+            function_simplex_type()));
 }
+
 auto DifferentiableFunctionEvaluator::get_gradient(double v) -> Vector
 {
     store(v);
     return get_gradient();
 }
+
 auto DifferentiableFunctionEvaluator::get_hessian(double v) -> Matrix
 {
     store(v);
     return get_hessian();
+}
+
+const std::vector<Tuple>& DifferentiableFunctionEvaluator::upper_level_cofaces() const
+{
+    return m_upper_level_cofaces;
+}
+std::vector<Tuple> DifferentiableFunctionEvaluator::compute_upper_level_cofaces() const
+{
+    return simplex::upper_level_cofaces_tuples(mesh(), simplex(), function_simplex_type());
+}
+
+std::vector<Tuple> DifferentiableFunctionEvaluator::compute_top_level_cofaces() const
+{
+    return simplex::top_level_cofaces_tuples(mesh(), simplex());
 }
 } // namespace wmtk::function::utils
