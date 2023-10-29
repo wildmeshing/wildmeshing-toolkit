7c5bd1d977c9b6478d51baae3d9a869bf85f0509
Daniel Zint <daniel.zint@mail.de>
2023-10-29 15:20:21 -0400

    simplify documentation

--- src/wmtk/simplex/vertices.hpp
+++ src/wmtk/simplex/vertices.hpp
@@ -6,25 +6,16 @@ namespace wmtk::simplex {
 /**
  * @brief Returns a vector with all vertices in the boundary of a simplex.
  *
- * The simplices are sorted according to the input tuple.
+ * The vertices are sorted according to the input tuple:
+ * v0 = simplex.tuple()
+ * v1 = mesh.switch_vertex(v0);
+ * v2 = mesh.switch_vertex(mesh.switch_edge(v0));
+ * v3 = mesh.switch_vertex(mesh.switch_edge(mesh.switch_face(v0)));
  *
- * Vertex: 0
- *
- * Edge: 0 --> 1
- *
- * Triangle:
- *      2
- *     / \ .
- *    / x \ .
- *   0 --> 1
- *
- * Tetrahedra:
- *        2
- *       / \\ .
- *      /   \ \ .
- *     /  x  \  \ .
- *    /       \   \ 3
- *   0 -->---- 1
+ * Vertex:     {v0}
+ * Edge:       {v0, v1}
+ * Triangle:   {v0, v1, v2}
+ * Tetrahedra: {v0, v1, v2, v3}
  *
  * @param mesh The mesh containing the simplex
  * @param simplex The simplex
