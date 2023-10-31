b082fd6a32fa774cc8d732ca2c126ae2443f3510
Daniel Zint <daniel.zint@mail.de>
2023-10-31 10:32:25 -0400

    Add documentation to `simplex::faces` and `simplex::boundary`.

--- src/wmtk/simplex/boundary.cpp
+++ src/wmtk/simplex/boundary.cpp
@@ -2,8 +2,6 @@
 #include "SimplexCollection.hpp"
 
 namespace wmtk::simplex {
-// returns all simplices that lie on the boundary of the input simplex (i.e all cofaces)
-// This does not include itself
 SimplexCollection boundary(const Mesh& mesh, const Simplex& simplex, const bool sort_and_clean)
 {
     SimplexCollection collection(mesh);
