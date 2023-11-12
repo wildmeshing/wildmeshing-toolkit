b6c5f36977bbe8751b1f1c48bb43503d737f15bb
Yifei <yz6994@nyu.edu>
2023-11-12 01:01:24 -0500

    fix internal bug and pass all tag tests on 2d tet

--- components/wmtk_components/extract_subset/internal/extract_subset_2d.cpp
+++ components/wmtk_components/extract_subset/internal/extract_subset_2d.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 namespace wmtk::components::internal {
 
-wmtk::TriMesh extract_subset_2d(wmtk::TriMesh m, wmtk::MeshAttributeHandle<long> tag_handle, bool pos)
+wmtk::TriMesh
+extract_subset_2d(wmtk::TriMesh m, wmtk::MeshAttributeHandle<long> tag_handle, bool pos)
 {
     auto tag_acc = m.create_accessor(tag_handle);
     auto faces = m.get_all(wmtk::PrimitiveType::Face);
@@ -34,26 +35,42 @@ wmtk::TriMesh extract_subset_2d(wmtk::TriMesh m, wmtk::MeshAttributeHandle<long>
         }
     }
     assert(nb_tri_in <= m.capacity(wmtk::PrimitiveType::Face));
+    // std::cout << "# of tri inside = " << nb_tri_in << std::endl;
 
     // for the tagged tri, mark their vertices as inside (duplicates handled by boolean)
+    // current algo for bug fixing: O(N^2), go over all vertices and look for match, 
+    // only assign tag to inside ones
+    // TODO: improve the algorithm to achieve O(N)
     for (size_t i = 0; i < nb_tri_in; ++i) {
         Simplex s = Simplex::face(faces[tag_tri_index[i]]);
         auto tuple_list = wmtk::simplex::faces_single_dimension(m, s, PrimitiveType::Vertex);
         for (auto t : tuple_list) {
-            vertices_in_bool[t] = true;
+            // This inner loop gives you additional N complexity
+            for (int j = 0; j < vertices.size(); ++j) {
+                if (m.simplices_are_equal(
+                        wmtk::Simplex::vertex(t),
+                        wmtk::Simplex::vertex(vertices[j]))) {
+                    vertices_in_bool[vertices[j]] = true;
+                    break;
+                }
+            }
         }
     }
 
+    // std::cout << "# of vertex inside = " << vertices_in_bool.size() << std::endl;
     // construct a map from old tuple to temp new "id" of a vertex
     std::map<const wmtk::Tuple, long> old2new;
     for (auto t : vertices) {
-        // ignore the not extracted vertices
-        if (vertices_in_bool[t]) {
+        if (vertices_in_bool.at(t)) {
+            // std::cout << "inside! nb_vertex_in = " << nb_vertex_in << std::endl;
             // old vertex tuple t mapped to new vertex id j, where j increases by count
             old2new.insert({t, nb_vertex_in});
             nb_vertex_in++;
         }
     }
+    // std::cout << "# of pairs in map = " << old2new.size() << std::endl;
+    // for (auto [_, i] : old2new) std::cout << i << std::endl;
+
     wmtk::TriMesh mesh;
     wmtk::RowVectors3l tris;
     tris.resize(nb_tri_in, 3);
@@ -61,28 +78,42 @@ wmtk::TriMesh extract_subset_2d(wmtk::TriMesh m, wmtk::MeshAttributeHandle<long>
     for (size_t i = 0; i < nb_tri_in; ++i) {
         Simplex s = Simplex::face(faces[tag_tri_index[i]]);
         auto list = wmtk::simplex::faces_single_dimension(m, s, PrimitiveType::Vertex);
-        assert(list.size() == 3);
-        tris.row(i) << old2new[list[0]], old2new[list[1]], old2new[list[2]];
+        std::vector<long> data(3, -1);
+        for (int index = 0; index < 3; ++index) {
+            for (int j = 0; j < vertices.size(); ++j) {
+                if (m.simplices_are_equal(
+                        wmtk::Simplex::vertex(list[index]),
+                        wmtk::Simplex::vertex(vertices[j]))) {
+                    data[index] = old2new[vertices[j]];
+                    break;
+                }
+            }
+        }
+        tris.row(i) << data[0], data[1], data[2];
+        // tris.row(i) << old2new[list[0]], old2new[list[1]], old2new[list[2]];
     }
+    // for (size_t i = 0; i < nb_tri_in; ++i) {
+    //     std::cout << tris.row(i)[0] << tris.row(i)[1] << tris.row(i)[2] << std::endl;
+    // }
     mesh.initialize(tris); // init the topology
 
     // if told to extract and preserve the coordinates
     if (pos) {
-    Eigen::MatrixXd points_in;
-    points_in.resize(nb_vertex_in, 2);
-    auto pos_handle = m.get_attribute_handle<double>("position", PrimitiveType::Vertex);
-    auto pos_acc = m.create_const_accessor(pos_handle);
-    for (const Tuple& t : vertices) {
-        // ignore the outside vertices
-        if (vertices_in_bool[t]) {
-            points_in.row(old2new[t]) = pos_acc.const_vector_attribute(t);
+        Eigen::MatrixXd points_in;
+        points_in.resize(nb_vertex_in, 2);
+        auto pos_handle = m.get_attribute_handle<double>("position", PrimitiveType::Vertex);
+        auto pos_acc = m.create_const_accessor(pos_handle);
+        for (const Tuple& t : vertices) {
+            // ignore the outside vertices
+            if (vertices_in_bool[t]) {
+                points_in.row(old2new[t]) = pos_acc.const_vector_attribute(t);
+            }
         }
-    }
-    wmtk::mesh_utils::set_matrix_attribute(
-        points_in,
-        "position",
-        wmtk::PrimitiveType::Vertex,
-        mesh);
+        wmtk::mesh_utils::set_matrix_attribute(
+            points_in,
+            "position",
+            wmtk::PrimitiveType::Vertex,
+            mesh);
     }
     return mesh;
 }
--- tests/components/test_component_extract_subset.cpp
+++ tests/components/test_component_extract_subset.cpp
@@ -4,17 +4,49 @@
 #include <wmtk/utils/mesh_utils.hpp>
 #include <wmtk_components/extract_subset/extract_subset.hpp>
 #include <wmtk_components/extract_subset/internal/extract_subset_2d.hpp>
+#include "../tools/DEBUG_TriMesh.hpp"
 #include "../tools/TriMesh_examples.hpp"
+
+void check_new_mesh(
+    wmtk::tests::DEBUG_TriMesh& m,
+    std::vector<int> data,
+    bool b,
+    int vertex_count,
+    int edge_count,
+    int face_count)
+{
+    wmtk::tests::DEBUG_TriMesh new_tm = wmtk::components::extract_subset(m, 2, data, b);
+    // new_tm.print_vf();
+    CHECK(new_tm.capacity(wmtk::PrimitiveType::Vertex) == vertex_count);
+    CHECK(new_tm.capacity(wmtk::PrimitiveType::Edge) == edge_count);
+    CHECK(new_tm.capacity(wmtk::PrimitiveType::Face) == face_count);
+    // wmtk::ParaviewWriter writer("mesh_smooth", "vertices", new_tm, true, true, true, false);
+    // new_tm.serialize(writer);
+}
 
 TEST_CASE("2d_tetrahedron_test_case", "[components][extract_subset][2D]")
 {
-    wmtk::TriMesh tm = wmtk::tests::tetrahedron_with_position();
-    std::vector<int> tag_vector = {1, 0, 1, 1};
-    wmtk::TriMesh new_tm = wmtk::components::extract_subset(tm, 2, tag_vector, true);
-    CHECK(new_tm.capacity(wmtk::PrimitiveType::Face) == 3);
-    CHECK(new_tm.capacity(wmtk::PrimitiveType::Vertex) == 4);
-    CHECK(new_tm.capacity(wmtk::PrimitiveType::Edge) == 6);
+    wmtk::tests::DEBUG_TriMesh tm = wmtk::tests::tetrahedron_with_position();
+    for (int i1 = 0; i1 < 2; ++i1) {
+        for (int i2 = 0; i2 < 2; ++i2) {
+            for (int i3 = 0; i3 < 2; ++i3) {
+                for (int i4 = 0; i4 < 2; ++i4) {
+                    std::vector<int> tag_vector = {i1, i2, i3, i4};
+                    switch (i1+i2+i3+i4){
+                        // TODO: what to return if none of the faces are tagged? NULL?
+                        // Maybe construct a trimesh with 0 vertices
+                        case 1:
+                            check_new_mesh(tm, tag_vector, true, 3, 3, 1); break;
+                        case 2:
+                            check_new_mesh(tm, tag_vector, true, 4, 5, 2); break;
+                        case 3:
+                            check_new_mesh(tm, tag_vector, true, 4, 6, 3); break;
+                        case 4:
+                            check_new_mesh(tm, tag_vector, true, 4, 6, 4); break;
+                    }
+                }
+            }
+        }
+    }
 
-    wmtk::ParaviewWriter writer("mesh_smooth", "vertices", new_tm, true, true, true, false);
-    new_tm.serialize(writer);
 }
