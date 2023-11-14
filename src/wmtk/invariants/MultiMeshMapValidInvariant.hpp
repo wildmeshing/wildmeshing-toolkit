0b87441614c0a9d0cdff69612a85cf974c438fd4
zlyfunction <zhuleyi001@126.com>
2023-11-14 12:58:39 -0500

    add comments

--- src/wmtk/invariants/MultiMeshMapValidInvariant.hpp
+++ src/wmtk/invariants/MultiMeshMapValidInvariant.hpp
@@ -6,6 +6,15 @@ namespace wmtk {
 class MultiMeshMapValidInvariant : public MeshInvariant
 {
 public:
+    /*@brief The MultiMeshMapValidInvariant checks whether the MultiMeshMap will remain valid after
+     * a collapse operation. It detects situations where the edge targeted for the collapse
+     * operation is not in child_meshes, but both of ears are in child_meshes
+     *
+     * @param m The mesh to check
+     * @param t The tuple to check
+     * @return True if the MultiMeshMap will still be valid after collapse operation
+     */
+
     MultiMeshMapValidInvariant(const Mesh& m);
     bool before(const Tuple& t) const override;
 };
