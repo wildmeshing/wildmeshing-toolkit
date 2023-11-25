ffc822488507ddd5b5b2fb6287ee43d40b193027
Michael Tao <michael.tao@nyu.edu>
2023-11-25 14:32:55 -0500

    minor organizsational changes to ATOperationBase and made it compile

--- components/wmtk_components/adaptive_tessellation/src/wmtk/components/adaptive_tessellation/operations/ATOperationBase.cpp
+++ components/wmtk_components/adaptive_tessellation/src/wmtk/components/adaptive_tessellation/operations/ATOperationBase.cpp
@@ -3,19 +3,15 @@
 namespace wmtk::components::adaptive_tessellation::operations {
 using namespace wmtk::operations;
 ATOperationBase::ATOperationBase(
-    const TriMesh& uv_mesh,
-    const TriMesh& position_mesh,
-    const std::vector<EdgeMesh>& edge_meshes,
-    const std::map<Mesh, Mesh>& edge_meshes_map,
+    TriMesh& position_mesh,
     const Tuple& t,
     const OperationSettings<ATOperationBase>& settings)
-    : TriMeshOperation(uv_mesh)
+    : TriMeshOperation(position_mesh)
     , TupleOperation(settings.invariants, t)
-    , m_position_mesh(position_mesh)
-    , m_edge_meshes(edge_meshes)
-    , m_sibling_meshes_map(edge_meshes_map)
 {
     // settings.initialize_invariants(uv_mesh);
 }
-bool ATOperationBase::execute() {}
+bool ATOperationBase::execute() {
+    return true;
+}
 } // namespace wmtk::components::adaptive_tessellation::operations
