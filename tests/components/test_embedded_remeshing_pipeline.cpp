d06ac4e039f9916558687ffda6c0600b64d1b589
zhouyuan chen <867442167@qq.com>
2023-11-28 10:49:28 -0500

    added new tests

--- tests/components/test_embedded_remeshing_pipeline.cpp
+++ tests/components/test_embedded_remeshing_pipeline.cpp
@@ -36,180 +36,6 @@ using namespace wmtk;
 
 const std::filesystem::path data_dir = WMTK_DATA_DIR;
 
-TEST_CASE("matrix_load", "[pipeline][.]")
-{
-    SECTION("trimesh_load")
-    {
-        std::vector<std::vector<long>> labels;
-        for (long j = 0; j < 22; ++j) {
-            std::vector<long> line;
-            line.reserve(20);
-            for (long i = 0; i < 20; ++i) {
-                if ((i - 10) * (i - 10) + (j - 11) * (j - 11) < 36) {
-                    line.push_back(1);
-                } else {
-                    line.push_back(0);
-                }
-            }
-            labels.push_back(line);
-        }
-        TriMesh mesh;
-        wmtk::components::internal::load_matrix_in_trimesh(mesh, labels);
-        if (true) {
-            ParaviewWriter
-                writer(data_dir / "trimesh_matrix_load", "position", mesh, true, true, true, false);
-            mesh.serialize(writer);
-        }
-    }
-    SECTION("tetmesh_load")
-    {
-        std::vector<std::vector<std::vector<long>>> labels;
-        for (long k = 0; k < 24; ++k) {
-            std::vector<std::vector<long>> layer;
-            for (long j = 0; j < 22; ++j) {
-                std::vector<long> line;
-                line.reserve(20);
-                for (long i = 0; i < 20; ++i) {
-                    if ((i - 10) * (i - 10) + (j - 11) * (j - 11) + (k - 12) * (k - 12) < 36) {
-                        line.push_back(1);
-                    } else {
-                        line.push_back(0);
-                    }
-                }
-                layer.push_back(line);
-            }
-            labels.push_back(layer);
-        }
-        TetMesh mesh;
-        wmtk::components::internal::load_matrix_in_tetmesh(mesh, labels);
-        if (true) {
-            ParaviewWriter
-                writer(data_dir / "tetmesh_matrix_load", "position", mesh, true, true, true, true);
-            mesh.serialize(writer);
-        }
-    }
-}
-
-TEST_CASE("embedded_remeshing_relocation_helper_function_invert", "[pipeline][helper_function]")
-{
-    SECTION("invert_2D-should_fail")
-    {
-        //   3 ----------- 4
-        //   |  \        / |
-        //   |   \  f3  /  |
-        //   |    \    /   |
-        //   |     \  /    |
-        //   |      0      |
-        //   |     / \     |
-        //   | f1 /   \ f2 |
-        //   |   / f0  \   |
-        //   |  /       \  |
-        //   1  ---------  2
-        //
-        tests::DEBUG_TriMesh mesh = wmtk::tests::two_neighbors_plus_one();
-        Eigen::MatrixXd V(5, 3);
-        V.row(0) << 0, 0, 0;
-        V.row(1) << -1, -1, 0;
-        V.row(2) << 1, -1, 0;
-        V.row(3) << -1, 1, 0;
-        V.row(4) << 1, 1, 0;
-        MeshAttributeHandle<double> pos_handle =
-            mesh_utils::set_matrix_attribute(V, "position", PrimitiveType::Vertex, mesh);
-
-        CHECK(!wmtk::operations::utils::is_invert(
-            mesh,
-            pos_handle,
-            mesh.get_all(PrimitiveType::Vertex)[0],
-            PrimitiveType::Face));
-    }
-    SECTION("invert_2D-should_pass")
-    {
-        //   3 ----------- 4
-        //   |  \        / |
-        //   |   \  f3  /  |
-        //   |    \    /   |
-        //   |     \  /    |
-        //   |      0      |
-        //   |     / \     |
-        //   | f1 /   \ f2 |
-        //   |   / f0  \   |
-        //   |  /       \  |
-        //   1  ---------  2
-        //
-        tests::DEBUG_TriMesh mesh = wmtk::tests::two_neighbors_plus_one();
-        Eigen::MatrixXd V(5, 3);
-        V.row(0) << 5, 0, 0;
-        V.row(1) << -1, -1, 0;
-        V.row(2) << 1, -1, 0;
-        V.row(3) << -1, 1, 0;
-        V.row(4) << 1, 1, 0;
-        MeshAttributeHandle<double> pos_handle =
-            mesh_utils::set_matrix_attribute(V, "position", PrimitiveType::Vertex, mesh);
-
-        CHECK(wmtk::operations::utils::is_invert(
-            mesh,
-            pos_handle,
-            mesh.get_all(PrimitiveType::Vertex)[0],
-            PrimitiveType::Face));
-    }
-}
-
-TEST_CASE("embedded_remeshing_relocation_helper_function_push", "[pipeline][helper_function]")
-{
-    SECTION("helper_function_push")
-    {
-        //    0---1---2
-        //   /0\1/2\3/4\ .
-        //  3---4---5---6
-        //   \5/6\7/  .
-        //    7---8
-        tests::DEBUG_TriMesh m = wmtk::tests::hex_plus_two_with_position();
-        MeshAttributeHandle<long> edge_tag_handle =
-            m.register_attribute<long>("edge_tag", PrimitiveType::Edge, 1);
-        MeshAttributeHandle<long> vertex_tag_handle =
-            m.register_attribute<long>("vertex_tag", PrimitiveType::Vertex, 1);
-        MeshAttributeHandle<long> todo_tag_handle =
-            m.register_attribute<long>("todo_tag", PrimitiveType::Vertex, 1, false, 1);
-        Accessor<long> acc_edge_tag = m.create_accessor(edge_tag_handle);
-        Accessor<long> acc_vertex_tag = m.create_accessor(vertex_tag_handle);
-        acc_vertex_tag.scalar_attribute(m.get_all(PrimitiveType::Vertex)[4]) = 2;
-        acc_vertex_tag.scalar_attribute(m.get_all(PrimitiveType::Vertex)[5]) = 1;
-        acc_vertex_tag.scalar_attribute(m.get_all(PrimitiveType::Vertex)[6]) = 1;
-        acc_vertex_tag.scalar_attribute(m.get_all(PrimitiveType::Vertex)[7]) = 1;
-        acc_vertex_tag.scalar_attribute(m.get_all(PrimitiveType::Vertex)[8]) = 1;
-        acc_edge_tag.scalar_attribute(m.edge_tuple_between_v1_v2(7, 8, 6)) = 1;
-        acc_edge_tag.scalar_attribute(m.edge_tuple_between_v1_v2(5, 8, 7)) = 1;
-        acc_edge_tag.scalar_attribute(m.edge_tuple_between_v1_v2(5, 6, 4)) = 1;
-        // wmtk::simplex::SimplexCollection sc =
-        //     simplex::link(m, Simplex(PrimitiveType::Vertex,
-        //     m.get_all(PrimitiveType::Vertex)[4]));
-        // int size = 0;
-        // for (const Simplex& s : sc.simplex_vector(PrimitiveType::Edge)) {
-        //     CHECK(acc_edge_tag.scalar_attribute(s.tuple()) == 1);
-        //     ++size;
-        // }
-        // CHECK(size == 6);
-
-        operations::OperationSettings<operations::tri_mesh::VertexPushOffset> settings;
-        settings.edge_tag_handle = edge_tag_handle;
-        settings.embedding_tag_value = 0;
-        settings.input_tag_value = 1;
-        settings.offset_len = 5;
-        settings.offset_tag_value = 2;
-        settings.position = m.get_attribute_handle<double>("position", PrimitiveType::Vertex);
-        settings.vertex_tag_handle = vertex_tag_handle;
-        settings.todo_tag_handle = todo_tag_handle;
-        settings.initialize_invariants(m);
-        operations::tri_mesh::VertexPushOffset op(m, m.get_all(PrimitiveType::Vertex)[4], settings);
-        op();
-
-        if (false) {
-            ParaviewWriter writer(data_dir / "push_result", "position", m, true, true, true, false);
-            m.serialize(writer);
-        }
-    }
-}
-
 TEST_CASE("embedded_remeshing_2D_pipeline", "[pipeline][2D][.]")
 {
     // std::vector<std::vector<long>> labels;
@@ -309,7 +135,7 @@ TEST_CASE("embedded_remeshing_2D_pipeline", "[pipeline][2D][.]")
         mesh.serialize(hdfwriter);
     }
 
-    int iteration_time = 0;
+    int iteration_time = 1;
     wmtk::components::internal::EmbeddedRemeshing er(
         vertex_tag_handle,
         edge_tag_handle,
@@ -378,10 +204,10 @@ TEST_CASE("state_continue2D", "[.]")
     }
 }
 
-TEST_CASE("embedded_remeshing_3D_pipeline", "[pipeline][3D][.]")
+TEST_CASE("embedded_remeshing_3D_pipeline", "[pipeline][3D]")
 {
-    long dim_len = 10;
-    long r = 3;
+    long dim_len = 2;
+    long r = 1;
     std::vector<std::vector<std::vector<long>>> labels;
     for (long k = 0; k < dim_len; ++k) {
         std::vector<std::vector<long>> layer;
@@ -438,7 +264,7 @@ TEST_CASE("embedded_remeshing_3D_pipeline", "[pipeline][3D][.]")
         mesh.serialize(hdfwriter);
     }
 
-    int iteration_time = 0;
+    int iteration_time = 1;
     wmtk::components::internal::EmbeddedRemeshing er(
         vertex_tag_handle,
         edge_tag_handle,
@@ -504,6 +330,183 @@ TEST_CASE("state_continue3D", "[.]")
 
         HDF5Writer hdfwriter(data_dir / ("save.hdf5"));
         mesh.serialize(hdfwriter);
+    }
+}
+
+TEST_CASE("matrix_load", "[pipeline]")
+{
+    SECTION("trimesh_load")
+    {
+        std::vector<std::vector<long>> labels;
+        for (long j = 0; j < 11; ++j) {
+            std::vector<long> line;
+            line.reserve(10);
+            for (long i = 0; i < 10; ++i) {
+                if ((i - 5) * (i - 5) + (j - 5) * (j - 5) < 9) {
+                    line.push_back(1);
+                } else {
+                    line.push_back(0);
+                }
+            }
+            labels.push_back(line);
+        }
+        TriMesh mesh;
+        wmtk::components::internal::load_matrix_in_trimesh(mesh, labels);
+        if (true) {
+            ParaviewWriter
+                writer(data_dir / "trimesh_matrix_load", "position", mesh, true, true, true, false);
+            mesh.serialize(writer);
+        }
+    }
+    SECTION("tetmesh_load")
+    {
+        std::vector<std::vector<std::vector<long>>> labels;
+        for (long k = 0; k < 12; ++k) {
+            std::vector<std::vector<long>> layer;
+            for (long j = 0; j < 11; ++j) {
+                std::vector<long> line;
+                line.reserve(10);
+                for (long i = 0; i < 10; ++i) {
+                    if ((i - 5) * (i - 5) + (j - 5) * (j - 5) + (k - 6) * (k - 6) < 9) {
+                        line.push_back(1);
+                    } else {
+                        line.push_back(0);
+                    }
+                }
+                layer.push_back(line);
+            }
+            labels.push_back(layer);
+        }
+        TetMesh mesh;
+        wmtk::components::internal::load_matrix_in_tetmesh(mesh, labels);
+        if (false) {
+            ParaviewWriter
+                writer(data_dir / "tetmesh_matrix_load", "position", mesh, true, true, true, true);
+            mesh.serialize(writer);
+        }
+    }
+}
+
+TEST_CASE("embedded_remeshing_relocation_helper_function_invert", "[pipeline][helper_function]")
+{
+    SECTION("invert_2D-should_fail")
+    {
+        //   3 ----------- 4
+        //   |  \        / |
+        //   |   \  f3  /  |
+        //   |    \    /   |
+        //   |     \  /    |
+        //   |      0      |
+        //   |     / \     |
+        //   | f1 /   \ f2 |
+        //   |   / f0  \   |
+        //   |  /       \  |
+        //   1  ---------  2
+        //
+        tests::DEBUG_TriMesh mesh = wmtk::tests::two_neighbors_plus_one();
+        Eigen::MatrixXd V(5, 3);
+        V.row(0) << 0, 0, 0;
+        V.row(1) << -1, -1, 0;
+        V.row(2) << 1, -1, 0;
+        V.row(3) << -1, 1, 0;
+        V.row(4) << 1, 1, 0;
+        MeshAttributeHandle<double> pos_handle =
+            mesh_utils::set_matrix_attribute(V, "position", PrimitiveType::Vertex, mesh);
+
+        CHECK(!wmtk::operations::utils::is_invert(
+            mesh,
+            pos_handle,
+            mesh.get_all(PrimitiveType::Vertex)[0],
+            PrimitiveType::Face));
+    }
+    SECTION("invert_2D-should_pass")
+    {
+        //   3 ----------- 4
+        //   |  \        / |
+        //   |   \  f3  /  |
+        //   |    \    /   |
+        //   |     \  /    |
+        //   |      0      |
+        //   |     / \     |
+        //   | f1 /   \ f2 |
+        //   |   / f0  \   |
+        //   |  /       \  |
+        //   1  ---------  2
+        //
+        tests::DEBUG_TriMesh mesh = wmtk::tests::two_neighbors_plus_one();
+        Eigen::MatrixXd V(5, 3);
+        V.row(0) << 5, 0, 0;
+        V.row(1) << -1, -1, 0;
+        V.row(2) << 1, -1, 0;
+        V.row(3) << -1, 1, 0;
+        V.row(4) << 1, 1, 0;
+        MeshAttributeHandle<double> pos_handle =
+            mesh_utils::set_matrix_attribute(V, "position", PrimitiveType::Vertex, mesh);
+
+        CHECK(wmtk::operations::utils::is_invert(
+            mesh,
+            pos_handle,
+            mesh.get_all(PrimitiveType::Vertex)[0],
+            PrimitiveType::Face));
+    }
+}
+
+TEST_CASE("embedded_remeshing_relocation_helper_function_push", "[pipeline][helper_function]")
+{
+    SECTION("helper_function_push")
+    {
+        //    0---1---2
+        //   /0\1/2\3/4\ .
+        //  3---4---5---6
+        //   \5/6\7/  .
+        //    7---8
+        //
+        //    0---1---2
+        //   / \ / \ / \ .
+        //  3---4---5---6
+        //   \ / \ / \ /
+        //    7---8---9
+        tests::DEBUG_TriMesh m = wmtk::tests::edge_region_with_position();
+        MeshAttributeHandle<long> edge_tag_handle =
+            m.register_attribute<long>("edge_tag", PrimitiveType::Edge, 1);
+        MeshAttributeHandle<long> vertex_tag_handle =
+            m.register_attribute<long>("vertex_tag", PrimitiveType::Vertex, 1);
+        MeshAttributeHandle<long> todo_tag_handle =
+            m.register_attribute<long>("todo_tag", PrimitiveType::Vertex, 1, false, 1);
+        Accessor<long> acc_edge_tag = m.create_accessor(edge_tag_handle);
+        Accessor<long> acc_vertex_tag = m.create_accessor(vertex_tag_handle);
+        acc_vertex_tag.scalar_attribute(m.get_all(PrimitiveType::Vertex)[3]) = 1;
+        acc_vertex_tag.scalar_attribute(m.get_all(PrimitiveType::Vertex)[7]) = 1;
+        acc_vertex_tag.scalar_attribute(m.get_all(PrimitiveType::Vertex)[8]) = 1;
+        acc_vertex_tag.scalar_attribute(m.get_all(PrimitiveType::Vertex)[4]) = 2;
+        acc_vertex_tag.scalar_attribute(m.get_all(PrimitiveType::Vertex)[5]) = 2;
+        acc_edge_tag.scalar_attribute(m.edge_tuple_between_v1_v2(3, 7, 5)) = 1;
+        acc_edge_tag.scalar_attribute(m.edge_tuple_between_v1_v2(7, 8, 6)) = 1;
+
+        operations::OperationSettings<operations::tri_mesh::VertexPushOffset> settings;
+        settings.edge_tag_handle = edge_tag_handle;
+        settings.embedding_tag_value = 0;
+        settings.input_tag_value = 1;
+        settings.offset_len = 5;
+        settings.offset_tag_value = 2;
+        settings.position = m.get_attribute_handle<double>("position", PrimitiveType::Vertex);
+        settings.vertex_tag_handle = vertex_tag_handle;
+        settings.todo_tag_handle = todo_tag_handle;
+        settings.initialize_invariants(m);
+        operations::tri_mesh::VertexPushOffset op1(
+            m,
+            m.get_all(PrimitiveType::Vertex)[4],
+            settings);
+        CHECK(op1());
+        operations::tri_mesh::VertexPushOffset op2(
+            m,
+            m.get_all(PrimitiveType::Vertex)[5],
+            settings);
+        CHECK(op2());
+        if (false) {
+            ParaviewWriter writer(data_dir / "push_result", "position", m, true, true, true, false);
+            m.serialize(writer);
+        }
     }
 }
 
