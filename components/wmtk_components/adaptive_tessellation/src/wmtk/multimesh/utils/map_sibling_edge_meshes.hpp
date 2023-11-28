816168e9919ac4ec87565347091f6e86a11dd28f
yunfanzhou <yfzhou42@gmail.com>
2023-11-27 22:15:01 -0500

    compiled multi mesh edge mesh pairing

--- components/wmtk_components/adaptive_tessellation/src/wmtk/multimesh/utils/map_sibling_edge_meshes.hpp
+++ components/wmtk_components/adaptive_tessellation/src/wmtk/multimesh/utils/map_sibling_edge_meshes.hpp
@@ -4,6 +4,6 @@
 #include <wmtk/Tuple.hpp>
 #include <wmtk/attribute/Attribute.hpp>
 
-namespace wmtk::multimesh::utils {
-std::map<Mesh*, Mesh*> map_sibling_edge_meshes(Mesh& position_mesh);
+namespace wmtk::components::adaptive_tessellation::multimesh::utils {
+std::map<Mesh*, Mesh*> map_sibling_edge_meshes(const Mesh& position_mesh);
 }
