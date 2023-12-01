6e2a67e98ed1ba59819630273857fea663635f5e
yunfanzhou <yfzhou42@gmail.com>
2023-12-01 18:14:46 -0500

    Use Mesh for initiation of ATOperation

--- components/wmtk_components/adaptive_tessellation/src/wmtk/components/adaptive_tessellation/operations/internal/ATOperation.cpp
+++ components/wmtk_components/adaptive_tessellation/src/wmtk/components/adaptive_tessellation/operations/internal/ATOperation.cpp
@@ -4,19 +4,19 @@
 namespace wmtk::components::adaptive_tessellation::operations::internal {
 using namespace wmtk;
 ATOperation::ATOperation(
-    TriMesh& uv_mesh,
-    TriMesh& position_mesh,
-    std::vector<std::shared_ptr<EdgeMesh>> edge_mesh_ptrs,
+    Mesh& uv_mesh,
+    Mesh& position_mesh,
+    std::vector<std::shared_ptr<Mesh>> edge_mesh_ptrs,
     std::map<Mesh*, Mesh*> sibling_meshes_map,
     MeshAttributeHandle<double>& uv_handle)
-    : m_uv_mesh_ptr(std::make_shared<TriMesh>(uv_mesh))
-    , m_position_mesh_ptr(std::make_shared<TriMesh>(position_mesh))
+    : m_uv_mesh_ptr(std::make_shared<TriMesh>(static_cast<TriMesh&>(uv_mesh)))
+    , m_position_mesh_ptr(std::make_shared<TriMesh>(static_cast<TriMesh&>(position_mesh)))
     , m_edge_mesh_ptrs(edge_mesh_ptrs)
     , m_sibling_meshes_map(sibling_meshes_map)
     , m_uv_handle(uv_handle)
 {}
 
-void ATOperation::initialize_invariants(const Mesh& input_mesh, const TriMesh& uv_mesh)
+void ATOperation::initialize_invariants(const Mesh& input_mesh, const Mesh& uv_mesh)
 {
     // outdated + is valid tuple
     invariants = basic_invariant_collection(input_mesh);
@@ -34,7 +34,7 @@ Mesh* ATOperation::sibling_edge_mesh_raw_ptr(Mesh* my_edge_mesh_ptr)
 {
     return m_sibling_meshes_map[my_edge_mesh_ptr];
 }
-const std::shared_ptr<EdgeMesh> ATOperation::edge_mesh_ptr(long i) const
+const std::shared_ptr<Mesh> ATOperation::edge_mesh_ptr(long i) const
 {
     return m_edge_mesh_ptrs[i];
 }
--- components/wmtk_components/adaptive_tessellation/src/wmtk/components/adaptive_tessellation/operations/internal/ATOperation.hpp
+++ components/wmtk_components/adaptive_tessellation/src/wmtk/components/adaptive_tessellation/operations/internal/ATOperation.hpp
@@ -12,7 +12,7 @@ class ATOperation
 {
     std::shared_ptr<TriMesh> m_uv_mesh_ptr;
     std::shared_ptr<TriMesh> m_position_mesh_ptr;
-    std::vector<std::shared_ptr<EdgeMesh>> m_edge_mesh_ptrs;
+    std::vector<std::shared_ptr<Mesh>> m_edge_mesh_ptrs;
     std::map<Mesh*, Mesh*> m_sibling_meshes_map;
 
 public:
@@ -21,22 +21,22 @@ public:
     // op input_m = uv_mesh, boundary edge op input_m = position_mesh)
     // The invariant that is shared among the operations besides the base invariants is the
     // no-triangle-inversion of the uv_mesh
-    void initialize_invariants(const Mesh& input_m, const TriMesh& uv_m);
+    void initialize_invariants(const Mesh& input_m, const Mesh& uv_m);
 
     // handle to vertex uv coordinates used for the uv non-inversion invariants
     MeshAttributeHandle<double> m_uv_handle;
     // Scheduler m_scheduler;
 
     ATOperation(
-        TriMesh& uv_mesh,
-        TriMesh& position_mesh,
-        std::vector<std::shared_ptr<EdgeMesh>> edge_mesh_ptrs,
+        Mesh& uv_mesh,
+        Mesh& position_mesh,
+        std::vector<std::shared_ptr<Mesh>> edge_mesh_ptrs,
         std::map<Mesh*, Mesh*> sibling_meshes_map,
         MeshAttributeHandle<double>& uv_handle);
 
     const std::shared_ptr<TriMesh> uv_mesh_ptr() const;
     const std::shared_ptr<TriMesh> position_mesh_ptr() const;
-    const std::shared_ptr<EdgeMesh> edge_mesh_ptr(long i) const;
+    const std::shared_ptr<Mesh> edge_mesh_ptr(long i) const;
     Mesh* sibling_edge_mesh_raw_ptr(Mesh* my_edge_mesh_ptr);
     Simplex sibling_edge(Mesh* my_edge_mesh_ptr, const Simplex& s);
 };
