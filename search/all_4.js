b716c3b10fa744715823fb15b4feacf5c394ddec
mtao <michael.tao@cims.nyu.edu>
2023-12-02 04:33:49 +0000

    Deploying to gh-pages from @ wildmeshing/wildmeshing-toolkit@6bef8778b1157f1764b6222ce9aca8adc81bcb8a 🚀

--- search/all_4.js
+++ search/all_4.js
@@ -15,6 +15,6 @@ var searchData=
   ['edgeswapbase_63',['EdgeSwapBase',['../classwmtk_1_1operations_1_1tri__mesh_1_1_edge_swap_base.html',1,'wmtk::operations::tri_mesh']]],
   ['edgeswapvalence_64',['EdgeSwapValence',['../classwmtk_1_1operations_1_1tri__mesh_1_1_edge_swap_valence.html',1,'wmtk::operations::tri_mesh']]],
   ['embedded_5fdimension_65',['embedded_dimension',['../classwmtk_1_1function_1_1_differentiable_function.html#a69efb2443b9eaa8fc54861b0e8647a66',1,'wmtk::function::DifferentiableFunction']]],
-  ['eval_66',['eval',['../classwmtk_1_1function_1_1_a_m_i_p_s.html#aa28fa05c07a60efd393e8cadf194fe92',1,'wmtk::function::AMIPS::eval()'],['../classwmtk_1_1function_1_1_autodiff_function.html#a83608b0de57190f5e29cc1cb1558aa99',1,'wmtk::function::AutodiffFunction::eval()']]],
+  ['eval_66',['eval',['../classwmtk_1_1function_1_1_a_m_i_p_s.html#a8d59b9d5013a4a585f58ec4933d52a5e',1,'wmtk::function::AMIPS::eval()'],['../classwmtk_1_1function_1_1_triangle_autodiff_function.html#ad2789c926e4826967a5073e150ea8f35',1,'wmtk::function::TriangleAutodiffFunction::eval()']]],
   ['export_5fcache_67',['export_cache',['../classwmtk_1_1io_1_1_cache.html#ae264d81e36515e42718a72469acb3a2a',1,'wmtk::io::Cache']]]
 ];
