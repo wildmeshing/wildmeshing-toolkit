981a8f2fa853768faddeb3279033f1671c7bdd2a
Daniel Zint <daniel.zint@mail.de>
2023-12-11 19:49:04 -0500

    Add TriMesh invariant code. Not tested yet.

--- src/wmtk/invariants/SubstructureTopologyPreservingInvariant.cpp
+++ src/wmtk/invariants/SubstructureTopologyPreservingInvariant.cpp
@@ -37,22 +37,102 @@ bool SubstructureTopologyPreservingInvariant::before_tri(const Tuple& t) const
 {
     using namespace simplex;
 
+    const auto edge_tag_acc = mesh().create_const_accessor(m_substructure_edge_tag_handle);
+
     // edge e = (u,v)
 
     Simplex edge_e(PrimitiveType::Edge, t);
     Simplex vertex_u(PrimitiveType::Vertex, t);
     Simplex vertex_v(PrimitiveType::Vertex, mesh().switch_vertex(t));
 
-    SimplexCollection link_of_u = link(mesh(), vertex_u);
-    // for (const Simplex& f_u :
-    //      cofaces_single_dimension_simplices(mesh(), vertex_u, PrimitiveType::Face)) {
-    //    //
-    //}
+    RawSimplexCollection lk_u_0(link(mesh(), vertex_u));
+    RawSimplexCollection lk_u_1;
+    RawSimplexCollection lk_u_2;
 
-    SimplexCollection link_of_v = link(mesh(), vertex_v);
+    for (const Simplex& e_u :
+         cofaces_single_dimension_simplices(mesh(), vertex_u, PrimitiveType::Edge)) {
+        if (edge_tag_acc.const_scalar_attribute(e_u.tuple()) == m_substructure_tag_value) {
+            std::vector<Tuple> vertices_dummy_tri =
+                faces_single_dimension_tuples(mesh(), e_u, PrimitiveType::Vertex);
+            vertices_dummy_tri.emplace_back(Tuple()); // add dummy vertex
 
-    throw std::runtime_error("not implemented");
-    return false;
+            RawSimplex dummy_tri(mesh(), vertices_dummy_tri);
+            RawSimplex opp_dummy_edge = dummy_tri.opposite_face(mesh(), vertex_u.tuple());
+            lk_u_0.add(opp_dummy_edge);
+            lk_u_0.add(opp_dummy_edge.faces());
+
+            lk_u_1.add(opp_dummy_edge);
+            lk_u_1.add(opp_dummy_edge.faces());
+
+            lk_u_2.add(opp_dummy_edge.opposite_face(mesh(), vertex_u.tuple()));
+        }
+    }
+
+    // TODO if u is an order 3 vertex ...
+
+    lk_u_0.sort_and_clean();
+    lk_u_1.sort_and_clean();
+    lk_u_2.sort_and_clean();
+
+    // same code for vertex v
+
+    RawSimplexCollection lk_v_0(link(mesh(), vertex_v));
+    RawSimplexCollection lk_v_1;
+    RawSimplexCollection lk_v_2;
+
+    for (const Simplex& e_v :
+         cofaces_single_dimension_simplices(mesh(), vertex_v, PrimitiveType::Edge)) {
+        if (edge_tag_acc.const_scalar_attribute(e_v.tuple()) == m_substructure_tag_value) {
+            std::vector<Tuple> vertices_dummy_tri =
+                faces_single_dimension_tuples(mesh(), e_v, PrimitiveType::Vertex);
+            vertices_dummy_tri.emplace_back(Tuple()); // add dummy vertex
+
+            RawSimplex dummy_tri(mesh(), vertices_dummy_tri);
+            RawSimplex opp_dummy_edge = dummy_tri.opposite_face(mesh(), vertex_v.tuple());
+            lk_v_0.add(opp_dummy_edge);
+            lk_v_0.add(opp_dummy_edge.faces());
+
+            lk_v_1.add(opp_dummy_edge);
+            lk_v_1.add(opp_dummy_edge.faces());
+
+            lk_v_2.add(opp_dummy_edge.opposite_face(mesh(), vertex_v.tuple()));
+        }
+    }
+
+    // TODO if v is an order 3 vertex ...
+
+    lk_v_0.sort_and_clean();
+    lk_v_1.sort_and_clean();
+    lk_v_2.sort_and_clean();
+
+    if (!RawSimplexCollection::get_intersection(lk_u_2, lk_v_2).simplex_vector().empty()) {
+        return false;
+    }
+
+    // lk_e
+    RawSimplexCollection lk_e_0(link(mesh(), edge_e));
+    RawSimplexCollection lk_e_1;
+
+    RawSimplex raw_edge_e(mesh(), edge_e);
+
+    if (edge_tag_acc.const_scalar_attribute(edge_e.tuple()) == m_substructure_tag_value) {
+        RawSimplex dummy_vertex({-1});
+        lk_e_0.add(dummy_vertex);
+        lk_e_1.add(dummy_vertex);
+    }
+
+    RawSimplexCollection intersection_u_v_0 =
+        RawSimplexCollection::get_intersection(lk_u_0, lk_v_0);
+    if (!RawSimplexCollection::are_simplex_collections_equal(intersection_u_v_0, lk_e_0)) {
+        return false;
+    }
+    RawSimplexCollection intersection_u_v_1 =
+        RawSimplexCollection::get_intersection(lk_u_1, lk_v_1);
+    if (!RawSimplexCollection::are_simplex_collections_equal(intersection_u_v_1, lk_e_1)) {
+        return false;
+    }
+
+    return true;
 }
 
 bool SubstructureTopologyPreservingInvariant::before_tet(const Tuple& t) const
