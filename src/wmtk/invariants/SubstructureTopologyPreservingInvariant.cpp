af81eac05c0c7a3de88c69cf8042ae8303e5f743
Daniel Zint <daniel.zint@mail.de>
2023-12-12 15:14:26 -0500

    guessing wildly what the typo in the invariant algorithm is

--- src/wmtk/invariants/SubstructureTopologyPreservingInvariant.cpp
+++ src/wmtk/invariants/SubstructureTopologyPreservingInvariant.cpp
@@ -310,7 +310,7 @@ bool SubstructureTopologyPreservingInvariant::before_tet(const Tuple& t) const
             lk_e_0.add(dummy_edge);
             lk_e_0.add(dummy_edge.faces());
 
-            lk_e_1.add(dummy_edge);
+            // lk_e_1.add(dummy_edge);
             lk_e_1.add(dummy_edge.faces());
             lk_e_1.add(dummy_tet.opposite_face(raw_f_e));
         }
