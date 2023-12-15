912bee615487ad8b394f9d2c6ccf9aa4537609f3
yunfanzhou <yfzhou42@gmail.com>
2023-12-15 17:36:55 -0500

    TextureIntegral Implemented as utils for texture integral energy

--- src/wmtk/function/utils/IntegralBase.hpp
+++ src/wmtk/function/utils/IntegralBase.hpp
@@ -1,22 +1,22 @@
 #pragma once
-
+#include <wmtk/components/adaptive_tessellation/image/utils/SamplingParameters.hpp>
 namespace wmtk {
-
+namespace image = wmtk::components::adaptive_tessellation::image;
 class IntegralBase
 {
 public:
-    enum class IntegrationMethod { Exact, Adaptive };
+    // enum class IntegrationMethod { Exact, Adaptive };
 
     enum class QuadratureOrder { Full, Reduced };
 
 public:
-    void set_sampling_method(SamplingMethod method) { m_sampling_method = method; }
-    void set_integration_method(IntegrationMethod method) { m_integration_method = method; }
+    void set_sampling_method(image::SAMPLING_METHOD method) { m_sampling_method = method; }
+    // void set_integration_method(IntegrationMethod method) { m_integration_method = method; }
     void set_quadrature_order(QuadratureOrder order) { m_quadrature_order = order; }
 
 protected:
-    SamplingMethod m_sampling_method = SamplingMethod::Bicubic;
-    IntegrationMethod m_integration_method = IntegrationMethod::Exact;
+    image::SAMPLING_METHOD m_sampling_method = image::SAMPLING_METHOD::Bicubic;
+    // IntegrationMethod m_integration_method = IntegrationMethod::Exact;
     QuadratureOrder m_quadrature_order = QuadratureOrder::Reduced;
 };
 
