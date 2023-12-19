6f6ea457e31832572a9ffa18d80aea0f14671e1c
Daniel Zint <daniel.zint@mail.de>
2023-12-19 15:33:39 -0500

    Add Section in test.

--- tests/components/test_component_tag_intersection.cpp
+++ tests/components/test_component_tag_intersection.cpp
@@ -32,32 +32,37 @@ TEST_CASE("component_tag_intersection_tri", "[components][tag_intersection]")
 {
     tests::DEBUG_TriMesh m = tests::edge_region();
 
-    auto tag1 = m.register_attribute<long>("tag1", PrimitiveType::Face, 1, 0);
-    auto tag2 = m.register_attribute<long>("tag2", PrimitiveType::Face, 1, 0);
-
+    SECTION("two_face_tags")
     {
-        auto tag1_acc = m.create_accessor(tag1);
-        auto tag2_acc = m.create_accessor(tag2);
-        tag1_acc.scalar_attribute(m.face_tuple_from_vids(1, 4, 5)) = 1;
-        tag2_acc.scalar_attribute(m.face_tuple_from_vids(4, 8, 5)) = 1;
+        auto tag1 = m.register_attribute<long>("tag1", PrimitiveType::Face, 1, 0);
+        auto tag2 = m.register_attribute<long>("tag2", PrimitiveType::Face, 1, 0);
+
+        {
+            auto tag1_acc = m.create_accessor(tag1);
+            auto tag2_acc = m.create_accessor(tag2);
+            tag1_acc.scalar_attribute(m.face_tuple_from_vids(1, 4, 5)) = 1;
+            tag2_acc.scalar_attribute(m.face_tuple_from_vids(4, 8, 5)) = 1;
+        }
+
+        auto v_otag = m.register_attribute<long>("v_otag", PrimitiveType::Vertex, 1, 0);
+        auto e_otag = m.register_attribute<long>("e_otag", PrimitiveType::Edge, 1, 0);
+
+        std::vector<std::tuple<MeshAttributeHandle<long>, long>> input_tags = {
+            {tag1, 1},
+            {tag2, 1}};
+        std::vector<std::tuple<MeshAttributeHandle<long>, long>> output_tags = {
+            {v_otag, 1},
+            {e_otag, 1}};
+
+        components::tag_intersection_tri(m, input_tags, output_tags);
+
+        auto v_otag_acc = m.create_accessor(v_otag);
+        CHECK(v_otag_acc.const_scalar_attribute(m.edge_tuple_from_vids(4, 5)) == 1);
+        CHECK(v_otag_acc.const_scalar_attribute(m.edge_tuple_from_vids(5, 4)) == 1);
+        CHECK(v_otag_acc.const_scalar_attribute(m.edge_tuple_from_vids(1, 4)) == 0);
+        CHECK(v_otag_acc.const_scalar_attribute(m.edge_tuple_from_vids(8, 4)) == 0);
+
+        auto e_otag_acc = m.create_accessor(e_otag);
+        CHECK(e_otag_acc.const_scalar_attribute(m.edge_tuple_from_vids(4, 5)) == 1);
     }
-
-    auto v_otag = m.register_attribute<long>("v_otag", PrimitiveType::Vertex, 1, 0);
-    auto e_otag = m.register_attribute<long>("e_otag", PrimitiveType::Edge, 1, 0);
-
-    std::vector<std::tuple<MeshAttributeHandle<long>, long>> input_tags = {{tag1, 1}, {tag2, 1}};
-    std::vector<std::tuple<MeshAttributeHandle<long>, long>> output_tags = {
-        {v_otag, 1},
-        {e_otag, 1}};
-
-    components::tag_intersection_tri(m, input_tags, output_tags);
-
-    auto v_otag_acc = m.create_accessor(v_otag);
-    CHECK(v_otag_acc.const_scalar_attribute(m.edge_tuple_from_vids(4, 5)) == 1);
-    CHECK(v_otag_acc.const_scalar_attribute(m.edge_tuple_from_vids(5, 4)) == 1);
-    CHECK(v_otag_acc.const_scalar_attribute(m.edge_tuple_from_vids(1, 4)) == 0);
-    CHECK(v_otag_acc.const_scalar_attribute(m.edge_tuple_from_vids(8, 4)) == 0);
-
-    auto e_otag_acc = m.create_accessor(e_otag);
-    CHECK(e_otag_acc.const_scalar_attribute(m.edge_tuple_from_vids(4, 5)) == 1);
 }
