e6f529a94e31c3cdc7ba7d0ddd73918f2b8393a1
Teseo Schneider <teseo@bluewin.ch>
2023-12-20 17:18:31 -0800

    collapse

--- src/wmtk/operations/tri_mesh/EdgeCollapse.cpp
+++ src/wmtk/operations/tri_mesh/EdgeCollapse.cpp
@@ -1,101 +1,30 @@
 #include "EdgeCollapse.hpp"
 #include <spdlog/spdlog.h>
-#include <wmtk/SimplicialComplex.hpp>
-#include <wmtk/TriMesh.hpp>
-#include <wmtk/invariants/InteriorEdgeInvariant.hpp>
-#include <wmtk/invariants/MultiMeshLinkConditionInvariant.hpp>
-#include <wmtk/invariants/MultiMeshTopologyInvariant.hpp>
-#include <wmtk/invariants/find_invariant_in_collection_by_type.hpp>
+
 #include <wmtk/operations/utils/multi_mesh_edge_collapse.hpp>
-namespace wmtk::operations {
 
-void OperationSettings<tri_mesh::EdgeCollapse>::create_invariants()
-{
-    invariants = std::make_shared<InvariantCollection>(m_mesh);
-    invariants->add(std::make_shared<MultiMeshLinkConditionInvariant>(m_mesh));
-    if (!collapse_boundary_edges) {
-        invariants->add(std::make_shared<InteriorEdgeInvariant>(m_mesh));
-    }
-    if (!collapse_boundary_vertex_to_interior) {
-        invariants->add(std::make_shared<InteriorVertexInvariant>(m_mesh));
-    }
-    if (preserve_topology) {
-        std::cout << "adding topology invaiants!!!" << std::endl;
-        for (const auto& child_mesh : m_mesh.get_child_meshes()) {
-            if (child_mesh->top_simplex_type() == PrimitiveType::Edge) {
-                const EdgeMesh& child_edgemesh = dynamic_cast<const EdgeMesh&>(*child_mesh);
-                invariants->add(
-                    std::make_shared<MultiMeshEdgeTopologyInvariant>(m_mesh, child_edgemesh));
-            }
-        }
-    }
-}
+namespace wmtk::operations::tri_mesh {
 
-namespace tri_mesh {
 
-EdgeCollapse::EdgeCollapse(
-    Mesh& m,
-    const Simplex& t,
-    const OperationSettings<EdgeCollapse>& settings)
-    : EdgeCollapse(dynamic_cast<TriMesh&>(m), t, settings)
-{}
-EdgeCollapse::EdgeCollapse(
-    TriMesh& m,
-    const Simplex& t,
-    const OperationSettings<EdgeCollapse>& settings)
+EdgeCollapse::EdgeCollapse(Mesh& m)
     : TriMeshOperation(m)
-    , TupleOperation(settings.invariants, t)
-// , m_settings(settings)
+{}
+
+std::vector<Simplex> EdgeCollapse::execute(const Simplex& simplex)
 {
-    assert(t.primitive_type() == PrimitiveType::Edge);
-    // assert(m_settings.are_invariants_initialized());
+    auto return_data = operations::utils::multi_mesh_edge_collapse(mesh(), simplex.tuple());
+
+    const operations::tri_mesh::EdgeOperationData& my_data = return_data.get(tri_mesh(), simplex);
+
+    return {Simplex(PrimitiveType::Vertex, my_data.m_output_tuple)};
 }
 
-bool EdgeCollapse::execute()
+std::vector<Simplex> EdgeCollapse::unmodified_primitives(const Simplex& simplex) const
 {
-    auto return_data = operations::utils::multi_mesh_edge_collapse(mesh(), input_tuple());
-
-    const operations::tri_mesh::EdgeOperationData& my_data =
-        return_data.get(mesh(), input_simplex());
-    // move vertex to center of old vertices
-    m_output_tuple = my_data.m_output_tuple;
-
-    return true;
-}
-
-std::vector<Simplex> EdgeCollapse::modified_primitives() const
-{
-    return {Simplex(PrimitiveType::Vertex, m_output_tuple)};
-}
-
-std::vector<Simplex> EdgeCollapse::unmodified_primitives() const
-{
-    const simplex::Simplex v0 = simplex::Simplex::vertex(input_tuple());
+    const simplex::Simplex v0 = simplex::Simplex::vertex(simplex.tuple());
     const simplex::Simplex v1 = mesh().parent_scope(
-        [&]() { return simplex::Simplex::vertex(mesh().switch_vertex(input_tuple())); });
+        [&]() { return simplex::Simplex::vertex(mesh().switch_vertex(simplex.tuple())); });
     return {v0, v1};
 }
 
-std::string EdgeCollapse::name() const
-{
-    return "tri_mesh_collapse_edge";
-}
-
-Tuple EdgeCollapse::return_tuple() const
-{
-    return m_output_tuple;
-}
-
-std::vector<Tuple> EdgeCollapse::modified_triangles() const
-{
-    Simplex v(PrimitiveType::Vertex, m_output_tuple);
-    auto sc = SimplicialComplex::open_star(mesh(), v);
-    auto faces = sc.get_simplices(PrimitiveType::Face);
-    std::vector<Tuple> ret;
-    for (const auto& face : faces) {
-        ret.emplace_back(face.tuple());
-    }
-    return ret;
-}
-} // namespace tri_mesh
-} // namespace wmtk::operations
+} // namespace wmtk::operations::tri_mesh
