2a91004d0143c77e8edcdec87a253bd9ee3639f8
Teseo Schneider <teseo@bluewin.ch>
2023-12-20 17:12:12 -0800

    split

--- src/wmtk/operations/tri_mesh/TriMeshOperation.hpp
+++ src/wmtk/operations/tri_mesh/TriMeshOperation.hpp
@@ -7,7 +7,7 @@ class TriMesh;
 namespace operations::tri_mesh {
 
 /// @brief utility class to store the cast from mesh to tri mesh
-class TriMeshOperation : public virtual Operation
+class TriMeshOperation : public Operation
 {
 public:
     TriMeshOperation(Mesh& m);
