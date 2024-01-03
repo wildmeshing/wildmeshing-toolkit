c01c100e49283feb0c9bf5009d03207a4345bb79
Daniel Zint <daniel.zint@mail.de>
2024-01-03 17:35:21 +0100

    Split TagIntersectionOptions into hpp/cpp.

--- components/wmtk_components/tag_intersection/internal/TagIntersectionOptions.hpp
+++ components/wmtk_components/tag_intersection/internal/TagIntersectionOptions.hpp
@@ -5,9 +5,7 @@
 #include <vector>
 #include <wmtk/PrimitiveType.hpp>
 
-namespace wmtk {
-namespace components {
-namespace internal {
+namespace wmtk::components::internal {
 
 struct TagIntersectionOptions
 {
@@ -18,31 +16,8 @@ struct TagIntersectionOptions
     std::vector<std::tuple<std::string, wmtk::PrimitiveType, long>> output_tags;
 };
 
-// TODO move to cpp
-inline void to_json(nlohmann::json& j, TagIntersectionOptions& o)
-{
-    j = {
-        {"type", o.type},
-        {"input", o.input},
-        {"output", o.output},
-        {"input_tags", o.input_tags},
-        {"output_tags", o.output_tags}};
-}
+void to_json(nlohmann::json& j, TagIntersectionOptions& o);
 
-// TODO move to cpp
-inline void from_json(const nlohmann::json& j, TagIntersectionOptions& o)
-{
-    o.type = j.at("type");
-    if (o.type != "tag_intersection") {
-        throw std::runtime_error("Wrong type in TagIntersectionOptions");
-    }
+void from_json(const nlohmann::json& j, TagIntersectionOptions& o);
 
-    o.input = j.at("input");
-    o.output = j.at("output");
-    j.at("input_tags").get_to(o.input_tags);
-    j.at("output_tags").get_to(o.output_tags);
-}
-
-} // namespace internal
-} // namespace components
-} // namespace wmtk
+} // namespace wmtk::components::internal
