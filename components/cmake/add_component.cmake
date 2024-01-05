b1cf66281194a753e6ad43986218bf5411e3a5de
Daniel Zint <daniel.zint@mail.de>
2024-01-05 12:14:46 -0500

    Add components base and some other minor changes to components.

--- components/cmake/add_component.cmake
+++ components/cmake/add_component.cmake
@@ -1,12 +1,18 @@
 function(add_component WMTK_COMPONENT_PREFIX folder)
     set(CURRENT_COMPONENT_LIB_NAME "${WMTK_COMPONENT_PREFIX}_${folder}")
 
+    option(WMTK_ENABLE_COMPONENT_${folder} "" ON)
+
+    IF(NOT ${WMTK_ENABLE_COMPONENT_${folder}})
+        return()
+    ENDIF()
+
     add_library(${CURRENT_COMPONENT_LIB_NAME})
     add_library(${WMTK_COMPONENT_PREFIX}::${folder} ALIAS ${CURRENT_COMPONENT_LIB_NAME})
     target_include_directories(${CURRENT_COMPONENT_LIB_NAME} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR})
 
     target_link_libraries(${CURRENT_COMPONENT_LIB_NAME} PRIVATE wmtk::warnings wmtk::toolkit)
-    # target_link_libraries(${CURRENT_COMPONENT_LIB_NAME} PRIVATE "${WMTK_COMPONENT_PREFIX}::base")
+    target_link_libraries(${CURRENT_COMPONENT_LIB_NAME} PRIVATE "${WMTK_COMPONENT_PREFIX}::base")
 
     add_subdirectory("wmtk_components/${folder}")
 
@@ -16,13 +22,19 @@ function(add_component WMTK_COMPONENT_PREFIX folder)
     file(APPEND "${CMAKE_CURRENT_SOURCE_DIR}/components_map.hpp"
      "components[\"${folder}\"] = wmtk::components::${folder};\n")
 
-     set(json_components
+    set(json_components
      "${json_components}{\"pointer\":\"/${folder}\",  \"type\": \"include\", \"spec_file\": \"${folder}_spec.json\"},\n" PARENT_SCOPE)
 
 
-     file(APPEND "${CMAKE_CURRENT_SOURCE_DIR}/spec_include.hpp"
+    file(APPEND "${CMAKE_CURRENT_SOURCE_DIR}/spec_include.hpp"
      "spec_engine.include_directories.push_back( \"${CMAKE_CURRENT_SOURCE_DIR}/wmtk_components/${folder}\");\n")
 
-     set(WMTKC_TEST_SOURCES "test_component_${folder}.cpp" ${WMTKC_TEST_SOURCES} PARENT_SCOPE)
+    set(WMTKC_TEST_SOURCES "test_component_${folder}.cpp" ${WMTKC_TEST_SOURCES} PARENT_SCOPE)
     target_link_libraries(wildmeshing_components INTERFACE ${CURRENT_COMPONENT_LIB_NAME})
+
+
+    # Group source files for IDEs
+    file(GLOB_RECURSE COMPONENTS_FILES_FOR_SOURCE_GROUP "${CMAKE_CURRENT_SOURCE_DIR}/wmtk_components/${folder}/*.cpp" "${CMAKE_CURRENT_SOURCE_DIR}/wmtk_components/${folder}/*.hpp")
+    source_group(TREE "${CMAKE_CURRENT_SOURCE_DIR}/wmtk_components/${folder}" PREFIX "src" FILES ${COMPONENTS_FILES_FOR_SOURCE_GROUP})
+
 endfunction()
--- components/wmtk_components/base/json_utils.hpp
+++ components/wmtk_components/base/json_utils.hpp
@@ -4,12 +4,12 @@
 #include <nlohmann/json.hpp>
 
 namespace nlohmann {
-template <>
 // json serialization of path
+template <>
 struct adl_serializer<std::filesystem::path>
 {
-    static void to_json(json& j, const std::filesystem::path& p) { j = p.string(); }
+    static void to_json(json& j, const std::filesystem::path& p);
 
-    static void from_json(const json& j, std::filesystem::path& p) { p = j.get<std::string>(); }
+    static void from_json(const json& j, std::filesystem::path& p);
 };
 } // namespace nlohmann
