476f32630265e8adeac0b109d92d9a9f816bb356
Teseo Schneider <teseo@bluewin.ch>
2024-01-07 15:42:16 -0800

    fixted test

--- components/tests/test_component_isotropic_remeshing.cpp
+++ components/tests/test_component_isotropic_remeshing.cpp
@@ -645,7 +645,7 @@ TEST_CASE("component_isotropic_remeshing", "[components][isotropic_remeshing][2D
         json mesh_isotropic_remeshing_json = R"({
         "input": "input_mesh",
         "output": "output_mesh",
-        "attributes": {"position": "vertices"},
+        "attributes": {"position": "vertices", "inversion_position": [], "other_positions": []},
         "pass_through": [],
         "iterations": 1,
         "length_abs": 0.003,
