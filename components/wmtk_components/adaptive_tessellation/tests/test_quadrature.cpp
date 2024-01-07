005d115694b744304a6d71db0b45dd2e913a9aa7
yunfanzhou <yfzhou42@gmail.com>
2024-01-07 00:11:49 -0500

    analytical quadrature test

--- components/wmtk_components/adaptive_tessellation/tests/test_quadrature.cpp
+++ components/wmtk_components/adaptive_tessellation/tests/test_quadrature.cpp
@@ -5,6 +5,7 @@
 #include <tools/TriMesh_examples.hpp>
 #include <wmtk/Primitive.hpp>
 #include <wmtk/Types.hpp>
+#include <wmtk/components/adaptive_tessellation/function/utils/AnalyticalFunctionTriangleQuadrature.hpp>
 #include <wmtk/components/adaptive_tessellation/function/utils/TextureIntegral.hpp>
 #include <wmtk/components/adaptive_tessellation/image/Image.hpp>
 #include <wmtk/utils/Logger.hpp>
@@ -19,9 +20,9 @@ TEST_CASE("sinxcosy over unit square")
 {
     tests::DEBUG_TriMesh mesh = tests::unit_squre();
     std::array<std::shared_ptr<image::Image>, 3> images = {
-        std::make_shared<image::Image>(500, 500),
-        std::make_shared<image::Image>(500, 500),
-        std::make_shared<image::Image>(500, 500)};
+        {std::make_shared<image::Image>(500, 500),
+         std::make_shared<image::Image>(500, 500),
+         std::make_shared<image::Image>(500, 500)}};
     auto u = [](const double& u, [[maybe_unused]] const double& v) -> double { return u; };
     auto v = []([[maybe_unused]] const double& u, const double& v) -> double { return v; };
     auto height_function = [](const double& u, [[maybe_unused]] const double& v) -> double {
@@ -55,6 +56,58 @@ TEST_CASE("sinxcosy over unit square")
         error += tri_error;
     }
 
+    // the analytical solve for
+    // (sin(2 * M_PI * u) * cos(2 * M_PI * v))^2 with x in (0,1), y in (0,1) = 0.25
+    REQUIRE(pow(error - 0.25, 2) < 1e-4);
+}
+TEST_CASE("analytical quadrature")
+{
+    tests::DEBUG_TriMesh mesh = tests::unit_squre();
+
+    std::array<std::shared_ptr<image::SamplingAnalyticFunction>, 3> funcs = {
+        {std::make_shared<image::SamplingAnalyticFunction>(
+             image::SamplingAnalyticFunction_FunctionType::Linear,
+             1,
+             0,
+             0.),
+         std::make_shared<image::SamplingAnalyticFunction>(
+             image::SamplingAnalyticFunction_FunctionType::Linear,
+             0,
+             1,
+             0.),
+         std::make_shared<image::SamplingAnalyticFunction>(
+             image::SamplingAnalyticFunction_FunctionType::Periodic,
+             2,
+             2,
+             1.)}
+
+    };
+
+    ATfunction::utils::ThreeChannelPositionMapEvaluator evaluator(funcs);
+    ATfunction::utils::AnalyticalFunctionTriangleQuadrature analy_quad(evaluator);
+
+    std::array<Tuple, 2> triangles = {mesh.tuple_from_face_id(0), mesh.tuple_from_face_id(1)};
+
+    wmtk::attribute::MeshAttributeHandle uv_handle =
+        mesh.get_attribute_handle<double>("vertices", PrimitiveType::Vertex);
+    ConstAccessor<double> uv = mesh.create_const_accessor(uv_handle.as<double>());
+
+
+    constexpr static PrimitiveType PV = PrimitiveType::Vertex;
+    constexpr static PrimitiveType PE = PrimitiveType::Edge;
+    constexpr static PrimitiveType PF = PrimitiveType::Face;
+    double error = 0.0;
+    for (const auto& triangle : triangles) {
+        // get coordinates of triangle 3 vertices
+        Vector2d uv0 = uv.const_vector_attribute(triangle);
+        Vector2d uv1 = uv.const_vector_attribute(mesh.switch_tuples(triangle, {PV}));
+        Vector2d uv2 = uv.const_vector_attribute(mesh.switch_tuples(triangle, {PE, PV}));
+
+        double tri_error = analy_quad.get_error_one_triangle_exact<double>(uv0, uv1, uv2);
+        std::cout << "tri_error " << tri_error << std::endl;
+        error += tri_error;
+    }
+    std::cout << "error " << error << std::endl;
     // the analytical solve for
     // (sin(2 * M_PI * u) * cos(2 * M_PI * v))^2 with x in (0,1), y in (0,1) = 0.25
     REQUIRE(pow(error - 0.25, 2) < 1e-4);
