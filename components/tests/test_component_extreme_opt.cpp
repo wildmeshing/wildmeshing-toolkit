6c068c08ef8cabf5fca29ba3607cca0744adbe51
zlyfunction <zhuleyi001@126.com>
2024-01-07 21:03:09 -0500

    change data path

--- components/tests/test_component_extreme_opt.cpp
+++ components/tests/test_component_extreme_opt.cpp
@@ -10,7 +10,9 @@ using namespace wmtk::components::base;
 
 using json = nlohmann::json;
 
-const std::filesystem::path data_dir = WMTK_DATA_DIR;
+const std::filesystem::path data_dir =
+    std::filesystem::path(WMTK_DATA_DIR) / ".." / "extreme_opt_data_msh";
+
 
 TEST_CASE("extreme_opt_cube", "[components][extreme_opt][.]")
 {
@@ -18,20 +20,20 @@ TEST_CASE("extreme_opt_cube", "[components][extreme_opt][.]")
     json seamed_json = {
         {"type", "input"},
         {"name", "cube_seamed"},
-        {"file", (data_dir / "extreme_opt_data_msh" / "cube_pos.msh").string()},
+        {"file", (data_dir / "cube_pos.msh").string()},
         {"ignore_z", false}};
     wmtk::components::input(Paths(), seamed_json, cache);
     json cut_json = {
         {"type", "input"},
         {"name", "cube_cut"},
-        {"file", (data_dir / "extreme_opt_data_msh" / "cube_tex.msh").string()},
+        {"file", (data_dir / "cube_tex.msh").string()},
         {"ignore_z", true}};
     wmtk::components::input(Paths(), cut_json, cache);
 
     json extreme_opt = {
         {"mesh_name", "cube"},
         {"length_rel", 0.1},
-        {"iterations", 1},
+        {"iterations", 3},
         {"lock_boundary", false},
         {"smooth_boundary", true},
         {"check_inversion", true},
@@ -53,20 +55,20 @@ TEST_CASE("extreme_opt_elk", "[components][extreme_opt][.]")
     json seamed_json = {
         {"type", "input"},
         {"name", "elk_seamed"},
-        {"file", (data_dir / "extreme_opt_data_msh" / "elk_pos.msh").string()},
+        {"file", (data_dir / "elk_pos.msh").string()},
         {"ignore_z", false}};
     wmtk::components::input(Paths(), seamed_json, cache);
     json cut_json = {
         {"type", "input"},
         {"name", "elk_cut"},
-        {"file", (data_dir / "extreme_opt_data_msh" / "elk_tex.msh").string()},
+        {"file", (data_dir / "elk_tex.msh").string()},
         {"ignore_z", true}};
     wmtk::components::input(Paths(), cut_json, cache);
 
     json extreme_opt = {
         {"mesh_name", "elk"},
         {"length_rel", 0.1},
-        {"iterations", 1},
+        {"iterations", 3},
         {"lock_boundary", false},
         {"smooth_boundary", true},
         {"check_inversion", true},
