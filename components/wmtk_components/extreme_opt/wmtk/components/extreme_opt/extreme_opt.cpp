8fefd272ff03c3300eba705b0423b27411cb3104
zlyfunction <zhuleyi001@126.com>
2024-01-07 20:41:30 -0500

    get to work

--- components/wmtk_components/extreme_opt/wmtk/components/extreme_opt/extreme_opt.cpp
+++ components/wmtk_components/extreme_opt/wmtk/components/extreme_opt/extreme_opt.cpp
@@ -2,10 +2,10 @@
 
 #include <wmtk/TriMesh.hpp>
 #include <wmtk/components/base/get_attributes.hpp>
+#include <wmtk/io/ParaviewWriter.hpp>
 #include <wmtk/multimesh/same_simplex_dimension_bijection.hpp>
-#include <wmtk/utils/SeamlessConstraints.hpp>
-
 #include <wmtk/utils/Logger.hpp>
+#include <wmtk/utils/SeamlessConstraints.hpp>
 
 #include "internal/ExtremeOpt.hpp"
 #include "internal/ExtremeOptOptions.hpp"
@@ -28,21 +28,84 @@ void extreme_opt(const base::Paths& paths, const nlohmann::json& j, io::Cache& c
     auto child_map = multimesh::same_simplex_dimension_bijection(seamed_mesh, *cut_mesh_ptr);
     seamed_mesh.register_child_mesh(cut_mesh_ptr, child_map);
 
-    /*
-        TriMesh& mesh = static_cast<TriMesh&>(*mesh_in);
 
+    // get target length for remeshing ON CUT MESH
+    double length_rel = options.length_rel;
+    double length_abs = 0;
+    {
         auto pos_handle =
-            mesh.get_attribute_handle<double>(options.attributes.position, PrimitiveType::Vertex);
+            cut_mesh_ptr->get_attribute_handle<double>("vertices", PrimitiveType::Vertex);
+        auto pos = cut_mesh_ptr->create_const_accessor(pos_handle.as<double>());
 
-        auto pass_through_attributes = base::get_attributes(mesh, options.pass_through);
+        Eigen::Vector2d p_max;
+        p_max.setConstant(std::numeric_limits<double>::lowest());
+        Eigen::Vector2d p_min;
+        p_min.setConstant(std::numeric_limits<double>::max());
 
-        if (options.length_abs < 0) {
-            if (options.length_rel < 0) {
-                throw std::runtime_error("Either absolute or relative length must be set!");
-            }
-            options.length_abs = relative_to_absolute_length(mesh, pos_handle, options.length_rel);
+        for (const Tuple& v : cut_mesh_ptr->get_all(PrimitiveType::Vertex)) {
+            const Eigen::Vector2d p = pos.const_vector_attribute(v);
+            p_max[0] = std::max(p_max[0], p[0]);
+            p_max[1] = std::max(p_max[1], p[1]);
+            p_min[0] = std::min(p_min[0], p[0]);
+            p_min[1] = std::min(p_min[1], p[1]);
         }
+        const double diag_length = (p_max - p_min).norm();
+        length_abs = diag_length * length_rel;
+        std::cout << "length_abs: " << length_abs << std::endl;
+    }
 
+    if (!wmtk::utils::check_constraints(
+            seamed_mesh,
+            *cut_mesh_ptr,
+            cut_mesh_ptr->get_attribute_handle_typed<double>("vertices", PrimitiveType::Vertex))) {
+        std::cout << "Input does not satisfying the seamless constraints!" << std::endl;
+        return;
+    };
+
+    ExtremeOpt extreme_opt(
+        options.mesh_name,
+        seamed_mesh,
+        length_abs,
+        options.lock_boundary,
+        options.do_split,
+        options.do_collapse,
+        options.collapse_optimize_E_max,
+        options.do_swap,
+        options.swap_optimize_E_max,
+        options.do_smooth,
+        options.debug_output);
+
+    extreme_opt.remeshing(options.iterations);
+    std::cout << "finish remeshing" << std::endl;
+    std::cout << "to check constraints" << std::endl;
+    wmtk::utils::check_constraints(
+        seamed_mesh,
+        *cut_mesh_ptr,
+        cut_mesh_ptr->get_attribute_handle_typed<double>("vertices", PrimitiveType::Vertex));
+
+    // TODO: Figure out if it is possible to write with cache?
+    // write final result to file
+    io::ParaviewWriter writer(
+        "extreme_opt_" + options.mesh_name + "seamed_final",
+        "vertices",
+        seamed_mesh,
+        true,
+        true,
+        true,
+        false);
+    seamed_mesh.serialize(writer);
+    io::ParaviewWriter writer2(
+        "extreme_opt_" + options.mesh_name + "cut_final",
+        "vertices",
+        *cut_mesh_ptr,
+        true,
+        true,
+        true,
+        false);
+    cut_mesh_ptr->serialize(writer2);
+
+    return;
+    /*
         // clear attributes
         std::vector<attribute::MeshAttributeHandle> keeps = pass_through_attributes;
         keeps.emplace_back(pos_handle);
@@ -52,21 +115,6 @@ void extreme_opt(const base::Paths& paths, const nlohmann::json& j, io::Cache& c
         pos_handle =
             mesh.get_attribute_handle<double>(options.attributes.position, PrimitiveType::Vertex);
         pass_through_attributes = base::get_attributes(mesh, options.pass_through);
-
-
-        ExtremeOpt extreme_opt(
-            mesh_name,
-            seamed_mesh,
-            length_abs,
-            options.lock_boundary,
-            options.do_split,
-            options.do_collapse,
-            options.collapse_optimize_E_max,
-            options.do_swap,
-            options.swap_optimize_E_max,
-            options.do_smooth,
-            options.debug_output);
-        extreme_opt.remeshing(options.iterations);
 
         // output
         cache.write_mesh(mesh, options.output);
