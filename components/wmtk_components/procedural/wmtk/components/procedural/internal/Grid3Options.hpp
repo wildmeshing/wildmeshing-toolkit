3f1e3de9f13175f38db09c3cb8f6349a7cc8cfaf
Michael Tao <michael.tao@nyu.edu>
2024-01-11 16:23:01 -0500

    more explicit about json types in grid procedural for clang

--- components/wmtk_components/procedural/wmtk/components/procedural/internal/Grid3Options.hpp
+++ components/wmtk_components/procedural/wmtk/components/procedural/internal/Grid3Options.hpp
@@ -31,7 +31,8 @@ public:
         nlohmann_json_j["dimensions"] = nlohmann_json_t.dimensions;
         {
             const auto& b = nlohmann_json_t.cycles;
-            nlohmann_json_j["cycles"] = nlohmann::json::array({b[0], b[1], b[2]});
+            std::array<bool,3> bs{{b[0],b[1],b[2]}};
+            nlohmann_json_j["cycles"] = bs;
         }
     }
     friend void from_json(const nlohmann::json& nlohmann_json_j, Grid3Options& nlohmann_json_t)
