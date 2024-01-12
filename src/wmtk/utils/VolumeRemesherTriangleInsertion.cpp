147a6f56a985a663fdae89c58fe32a0e7334a80d
JcDai <jiacheng_dai@qq.com>
2024-01-12 16:42:47 -0500

    fix cmake

--- src/wmtk/utils/VolumeRemesherTriangleInsertion.cpp
+++ src/wmtk/utils/VolumeRemesherTriangleInsertion.cpp
@@ -9,11 +9,11 @@
 
 namespace wmtk::utils {
 
-// void generate_background_mesh(
-//     const RowVectors3d& V,
-//     RowVectors3d& background_V,
-//     RowVecors4l& background_TV)
-// {}
+void generate_background_mesh(
+    const RowVectors3d& V,
+    RowVectors3d& background_V,
+    RowVectors4l& background_TV)
+{}
 
 std::shared_ptr<wmtk::TetMesh> generate_raw_tetmesh_from_input_surface(
     const RowVectors3d& V,
