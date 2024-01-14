3ce19dba6d96ae7dba944cd6182d464a82d7a6fd
JcDai <jiacheng_dai@qq.com>
2024-01-14 00:16:26 -0500

    resurrected triangle insertion, need to fix mpq get_d segfault

--- src/wmtk/utils/VolumeRemesherTriangleInsertion.hpp
+++ src/wmtk/utils/VolumeRemesherTriangleInsertion.hpp
@@ -9,9 +9,21 @@ class TetMesh;
 
 namespace wmtk::utils {
 
-std::shared_ptr<wmtk::TetMesh> generate_raw_tetmesh_from_input_surface(
+/**
+ * @brief input a triangle surface mesh, embed it into a regular grid and generate a tetmesh, track
+ * the input surfaces on the tetmesh
+ *
+ * @param V input vertex coordinates
+ * @param F input FV matrix
+ * @param target_edge_length target edge length
+ * @return [tetmesh ptr, tet local faces on input surface]
+ */
+
+std::tuple<std::shared_ptr<wmtk::TetMesh>, std::vector<std::array<bool, 4>>>
+generate_raw_tetmesh_from_input_surface(
     const RowVectors3d& V,
-    const RowVectors3l& F);
+    const RowVectors3l& F,
+    const double eps_target);
 
 
 } // namespace wmtk::utils
