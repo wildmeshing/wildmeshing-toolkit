d3a29babd24eb8c566f36647c90d5437f23c25dd
Teseo Schneider <teseo@bluewin.ch>
2024-01-14 10:29:05 -0800

    fixed vrem

--- cmake/recipes/volume_mesher.cmake
+++ cmake/recipes/volume_mesher.cmake
@@ -7,7 +7,7 @@ endif()
 message(STATUS "Third-party: creating target 'VolumeMesher'")
 
 include(CPM)
-CPMAddPackage("gh:JcDai/VolumeRemesher#9cfcbc64b115ad91f08ffbdbc0a1d59344aa4dbe")
+CPMAddPackage("gh:JcDai/VolumeRemesher#a5e6d9f0df1ccd7070360cf957aa74d6e6a9d7d6")
 
 set_target_properties(mesh_generator_lib PROPERTIES FOLDER third-party)
 
