d5bd14dada53e1a4b674cf0da3683a94d16aecd3
yunfanzhou <yfzhou42@gmail.com>
2024-01-17 18:07:02 -0500

    add distance energy that's ignorant of what type of function it is evaluating

---
+++ components/wmtk_components/adaptive_tessellation/wmtk/components/adaptive_tessellation/function/simplex/DistanceEnergy.cpp
@@ -0,0 +1,33 @@
+#include "DistanceEnergy.hpp"
+#include <wmtk/Mesh.hpp>
+#include <wmtk/Primitive.hpp>
+#include <wmtk/components/adaptive_tessellation/function/utils/TextureIntegral.hpp>
+#include <wmtk/function/utils/AutoDiffRAII.hpp>
+#include <wmtk/function/utils/amips.hpp>
+
+namespace image = wmtk::components::image;
+
+namespace wmtk::function {
+DistanceEnergy::DistanceEnergy(
+    const Mesh& mesh,
+    const wmtk::attribute::MeshAttributeHandle& vertex_uv_handle,
+    std::shared_ptr<wmtk::components::function::utils::IntegralBase> integral_ptr,
+    double weight)
+    : wmtk::function::PerSimplexAutodiffFunction(mesh, PrimitiveType::Vertex, vertex_uv_handle)
+    , m_integral_ptr(integral_ptr)
+    , m_weight(weight)
+{}
+
+DistanceEnergy::~DistanceEnergy() = default;
+DScalar DistanceEnergy::eval(
+    const simplex::Simplex& domain_simplex,
+    const std::vector<DSVec>& coords) const
+{
+    assert(embedded_dimension() == 2);
+    assert(coords.size() == 3);
+
+    DSVec2 a = coords[0], b = coords[1], c = coords[2];
+    return m_weight * m_integral_ptr->get_error_one_triangle_exact(a, b, c);
+}
+
+} // namespace wmtk::function
