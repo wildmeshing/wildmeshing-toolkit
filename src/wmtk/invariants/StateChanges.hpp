6e786cdd1acd270742250e14a85529ea7a39e629
yunfanzhou <yfzhou42@gmail.com>
2024-01-17 18:07:43 -0500

    add StateChage invariant to allow splitting undersampled meshes to be split

---
+++ src/wmtk/invariants/StateChanges.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <memory>
+#include "Invariant.hpp"
+
+namespace wmtk {
+
+namespace function {
+class PerSimplexFunction;
+}
+
+namespace invariants {
+
+class StateChanges : public Invariant
+{
+public:
+    StateChanges(
+        const PrimitiveType type,
+        const std::shared_ptr<function::PerSimplexFunction>& func);
+
+    bool after(
+        const std::vector<Tuple>& top_dimension_tuples_before,
+        const std::vector<Tuple>& top_dimension_tuples_after) const override;
+
+private:
+    std::shared_ptr<function::PerSimplexFunction> m_func;
+    const PrimitiveType m_type;
+};
+} // namespace invariants
+} // namespace wmtk
