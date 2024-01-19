854843705d94e8c9b4bac043541ef433a8495926
Michael Tao <mtao@dgp.toronto.edu>
2024-01-18 23:59:42 -0500

    Revert "attributescope are cache-coherent in a vector and some more inlining"
    This reverts commit 6e551efb789db3b4ee3c8f02b6f3fa99f58647b0.

--- src/wmtk/attribute/AttributeCacheData.cpp
+++ src/wmtk/attribute/AttributeCacheData.cpp
@@ -1,6 +1,17 @@
 #include "AttributeCacheData.hpp"
 #include <wmtk/utils/Rational.hpp>
 namespace wmtk {
+template <typename T>
+auto AttributeCacheData<T>::data_as_map() -> typename Vector::MapType
+{
+    return typename Vector::MapType(data.data(), data.size());
+}
+template <typename T>
+auto AttributeCacheData<T>::data_as_const_map() const -> typename Vector::ConstMapType
+{
+    return typename Vector::ConstMapType(data.data(), data.size());
+}
+
 template class AttributeCacheData<int64_t>;
 template class AttributeCacheData<double>;
 template class AttributeCacheData<char>;
