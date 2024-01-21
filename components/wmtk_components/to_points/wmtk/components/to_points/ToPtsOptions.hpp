690b3a99e91c7e78625e55205ebecd874b7b5b02
Teseo Schneider <teseo@bluewin.ch>
2024-01-21 09:48:26 -0800

    added boxes to to pts

--- components/wmtk_components/to_points/wmtk/components/to_points/ToPtsOptions.hpp
+++ components/wmtk_components/to_points/wmtk/components/to_points/ToPtsOptions.hpp
@@ -10,8 +10,10 @@ struct ToPtsOptions
     std::string input;
     std::string position;
     std::string name;
+    bool add_box;
+    double box_scale;
 };
 
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ToPtsOptions, input, position, name);
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ToPtsOptions, input, position, name, add_box, box_scale);
 
 } // namespace wmtk::components
--- components/wmtk_components/to_points/wmtk/components/to_points/to_points.cpp
+++ components/wmtk_components/to_points/wmtk/components/to_points/to_points.cpp
@@ -21,11 +21,33 @@ void to_points(const base::Paths& paths, const nlohmann::json& j, io::Cache& cac
     const auto pts_acc = mesh.create_const_accessor<double>(pts_attr);
     const auto vs = mesh.get_all(PrimitiveType::Vertex);
 
-    Eigen::MatrixXd pts(vs.size(), pts_acc.dimension());
+    Eigen::AlignedBox<double, Eigen::Dynamic> bbox(pts_acc.dimension());
+
+    int64_t box_size = std::pow(2, pts_acc.dimension());
+
+
+    Eigen::MatrixXd pts(vs.size() + (options.add_box ? box_size : 0), pts_acc.dimension());
     int64_t index = 0;
     for (const auto& v : vs) {
         pts.row(index) << pts_acc.const_vector_attribute(v).transpose();
+        bbox.extend(pts.row(index).transpose());
         ++index;
+    }
+
+    if (options.add_box) {
+        auto center = bbox.center();
+        auto r = bbox.diagonal() / 2.;
+
+        bbox.min() = center - options.box_scale * r;
+        bbox.max() = center + options.box_scale * r;
+
+        for (int64_t d = 0; d < box_size; ++d) {
+            std::bitset<32> bits = d;
+            for (int64_t i = 0; i < pts.cols(); ++i) {
+                pts(index, i) = bits[i] == 0 ? bbox.min()[i] : bbox.max()[i];
+            }
+            ++index;
+        }
     }
 
     PointMesh pts_mesh(pts.rows());
