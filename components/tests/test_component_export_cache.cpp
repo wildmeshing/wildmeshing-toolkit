38c1440fc2461e02133fd3e42e2c3ce57cacc4aa
Daniel Zint <daniel.zint@mail.de>
2024-01-20 21:14:39 -0500

    Add components for importing and exporting cache. Completely untested so far (not even compiled).

---
+++ components/tests/test_component_export_cache.cpp
@@ -0,0 +1,5 @@
+#include <catch2/catch_test_macros.hpp>
+#include <nlohmann/json.hpp>
+#include <wmtk/components/base/Paths.hpp>
+#include <wmtk/components/export_cache/ExportCacheOptions.hpp>
+#include <wmtk/components/export_cache/export_cache.hpp>
---
+++ components/tests/test_component_import_cache.cpp
@@ -0,0 +1,5 @@
+#include <catch2/catch_test_macros.hpp>
+#include <nlohmann/json.hpp>
+#include <wmtk/components/base/Paths.hpp>
+#include <wmtk/components/import_cache/ImportCacheOptions.hpp>
+#include <wmtk/components/import_cache/import_cache.hpp>
