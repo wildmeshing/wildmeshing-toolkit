f1710b8f0f0b7eb5d11551e8ed99deb71b568d44
Daniel Zint <daniel.zint@mail.de>
2024-01-29 19:41:07 -0500

    Fix reservation size.

--- src/wmtk/simplex/closed_star.cpp
+++ src/wmtk/simplex/closed_star.cpp
@@ -17,16 +17,18 @@ SimplexCollection closed_star(const Mesh& mesh, const Simplex& simplex, const bo
     const size_t n_top_dimension_cofaces = collection.simplex_vector().size();
 
     // reserve memory for all cells, their faces, and the input simplex
+    // this is crucial to avoid reallocation!!
     switch (mesh.top_simplex_type()) {
     case PrimitiveType::Vertex: break;
     case PrimitiveType::Edge: collection.reserve(n_top_dimension_cofaces * 3 + 1); break;
     case PrimitiveType::Face: collection.reserve(n_top_dimension_cofaces * 7 + 1); break;
-    case PrimitiveType::Tetrahedron: collection.reserve(n_top_dimension_cofaces * 14 + 1); break;
+    case PrimitiveType::Tetrahedron: collection.reserve(n_top_dimension_cofaces * 15 + 1); break;
     case PrimitiveType::HalfEdge:
     default: log_and_throw_error("unknown mesh type in top_dimension_cofaces_tuples");
     }
 
     for (size_t i = 0; i < n_top_dimension_cofaces; ++i) {
+        // breaks if simplex vector needs to allocate memory due to reference!
         faces(collection, collection.simplex_vector()[i], false);
     }
 
