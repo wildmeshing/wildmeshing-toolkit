b517344aaace7d0c26c103ffbeae43c01a6f3d34
Daniel Zint <daniel.zint@mail.de>
2024-01-31 19:05:57 -0500

    Update accessor performance test.

--- tests/test_performance.cpp
+++ tests/test_performance.cpp
@@ -1,8 +1,10 @@
 #include <catch2/catch_test_macros.hpp>
 #include <nlohmann/json.hpp>
+#include <wmtk/PointMesh.hpp>
 #include <wmtk/TriMesh.hpp>
 #include <wmtk/io/MeshReader.hpp>
 #include <wmtk/utils/Logger.hpp>
+#include <wmtk/utils/mesh_utils.hpp>
 
 #include <polysolve/Utils.hpp>
 
@@ -49,26 +51,9 @@ TEST_CASE("accessor_performance", "[accessor][.]")
     auto pos_handle = m.get_attribute_handle<double>("vertices", PrimitiveType::Vertex);
     auto pos_acc = m.create_accessor<double>(pos_handle);
 
-    const size_t n_repetitions = 10000;
-
+    const size_t n_repetitions = 50000;
 
     const auto vertices = m.get_all(PrimitiveType::Vertex);
-    {
-        POLYSOLVE_SCOPED_STOPWATCH("Accessors", logger());
-        double sum = 0;
-        for (size_t i = 0; i < n_repetitions; ++i) {
-            for (const Tuple& t : vertices) {
-                sum += pos_acc.const_vector_attribute(t)[0];
-            }
-            for (const Tuple& t : vertices) {
-                sum += pos_acc.const_vector_attribute(t)[1];
-            }
-            for (const Tuple& t : vertices) {
-                sum += pos_acc.const_vector_attribute(t)[2];
-            }
-        }
-        std::cout << "sum = " << sum << std::endl;
-    }
 
     // create matrix of positions
     Eigen::MatrixXd positions;
@@ -93,4 +78,42 @@ TEST_CASE("accessor_performance", "[accessor][.]")
         }
         std::cout << "sum = " << sum << std::endl;
     }
+
+    PointMesh pm(vertices.size());
+    auto pph = mesh_utils::set_matrix_attribute(positions, "vertices", PrimitiveType::Vertex, pm);
+    auto pp_acc = pm.create_accessor<double>(pph);
+    {
+        const auto vv = pm.get_all(PrimitiveType::Vertex);
+        POLYSOLVE_SCOPED_STOPWATCH("PointMesh Accessors", logger());
+        double sum = 0;
+        for (size_t i = 0; i < n_repetitions; ++i) {
+            for (const Tuple& t : vv) {
+                sum += pp_acc.const_vector_attribute(t)[0];
+            }
+            for (const Tuple& t : vv) {
+                sum += pp_acc.const_vector_attribute(t)[1];
+            }
+            for (const Tuple& t : vv) {
+                sum += pp_acc.const_vector_attribute(t)[2];
+            }
+        }
+        std::cout << "sum = " << sum << std::endl;
+    }
+
+    //{
+    //    POLYSOLVE_SCOPED_STOPWATCH("TriMesh Accessors", logger());
+    //    double sum = 0;
+    //    for (size_t i = 0; i < n_repetitions; ++i) {
+    //        for (const Tuple& t : vertices) {
+    //            sum += pos_acc.const_vector_attribute(t)[0];
+    //        }
+    //        for (const Tuple& t : vertices) {
+    //            sum += pos_acc.const_vector_attribute(t)[1];
+    //        }
+    //        for (const Tuple& t : vertices) {
+    //            sum += pos_acc.const_vector_attribute(t)[2];
+    //        }
+    //    }
+    //    std::cout << "sum = " << sum << std::endl;
+    //}
 }
