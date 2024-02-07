5d89c642cc3b743b068ddc9784c21a18c6433932
Yifei <yz6994@nyu.edu>
2024-02-07 14:40:08 -0500

    finish new_topo_sep middle part, did experiments with top_dimension_cofaces

--- components/wmtk_components/extract_subset/internal/new_topology_separate.cpp
+++ components/wmtk_components/extract_subset/internal/new_topology_separate.cpp
@@ -9,6 +9,8 @@ std::unique_ptr<wmtk::Mesh>
 topology_separate(wmtk::Mesh& m, const wmtk::MeshAttributeHandle<long>& tag_handle, bool pos)
 {
     int top_simplex_dim = m.top_cell_dimension();
+    if (top_simplex_dim != 2 && top_simplex_dim != 3)
+        throw std::runtime_error("Invalid top dimension in separating topology!");
     wmtk::Accessor<long> tag_acc = m.create_accessor(tag_handle);
     wmtk::PrimitiveType topType = m.top_simplex_type();
     std::vector<wmtk::Tuple> top_simplices = m.get_all(topType);
@@ -36,26 +38,24 @@ topology_separate(wmtk::Mesh& m, const wmtk::MeshAttributeHandle<long>& tag_hand
         auto corners = wmtk::simplex::faces_single_dimension(m, s, PrimitiveType::Vertex);
         for (long j = 0; i < top_simplex_dim + 1; ++j) {
             // check whether it has been visited
-            if (v[j] == -1) {
-                // if the corner has not been assigned a duplicate index, assign it
-                v[j] = counter;
-                
-                // find all top simplices sharing the same corner vertex,
-                // update duplicate index of theie corner accordingly
+            if (v[j] != -1) continue;
+            // if the corner has not been assigned a duplicate index, assign it
+            v[j] = counter;
 
-                // get all top dimension simplices sharing the corner vertex
-                wmtk::simplex::SimplexCollection sc =
-                    wmtk::simplex::top_dimension_cofaces(m, Simplex::vertex(corners[j]));
-                for (wmtk::Simplex adj_simplex : sc) {
-                    // tuple for a top dimension simplex would be the same as tuple for the corner
-                    wmtk::Tuple adj_corner_tuple = adj_simplex.tuple();
-                    // TODO: determine whether this adj simplex is vertex-connected to the corner
+            // find all top simplices sharing the same corner vertex,
+            // update duplicate index of theie corner accordingly
 
-                    auto adj_vector = dup_acc.vector_attribute(adj_corner_tuple);
-                    long k = adj_corner_tuple.get_local_vid();
-                    assert(adj_vector[k] == -1);
-                    adj_vector[k] = counter;
-                }
+            // get all top dimension simplices sharing the corner vertex that are connected by n-1
+            // faces
+            wmtk::simplex::SimplexCollection sc =
+                wmtk::simplex::top_dimension_cofaces(m, Simplex::vertex(corners[j]));
+            for (wmtk::Simplex adj_simplex : sc) {
+                // tuple for a top dimension simplex would be the same as tuple for the corner
+                wmtk::Tuple adj_corner_tuple = adj_simplex.tuple();
+                auto adj_vector = dup_acc.vector_attribute(adj_corner_tuple);
+                long k = adj_corner_tuple.get_local_vid();
+                assert(adj_vector[k] == -1);
+                adj_vector[k] = counter;
             }
             // finally, increment the counter
             counter++;
@@ -76,7 +76,7 @@ topology_separate(wmtk::Mesh& m, const wmtk::MeshAttributeHandle<long>& tag_hand
         }
         mesh.initialize(tris);
         return std::make_unique<wmtk::TriMesh>(mesh);
-    } else if (top_simplex_dim == 3) {
+    } else {
         wmtk::TetMesh mesh;
         wmtk::RowVectors4l tets;
         tets.resize(top_simplex_count, 4);
@@ -89,8 +89,6 @@ topology_separate(wmtk::Mesh& m, const wmtk::MeshAttributeHandle<long>& tag_hand
         }
         mesh.initialize(tets);
         return std::make_unique<wmtk::TetMesh>(mesh);
-    } else {
-        throw std::runtime_error("Invalid top dimension in separating topology!");
     }
 }
 } // namespace wmtk::components::internal
