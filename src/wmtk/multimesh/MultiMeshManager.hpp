a7f9cc7df5e83ccbf95a48faacde5402b293e33d
Daniel Zint <daniel.zint@mail.de>
2024-03-04 13:35:44 -0500

    Add declaration for deregistering a mesh from multi-mesh.

--- src/wmtk/multimesh/MultiMeshManager.hpp
+++ src/wmtk/multimesh/MultiMeshManager.hpp
@@ -12,8 +12,8 @@
 
 namespace wmtk {
 
-    class Mesh;
-    class HDF5Reader;
+class Mesh;
+class HDF5Reader;
 namespace operations::utils {
 class UpdateEdgeOperationMultiMeshMapFunctor;
 }
@@ -37,7 +37,7 @@ class Mesh;
 namespace simplex {
 class SimplexCollection;
 }
-}
+} // namespace wmtk
 namespace wmtk::multimesh {
 /**
  * @brief Implementation details for how the Mesh class implements multiple meshes
@@ -130,6 +130,15 @@ public:
         Mesh& my_mesh,
         const std::shared_ptr<Mesh>& child_mesh,
         const std::vector<std::array<Tuple, 2>>& child_tuple_my_tuple_map);
+
+    /**
+     * @brief Deregister a child mesh.
+     *
+     * The child mesh is not deleted. It is only detached from the multi-mesh structure. The child
+     * mesh becomes the new root for its own children. Attribute handles for the child and parent
+     * mesh will be invalidated by deregistration.
+     */
+    void deregister_child_mesh(Mesh& my_mesh, Mesh& child_mesh);
 
 
     // bool are_maps_valid(const Mesh& my_mesh) const;
@@ -533,4 +542,4 @@ public:
 
     void check_child_map_valid(const Mesh& my_mesh, const ChildData& child_data) const;
 };
-} // namespace wmtk
+} // namespace wmtk::multimesh
