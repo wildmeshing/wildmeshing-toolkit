de72f6c53691b08ebbbae069a3fb64f55df0fe13
Daniel Zint <daniel.zint@mail.de>
2024-03-06 18:52:18 -0500

    Fix the case that the new name is the same as the old one.

--- src/wmtk/attribute/MeshAttributes.cpp
+++ src/wmtk/attribute/MeshAttributes.cpp
@@ -263,13 +263,16 @@ std::string MeshAttributes<T>::get_name(const AttributeHandle& handle) const
 template <typename T>
 void MeshAttributes<T>::set_name(const AttributeHandle& handle, const std::string& name)
 {
-    assert(m_handles.count(name) == 0); // name should not exist already
-
     const std::string old_name = get_name(handle);
 
-    auto& attr = m_attributes[handle.index];
+    if (old_name == name) {
+        return;
+    }
 
+    auto& attr = m_attributes[handle.index];
     assert(attr->m_name == old_name);
+
+    assert(m_handles.count(name) == 0); // name should not exist already
 
     attr->m_name = name;
     m_handles[name] = m_handles[old_name];
