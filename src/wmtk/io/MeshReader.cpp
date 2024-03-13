f6002b06df57c93732e21a8ad0f5ce81cb8d6c5a
Yifei <yz6994@nyu.edu>
2024-03-13 07:42:51 -0400

    remove comments

--- src/wmtk/io/MeshReader.cpp
+++ src/wmtk/io/MeshReader.cpp
@@ -16,12 +16,6 @@ std::shared_ptr<Mesh> read_mesh(const std::filesystem::path& filename, const boo
         return reader.read(filename);
     } else if (extension == ".msh") {
         MshReader reader;
-        // std::shared_ptr<Mesh> ret = reader.read(filename, ignore_z);
-        // std::vector<std::string> attribute_names = reader.get_element_attribute_names<3>();
-        // for (const std::string& name : attribute_names) {
-        //     std::cout << "Extracting attribute " << name << std::endl;
-        // }
-        // return ret;
         return reader.read(filename, ignore_z);
     }
 
