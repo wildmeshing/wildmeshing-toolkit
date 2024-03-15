0895d30f2789765e9913af3c5b6d5c895e555997
Michael Tao <michael.tao@nyu.edu>
2024-03-15 11:23:21 -0400

    partial updates for fixing special accessor build

--- src/wmtk/invariants/Swap23EnergyBeforeInvariant.hpp
+++ src/wmtk/invariants/Swap23EnergyBeforeInvariant.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
-#include <wmtk/attribute/AttributeHandle.hpp>
+#include <wmtk/attribute/TypedAttributeHandle.hpp>
 #include "Invariant.hpp"
 
 namespace wmtk {
 class Swap23EnergyBeforeInvariant : public Invariant
 {
 public:
-    Swap23EnergyBeforeInvariant(const Mesh& m, const TypedAttributeHandle<double>& coordinate);
+    Swap23EnergyBeforeInvariant(const Mesh& m, const attribute::TypedAttributeHandle<double>& coordinate);
     using Invariant::Invariant;
 
     bool before(const simplex::Simplex& t) const override;
 
 private:
-    const TypedAttributeHandle<double> m_coordinate_handle;
+    const attribute::TypedAttributeHandle<double> m_coordinate_handle;
 };
 } // namespace wmtk
