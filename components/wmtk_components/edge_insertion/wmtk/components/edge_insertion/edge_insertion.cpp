5e1b4f8bb7317fba9426a630bbba9859a190b11f
JcDai <jiacheng_dai@qq.com>
2024-03-21 17:47:11 -0400

    create edge insertion component

---
+++ components/wmtk_components/edge_insertion/wmtk/components/edge_insertion/edge_insertion.cpp
@@ -0,0 +1,9 @@
+#include "edge_insertion.hpp"
+
+#include "EdgeInsOptions.hpp"
+
+namespace wmtk::components {
+
+void edge_insertion(const base::Paths& paths, const nlohmann::json& j, io::Cache& cache) {}
+
+} // namespace wmtk::components
