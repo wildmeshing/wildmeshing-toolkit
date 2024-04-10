bdb80609c6275263e1ade0b3c453dfff4a7f123d
JcDai <jiacheng_dai@qq.com>
2024-04-10 14:53:56 -0400

    fix rounded invariant

--- src/wmtk/invariants/RoundedInvariant.cpp
+++ src/wmtk/invariants/RoundedInvariant.cpp
@@ -14,31 +14,11 @@ RoundedInvariant::RoundedInvariant(
 
 bool RoundedInvariant::before(const simplex::Simplex& t) const
 {
-    const std::array<Tuple, 2> vs = {
-        {t.tuple(), mesh().switch_tuple(t.tuple(), PrimitiveType::Vertex)}};
-
     auto accessor = mesh().create_const_accessor(m_coordinate_handle);
-
-    // check endpoints
-    const auto& p1 = accessor.const_vector_attribute(vs[0]);
-    const auto& p2 = accessor.const_vector_attribute(vs[1]);
-
     int dim = mesh().get_attribute_dimension(m_coordinate_handle);
 
     for (int i = 0; i < dim; ++i) {
-        if (!p1[i].is_rounded() || !p2[i].is_rounded()) return false;
-    }
-
-    // check one ring
-    for (int i = 0; i < 2; ++i) {
-        const auto one_ring_v = simplex::k_ring(mesh(), simplex::Simplex::vertex(vs[i]), 1)
-                                    .simplex_vector(PrimitiveType::Vertex);
-        for (const auto& v : one_ring_v) {
-            const auto& pv = accessor.const_vector_attribute(v.tuple());
-            for (int d = 0; d < dim; ++d) {
-                if (!pv[d].is_rounded()) return false;
-            }
-        }
+        if (!accessor.const_vector_attribute(t.tuple())[i].is_rounded()) return false;
     }
 
     return true;
