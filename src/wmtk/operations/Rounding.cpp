9c5284abd9e613ba0b3058f3cdcf898b0d7cda9f
Teseo Schneider <teseo@bluewin.ch>
2024-04-11 15:13:00 -0700

    better rounding

--- src/wmtk/operations/Rounding.cpp
+++ src/wmtk/operations/Rounding.cpp
@@ -19,11 +19,11 @@ std::vector<simplex::Simplex> Rounding::execute(const simplex::Simplex& simplex)
 
     auto accessor = mesh().create_accessor(m_coordinate_handle);
 
-    auto pos = accessor.vector_attribute(simplex.tuple());
+    // auto& pos = accessor.vector_attribute(simplex.tuple());
     for (int i = 0; i < mesh().get_attribute_dimension(m_coordinate_handle); ++i) {
-        pos[i].round();
+        accessor.vector_attribute(simplex.tuple())[i].round();
     }
-    accessor.vector_attribute(simplex.tuple()) = pos;
+    // accessor.vector_attribute(simplex.tuple()) = pos;
 
     return AttributesUpdate::execute(simplex);
 }
