447399d5d725a77bcaae89b913b23a67814d2c4c
Michael Tao <michael.tao@cims.nyu.edu>
2024-07-22 16:43:16 -0400

    updating autogen tempaltes to allow for many files

---
+++ scripts/templates/valid_index_from_tuple.hpp
@@ -0,0 +1,10 @@
+
+#pragma once
+#include <wmtk/Tuple.hpp>
+
+namespace wmtk::autogen::tet_mesh {
+int8_t valid_index_from_tuple(const Tuple& t);
+
+} // namespace wmtk::autogen::tet_mesh
+
+#include "valid_index_from_tuple.hxx"
