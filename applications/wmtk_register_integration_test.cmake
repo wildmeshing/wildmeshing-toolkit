44f7f71d36cdf25247a88ebc965fec18c6e735dd
teseoch <teseo.schneider@gmail.com>
2024-09-06 15:29:21 -0700

    wmtk_register_integration_test hack

--- applications/wmtk_register_integration_test.cmake
+++ applications/wmtk_register_integration_test.cmake
@@ -1,15 +1,22 @@
 macro(wmtk_register_integration_test EXEC_NAME CONFIG_FILE GIT_REPOSITORY GIT_TAG)
     include(CPM)
 
-    CPMAddPackage(
-        NAME wmtk_integration_data
+    MESSAGE(STATUS "Registering integration test for ${EXEC_NAME}")
+
+    string(MD5 REPO_HASH ${GIT_REPOSITORY})
+
+    SET(WMTK_I_DATA_FOLDER ${CPM_SOURCE_CACHE}/wmtk_integration_data/${REPO_HASH}/${GIT_TAG})
+
+    include(FetchContent)
+    FetchContent_Declare(
+        wmtk_integration_data_${GIT_TAG}
         GIT_REPOSITORY ${GIT_REPOSITORY}
         GIT_TAG ${GIT_TAG}
-        DOWNLOAD_ONLY True
+        SOURCE_DIR ${WMTK_I_DATA_FOLDER}
     )
+    FetchContent_MakeAvailable(wmtk_integration_data_${GIT_TAG})
 
-    list (APPEND WMTK_TEST_CONFIG "\"${EXEC_NAME}\":{\"data_folder\":\"${wmtk_integration_data_SOURCE_DIR}\", \"config_file\":\"${CONFIG_FILE}\"}")
+    list (APPEND WMTK_TEST_CONFIG "\"${EXEC_NAME}\":{\"data_folder\":\"${WMTK_I_DATA_FOLDER}\", \"config_file\":\"${CONFIG_FILE}\"}")
 
     SET(WMTK_TEST_CONFIG ${WMTK_TEST_CONFIG} PARENT_SCOPE)
-
 endmacro()
