1ac72df911c148568faf57d07a13e9f446da55aa
Michael Tao <michael.tao@cims.nyu.edu>
2024-09-06 17:24:32 -0400

    updating cmake cache dir default and setting a fallback incase the var is not set

--- applications/wmtk_register_integration_test.cmake
+++ applications/wmtk_register_integration_test.cmake
@@ -1,14 +1,20 @@
 macro(wmtk_register_integration_test EXEC_NAME CONFIG_FILE GIT_REPOSITORY GIT_TAG)
     include(CPM)
-
+if(CPM_SOURCE_CACHE)
+    set(CPM_TARGET_DIR wmtk_integration_data)
+else()
+    set(CPM_TARGET_DIR wmtk_${EXEC_NAME}_data)
+endif()
     CPMAddPackage(
-        NAME wmtk_integration_data
+        NAME ${CPM_TARGET_DIR}
         GIT_REPOSITORY ${GIT_REPOSITORY}
         GIT_TAG ${GIT_TAG}
         DOWNLOAD_ONLY True
     )
 
-    list (APPEND WMTK_TEST_CONFIG "\"${EXEC_NAME}\":{\"data_folder\":\"${wmtk_integration_data_SOURCE_DIR}\", \"config_file\":\"${CONFIG_FILE}\"}")
+    set(CPM_DATA_DIR ${CPM_TARGET_DIR}_SOURCE_DIR)
+
+list (APPEND WMTK_TEST_CONFIG "\"${EXEC_NAME}\":{\"data_folder\":\"${CPM_DATA_DIR}\", \"config_file\":\"${CONFIG_FILE}\"}")
 
     SET(WMTK_TEST_CONFIG ${WMTK_TEST_CONFIG} PARENT_SCOPE)
 
