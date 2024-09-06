3fd599e0935be41f6240a584a833a7b1fbfa1c65
Michael Tao <michael.tao@cims.nyu.edu>
2024-09-06 17:09:00 -0400

    fixing a name

--- applications/wmtk_register_integration_test.cmake
+++ applications/wmtk_register_integration_test.cmake
@@ -8,7 +8,7 @@ macro(wmtk_register_integration_test EXEC_NAME CONFIG_FILE GIT_REPOSITORY GIT_TA
         DOWNLOAD_ONLY True
     )
 
-    list (APPEND WMTK_TEST_CONFIG "\"${EXEC_NAME}\":{\"data_folder\":\"${wmtk_${EXEC_NAME}_data_SOURCE_DIR}\", \"config_file\":\"${CONFIG_FILE}\"}")
+    list (APPEND WMTK_TEST_CONFIG "\"${EXEC_NAME}\":{\"data_folder\":\"${wmtk_integration_data_SOURCE_DIR}\", \"config_file\":\"${CONFIG_FILE}\"}")
 
     SET(WMTK_TEST_CONFIG ${WMTK_TEST_CONFIG} PARENT_SCOPE)
 
