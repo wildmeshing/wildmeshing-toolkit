8113e7fce49167727acd8eba2ffe7d7d2c188ab2
Daniel Zint <daniel.zint@mail.de>
2024-09-07 18:51:06 -0400

    Use FetchContent_Populate instead of Declare and MakeAvailable.

--- applications/wmtk_register_integration_test.cmake
+++ applications/wmtk_register_integration_test.cmake
@@ -4,15 +4,17 @@ macro(wmtk_register_integration_test EXEC_NAME CONFIG_FILE GIT_REPOSITORY GIT_TA
     MESSAGE(STATUS "Registering integration test for ${EXEC_NAME}")
     string(MD5 REPO_HASH "${GIT_REPOSITORY}${GIT_TAG}")
 
-    # SET(WMTK_I_DATA_FOLDER ${CPM_SOURCE_CACHE}/wmtk_integration_data/${REPO_HASH})
+    SET(WMTK_I_DATA_FOLDER ${CPM_SOURCE_CACHE}/wmtk_integration_data/${REPO_HASH})
+    MESSAGE(STATUS "Test data stored at ${WMTK_I_DATA_FOLDER}")
 
-    # include(FetchContent)
-    # FetchContent_Declare(
-    #     wmtk_id_${REPO_HASH}
-    #     GIT_REPOSITORY ${GIT_REPOSITORY}
-    #     GIT_TAG ${GIT_TAG}
-    #     SOURCE_DIR ${WMTK_I_DATA_FOLDER}
-    # )
+    include(FetchContent)
+    FetchContent_Populate(
+        wmtk_id
+        QUIET
+        GIT_REPOSITORY ${GIT_REPOSITORY}
+        GIT_TAG ${GIT_TAG}
+        SOURCE_DIR ${WMTK_I_DATA_FOLDER}
+    )
     # FetchContent_MakeAvailable(wmtk_id_${REPO_HASH})
 
     # message(STATUS "Test data folder: ${WMTK_I_DATA_FOLDER}")
@@ -32,13 +34,14 @@ macro(wmtk_register_integration_test EXEC_NAME CONFIG_FILE GIT_REPOSITORY GIT_TA
     # list (APPEND WMTK_TEST_CONFIG "\"${EXEC_NAME}\":{\"data_folder\":\"${WMTK_I_DATA_FOLDER}\", \"config_file\":\"${CONFIG_FILE}\"}")
 
 
-    CPMAddPackage(
-        NAME wmtk_itd_${REPO_HASH}
-        GIT_REPOSITORY ${GIT_REPOSITORY}
-        GIT_TAG ${GIT_TAG}
-        DOWNLOAD_ONLY TRUE
-    )
-    list (APPEND WMTK_TEST_CONFIG "\"${EXEC_NAME}\":{\"data_folder\":\"${wmtk_itd_${REPO_HASH}_SOURCE_DIR}\", \"config_file\":\"${CONFIG_FILE}\"}")
+    # CPMAddPackage(
+    #     NAME wmtk_itd_${REPO_HASH}
+    #     GIT_REPOSITORY ${GIT_REPOSITORY}
+    #     GIT_TAG ${GIT_TAG}
+    #     GIT_SHALLOW TRUE
+    #     DOWNLOAD_ONLY TRUE
+    # )
+    # list (APPEND WMTK_TEST_CONFIG "\"${EXEC_NAME}\":{\"data_folder\":\"${wmtk_itd_${REPO_HASH}_SRC}\", \"config_file\":\"${CONFIG_FILE}\"}")
 
 
     SET(WMTK_TEST_CONFIG ${WMTK_TEST_CONFIG} PARENT_SCOPE)
