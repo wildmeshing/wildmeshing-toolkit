1aa66e05f81085426f68bede4c7a59f251cb39a3
Daniel Zint <daniel.zint@mail.de>
2024-09-09 17:34:56 -0400

    Download data into cpm cache by default.

--- applications/wmtk_register_integration_test.cmake
+++ applications/wmtk_register_integration_test.cmake
@@ -1,48 +1,13 @@
-macro(wmtk_register_integration_test EXEC_NAME CONFIG_FILE GIT_REPOSITORY GIT_TAG)
+function(wmtk_register_integration_test EXEC_NAME CONFIG_FILE GIT_REPOSITORY GIT_TAG)
     include(CPM)
 
     MESSAGE(STATUS "Registering integration test for ${EXEC_NAME}")
     string(MD5 REPO_HASH "${GIT_REPOSITORY}${GIT_TAG}")
 
-    SET(WMTK_I_DATA_FOLDER ${CPM_SOURCE_CACHE}/wmtk_integration_data/${REPO_HASH})
-    MESSAGE(STATUS "Test data stored at ${WMTK_I_DATA_FOLDER}")
+    include(wmtk_download_data)
+    wmtk_download_data(DATA_DIR ${GIT_REPOSITORY} ${GIT_TAG})
 
-    include(FetchContent)
-    FetchContent_Populate(
-        wmtk_id
-        QUIET
-        GIT_REPOSITORY ${GIT_REPOSITORY}
-        GIT_TAG ${GIT_TAG}
-        SOURCE_DIR ${WMTK_I_DATA_FOLDER}
-    )
-    # FetchContent_MakeAvailable(wmtk_id_${REPO_HASH})
-
-    # message(STATUS "Test data folder: ${WMTK_I_DATA_FOLDER}")
-    # message(STATUS "Test data name: wmtk_id_${REPO_HASH}")
-
-    # ExternalProject_Add(
-    #     wmtk_id_${REPO_HASH}
-    #     SOURCE_DIR ${WMTK_I_DATA_FOLDER}
-    #     GIT_REPOSITORY ${GIT_REPOSITORY}
-    #     GIT_TAG ${GIT_TAG}
-    #     CONFIGURE_COMMAND ""
-    #     BUILD_COMMAND ""
-    #     INSTALL_COMMAND ""
-    #     LOG_DOWNLOAD ON
-    # )
-    # add_dependencies(${EXEC_NAME} wmtk_id_${REPO_HASH})
-    # list (APPEND WMTK_TEST_CONFIG "\"${EXEC_NAME}\":{\"data_folder\":\"${WMTK_I_DATA_FOLDER}\", \"config_file\":\"${CONFIG_FILE}\"}")
-
-
-    # CPMAddPackage(
-    #     NAME wmtk_itd_${REPO_HASH}
-    #     GIT_REPOSITORY ${GIT_REPOSITORY}
-    #     GIT_TAG ${GIT_TAG}
-    #     GIT_SHALLOW TRUE
-    #     DOWNLOAD_ONLY TRUE
-    # )
-    # list (APPEND WMTK_TEST_CONFIG "\"${EXEC_NAME}\":{\"data_folder\":\"${wmtk_itd_${REPO_HASH}_SRC}\", \"config_file\":\"${CONFIG_FILE}\"}")
-
+    list (APPEND WMTK_TEST_CONFIG "\"${EXEC_NAME}\":{\"data_folder\":\"${DATA_DIR}\", \"config_file\":\"${CONFIG_FILE}\"}")
 
     SET(WMTK_TEST_CONFIG ${WMTK_TEST_CONFIG} PARENT_SCOPE)
-endmacro()
+endfunction()
--- cmake/recipes/tests/wmtk_data.cmake
+++ cmake/recipes/tests/wmtk_data.cmake
@@ -5,30 +5,12 @@ if(TARGET wmtk::data)
     return()
 endif()
 
-include(ExternalProject)
-include(FetchContent)
-
-set(WMTK_DATA_ROOT "${PROJECT_SOURCE_DIR}/data/" CACHE PATH "Where should the toolkit download and look for test data?")
-
-ExternalProject_Add(
-    wmtk_data_download
-    PREFIX "${FETCHCONTENT_BASE_DIR}/wmtk-test-data"
-    SOURCE_DIR ${WMTK_DATA_ROOT}
-
-    GIT_REPOSITORY https://github.com/wildmeshing/data.git
-    GIT_TAG
+include(wmtk_download_data)
+wmtk_download_data(DATA_DIR 
+    https://github.com/wildmeshing/data.git
     152a561697a6e923451ca8535309cbe1e116a9fa
+    )
 
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-    LOG_DOWNLOAD ON
-)
-
-# Create a dummy target for convenience
 add_library(wmtk_data INTERFACE)
 add_library(wmtk::data ALIAS wmtk_data)
-
-add_dependencies(wmtk_data wmtk_data_download)
-
-target_compile_definitions(wmtk_data INTERFACE WMTK_DATA_DIR=\"${WMTK_DATA_ROOT}\")
+target_compile_definitions(wmtk_data INTERFACE WMTK_DATA_DIR=\"${DATA_DIR}\")
