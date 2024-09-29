21e35de8d8511723ae1dce4c8e8841f79c74ee70
Daniel Zint <daniel.zint@mail.de>
2024-09-29 17:44:44 -0400

    Set up iterable for TriMesh vertex.

--- src/wmtk/simplex/iterable/TopDimensionCofacesIterable.cpp
+++ src/wmtk/simplex/iterable/TopDimensionCofacesIterable.cpp
@@ -5,8 +5,92 @@
 namespace wmtk::simplex {
 
 
-TopDimensionCofacesIterable::TopDimensionCofacesIterable(const Mesh& mesh, const Simplex& simplex)
-    : m_collection(top_dimension_cofaces(mesh, simplex))
+TopDimensionCofacesIterable::TopDimensionCofacesIterable(Mesh& mesh, const Simplex& simplex)
+    //: m_collection(top_dimension_cofaces(mesh, simplex))
+    : m_mesh(&mesh)
+    , m_simplex(simplex)
 {}
+
+TopDimensionCofacesIterable::Iterator::Iterator(Mesh& mesh, const Simplex& simplex, bool is_done)
+    : m_mesh(&mesh)
+    , m_simplex(simplex)
+    , t(simplex.tuple())
+    , m_phase(IteratorPhase::Forward)
+    , m_is_end(is_done)
+{
+    if (m_is_end) {
+        return;
+    }
+
+    constexpr PrimitiveType PE = PrimitiveType::Edge;
+
+    // check if forward or backward phase can be executed
+    if (m_mesh->is_boundary(PE, t)) {
+        m_phase = IteratorPhase::Backward;
+
+        // check if a backward phase exists
+        const Tuple opp_of_input = m_mesh->switch_tuple(m_simplex.tuple(), PE);
+        if (m_mesh->is_boundary(PE, opp_of_input)) {
+            m_phase = IteratorPhase::End;
+        }
+    }
+}
+
+TopDimensionCofacesIterable::Iterator TopDimensionCofacesIterable::Iterator::operator++()
+{
+    constexpr PrimitiveType PE = PrimitiveType::Edge;
+    constexpr PrimitiveType PF = PrimitiveType::Triangle;
+
+    if (m_phase == IteratorPhase::End) {
+        m_is_end = true;
+        return *this;
+    }
+
+    if (m_phase == IteratorPhase::Forward) {
+        // switch to backward phase
+        t = m_mesh->switch_tuple(m_simplex.tuple(), PE);
+        m_phase = IteratorPhase::Backward;
+    }
+
+    t = m_mesh->switch_tuples(t, {PF, PE});
+
+    if (t == m_simplex.tuple()) {
+        m_is_end = true;
+        return *this;
+    }
+
+    if (m_mesh->is_boundary(PE, t)) {
+        if (m_phase == IteratorPhase::Forward) {
+            // check if a backward phase exists
+            const Tuple opp_of_input = m_mesh->switch_tuple(m_simplex.tuple(), PE);
+            if (m_mesh->is_boundary(PE, opp_of_input)) {
+                m_phase = IteratorPhase::End;
+            } else {
+                m_phase = IteratorPhase::Backward;
+            }
+        } else {
+            m_phase = IteratorPhase::End;
+        }
+    }
+
+    return *this;
+}
+
+bool TopDimensionCofacesIterable::Iterator::operator!=(const Iterator& other) const
+{
+    // const bool diff_tuple = (t != other.t);
+    const bool diff_end = (m_is_end != other.m_is_end);
+    return diff_end;
+}
+
+Tuple TopDimensionCofacesIterable::Iterator::operator*()
+{
+    return t;
+}
+
+const Tuple& TopDimensionCofacesIterable::Iterator::operator*() const
+{
+    return t;
+}
 
 } // namespace wmtk::simplex
--- src/wmtk/simplex/iterable/TopDimensionCofacesIterable.hpp
+++ src/wmtk/simplex/iterable/TopDimensionCofacesIterable.hpp
@@ -6,17 +6,44 @@
 
 namespace wmtk::simplex {
 
-
+/**
+ * For TriMesh:
+ * Iterate first in forward direction, using `switch_tuples(t, {PF, PE})`.
+ * If a boundary is hit, restart from the input simplex, and switch direction.
+ *
+ * Stop iterating if the boundary was hit twice, or the input simplex was reached again.
+ */
 class TopDimensionCofacesIterable
 {
 public:
-    TopDimensionCofacesIterable(const Mesh& mesh, const Simplex& simplex);
+    enum IteratorPhase { Forward = 0, Intermediate = 1, Backward = 2, End = 3 };
 
-    auto begin() { return m_collection.simplex_vector().begin(); }
-    auto end() { return m_collection.simplex_vector().end(); }
+    class Iterator
+    {
+    public:
+        Iterator(Mesh& mesh, const Simplex& simplex, bool is_done = false);
+        Iterator operator++();
+        bool operator!=(const Iterator& other) const;
+        Tuple operator*();
+        const Tuple& operator*() const;
+
+    private:
+        Mesh* m_mesh;
+        Simplex m_simplex;
+        Tuple t;
+        IteratorPhase m_phase = IteratorPhase::Forward;
+        bool m_is_end = false;
+    };
+
+public:
+    TopDimensionCofacesIterable(Mesh& mesh, const Simplex& simplex);
+
+    Iterator begin() const { return Iterator(*m_mesh, m_simplex); }
+    Iterator end() const { return Iterator(*m_mesh, m_simplex, true); }
 
 private:
-    SimplexCollection m_collection;
+    Mesh* m_mesh;
+    Simplex m_simplex;
 };
 
 } // namespace wmtk::simplex
