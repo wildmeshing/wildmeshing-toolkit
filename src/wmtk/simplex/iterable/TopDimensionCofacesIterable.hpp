955b3f856a68aa21c6fe47956a5884afac581c0e
Daniel Zint <daniel.zint@mail.de>
2024-09-30 16:59:15 -0400

    Add some documentation.

--- src/wmtk/simplex/iterable/TopDimensionCofacesIterable.hpp
+++ src/wmtk/simplex/iterable/TopDimensionCofacesIterable.hpp
@@ -18,6 +18,9 @@ namespace wmtk::simplex {
 class TopDimensionCofacesIterable
 {
 public:
+    /**
+     * The IteratorPhase is only used for depth 1 and 2.
+     */
     enum IteratorPhase { Forward = 0, Intermediate = 1, Backward = 2, End = 3 };
 
     class Iterator
@@ -33,29 +36,59 @@ public:
         /**
          * @brief Get the d - depth primitive type.
          *
-         * E.g., for a triangle mesh the d-1 simplex is an edge, and the d-2 simplex a vertex.
+         * Example: for a TriMesh the d-1 simplex is an edge, and the d-2 simplex a vertex.
          */
         PrimitiveType pt(int64_t depth) const;
+        /**
+         * @brief Compute the depth from the mesh and the simplex type.
+         *
+         * The depth is "mesh top simplex dimension" - "simplex dimension".
+         */
         int64_t depth();
 
+        /**
+         * @brief Depending on the depth, the iterator must be initialized differently.
+         */
         void init(int64_t depth);
 
+        /**
+         * @brief Just return the simplex and stop.
+         *
+         * Example: triangle in a TriMesh.
+         */
         Iterator step_depth_0();
+        /**
+         * @brief There are at max two d-simplices.
+         *
+         * Example: edge in a TriMesh.
+         * Example: triangle in a TetMesh.
+         */
         Iterator step_depth_1();
+        /**
+         * @brief Iterate around simplex to find all d-simplices.
+         *
+         * Example: vertex in a TriMesh.
+         * Example: edge in a TetMesh.
+         */
         Iterator step_depth_2();
+        /**
+         * @brief Use breadth first search to find all d-simplices.
+         *
+         * Example: vertex in a TetMesh.
+         */
         Iterator step_depth_3();
 
 
     private:
         const Mesh* m_mesh;
 
-        Simplex m_simplex;
-        Tuple m_t;
-        IteratorPhase m_phase = IteratorPhase::Forward;
-        bool m_is_end = false;
+        Simplex m_simplex; // the input simplex
+        Tuple m_t; // the tuple that iterates through the mesh
+        IteratorPhase m_phase = IteratorPhase::Forward; // for depth 1 and 2 iteration
+        bool m_is_end = false; // mark iterator as end
 
-        std::queue<Tuple> m_queue;
-        std::vector<bool> m_visited;
+        std::queue<Tuple> m_queue; // for depth 3 iteration
+        std::vector<bool> m_visited; // for depth 3 iteration
     };
 
 public:
