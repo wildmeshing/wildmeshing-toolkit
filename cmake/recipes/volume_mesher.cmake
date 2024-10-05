ee7ed37696e28156b10dcfbbc6ca2e33202e170e
Daniele Panozzo <daniele.panozzo@gmail.com>
2024-10-04 22:52:53 -0400

    bumped version of volume_mesher to fix windows ci

--- cmake/recipes/volume_mesher.cmake
+++ cmake/recipes/volume_mesher.cmake
@@ -7,7 +7,6 @@ endif()
 message(STATUS "Third-party: creating target 'VolumeMesher'")
 
 include(CPM)
-CPMAddPackage("gh:JcDai/VolumeRemesher#dffb9ca9ab14a0d8fd4aa3eaa21fafed3980ccfb")
+CPMAddPackage("gh:JcDai/VolumeRemesher#8927a1e057c643f1193ed50117bb3fb311b81b09")
 
 set_target_properties(mesh_generator_lib PROPERTIES FOLDER third-party)
-
