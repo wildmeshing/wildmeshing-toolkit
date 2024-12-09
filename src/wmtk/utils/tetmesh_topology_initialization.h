21bf802071494f7274e2252f399b44d731b1d8fb
Michael Tao <michael.tao@nyu.edu>
2024-12-09 13:22:36 -0500

    changing header parameters to more clearly indicate that params are vertex indices

--- src/wmtk/utils/tetmesh_topology_initialization.h
+++ src/wmtk/utils/tetmesh_topology_initialization.h
@@ -12,6 +12,6 @@ namespace wmtk {
  */
 
 std::tuple<RowVectors6l, RowVectors4l, RowVectors4l, VectorXl, VectorXl, VectorXl>
-tetmesh_topology_initialization(Eigen::Ref<const RowVectors4l> T);
+tetmesh_topology_initialization(Eigen::Ref<const RowVectors4l> TV);
 // returns TE, TF, TT, VT, ET, FT
 } // namespace wmtk
--- src/wmtk/utils/trimesh_topology_initialization.h
+++ src/wmtk/utils/trimesh_topology_initialization.h
@@ -7,6 +7,6 @@
 namespace wmtk {
 
 std::tuple<RowVectors3l,RowVectors3l,VectorXl,VectorXl> trimesh_topology_initialization(
-    Eigen::Ref<const RowVectors3l> F); // returns {FE, FF, VF, EF}
+    Eigen::Ref<const RowVectors3l> FV); // returns {FE, FF, VF, EF}
 
 }
