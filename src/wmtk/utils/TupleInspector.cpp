008fd84e7d8334e1fcfe26f4723e7a2583c92d00
Michael Tao <mtao@dgp.toronto.edu>
2024-12-10 15:36:35 -0500

    removing global id stuff that was breaking tupleinspector

--- src/wmtk/utils/TupleInspector.cpp
+++ src/wmtk/utils/TupleInspector.cpp
@@ -4,10 +4,6 @@
 #include <wmtk/utils/Logger.hpp>
 
 namespace wmtk::utils {
-static int64_t TupleInspector::global_id(const Mesh& m, const Tuple& t, PrimitiveType pt)
-{
-    return m.id(t, pt);
-}
 std::string TupleInspector::as_string(const Tuple& t)
 {
     return fmt::format(
