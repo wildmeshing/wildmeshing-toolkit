db79155256019ed138b0ba117556479b5da60c27
Michael Tao <michael.tao@nyu.edu>
2024-12-10 14:56:18 -0500

    adding an index-based manifoldy checker

--- src/wmtk/utils/internal/IndexSimplexMapper.cpp
+++ src/wmtk/utils/internal/IndexSimplexMapper.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <set>
 #include <wmtk/Mesh.hpp>
+#include <wmtk/simplex/IdSimplex.hpp>
 #include <wmtk/utils/EigenMatrixWriter.hpp>
 
 namespace wmtk::utils::internal {
@@ -71,26 +72,35 @@ IndexSimplexMapper::IndexSimplexMapper(Eigen::Ref<const MatrixXl> S)
     default: assert(false); break;
     }
 }
+
+int64_t IndexSimplexMapper::id(const simplex::IdSimplex& id)
+{
+    return id.index();
+}
 void IndexSimplexMapper::initialize_edge_mesh(Eigen::Ref<const RowVectors2l> S)
 {
-    auto SV = from_eigen<2>(S);
-    m_E = make_map<2>(SV);
-    m_V = make_child_map<2, 1>(SV);
+    m_E = from_eigen<2>(S);
+    m_E_map = make_map<2>(m_E);
+    m_V_map = make_child_map<2, 1>(m_E);
+    update_simplices();
 }
 void IndexSimplexMapper::initialize_tri_mesh(Eigen::Ref<const RowVectors3l> S)
 {
-    auto SV = from_eigen<3>(S);
-    m_F = make_map<3>(SV);
-    m_E = make_child_map<3, 2>(SV);
-    m_V = make_child_map<3, 1>(SV);
+    m_F = from_eigen<3>(S);
+    m_F_map = make_map<3>(m_F);
+    m_E_map = make_child_map<3, 2>(m_F);
+    m_V_map = make_child_map<3, 1>(m_F);
+    update_simplices();
 }
 void IndexSimplexMapper::initialize_tet_mesh(Eigen::Ref<const RowVectors4l> S)
 {
-    auto SV = from_eigen<4>(S);
-    m_T = make_map<4>(SV);
-    m_F = make_child_map<4, 3>(SV);
-    m_E = make_child_map<4, 2>(SV);
-    m_V = make_child_map<4, 1>(SV);
+    m_T = from_eigen<4>(S);
+    m_T_map = make_map<4>(m_T);
+    m_F_map = make_child_map<4, 3>(m_T);
+    m_E_map = make_child_map<4, 2>(m_T);
+    m_V_map = make_child_map<4, 1>(m_T);
+
+    update_simplices();
 }
 template <int Dim, int ChildDim>
 std::map<std::array<int64_t, ChildDim>, int64_t> IndexSimplexMapper::make_child_map(
@@ -112,6 +122,21 @@ std::map<std::array<int64_t, Dim>, int64_t> IndexSimplexMapper::make_map(
     }
     return mp;
 }
+void IndexSimplexMapper::update_simplices()
+{
+    auto update = [](const auto& mp, auto& vec) {
+        vec.resize(mp.size());
+
+        for (const auto& [arr, ind] : mp) {
+            vec[ind] = arr;
+        }
+    };
+
+    update(m_V_map, m_V);
+    update(m_E_map, m_E);
+    update(m_F_map, m_F);
+    update(m_T_map, m_T);
+}
 
 template <int Dim>
 int64_t IndexSimplexMapper::get_index(
@@ -130,4 +155,85 @@ int64_t get_index(
     Eigen::Map<RowVector<int64_t, Dim>>(a.data()) = s;
     return get_index(a, mp);
 }
+
+template <int Dim>
+int64_t IndexSimplexMapper::get_index(const std::array<int64_t, Dim + 1>& s) const
+{
+    return get_index<Dim + 1>(s, simplex_map<Dim>());
+}
+
+template <int Dim, int ChildDim>
+std::vector<int64_t> IndexSimplexMapper::faces(size_t index) const
+{
+    if constexpr (Dim < ChildDim) {
+        return {};
+    } else {
+        auto s = simplices<Dim>().at(index);
+        auto faces = get_simplices<Dim + 1, ChildDim + 1>(s);
+        std::vector<int64_t> r;
+        r.reserve(faces.size());
+        std::transform(
+            faces.begin(),
+            faces.end(),
+            std::back_inserter(r),
+            [&](const std::array<int64_t, ChildDim + 1>& d) {
+                return simplex_map<ChildDim>().at(d);
+            });
+        return r;
+    }
+}
+std::vector<int64_t> IndexSimplexMapper::faces(size_t index, int8_t dim, int8_t child_dim) const
+{
+    auto run = [&](const auto& d) -> std::vector<int64_t> {
+        using D = std::decay_t<decltype(d)>;
+
+        constexpr static int Dim = D::value;
+        switch (child_dim) {
+        case 0: return faces<Dim, 0>(index);
+        case 1: return faces<Dim, 1>(index);
+        case 2: return faces<Dim, 2>(index);
+        case 3: return faces<Dim, 3>(index);
+        default: assert(false); return {};
+        }
+    };
+    switch (dim) {
+    case 0: return run(std::integral_constant<int, 0>{});
+    case 1: return run(std::integral_constant<int, 1>{});
+    case 2: return run(std::integral_constant<int, 2>{});
+    case 3: return run(std::integral_constant<int, 3>{});
+    default: assert(false); return {};
+    }
+}
+
+template <int D>
+const std::map<std::array<int64_t, D + 1>, int64_t>& IndexSimplexMapper::simplex_map() const
+{
+    if constexpr (D == 0) {
+        return m_V_map;
+    } else if constexpr (D == 1) {
+        return m_E_map;
+    } else if constexpr (D == 2) {
+        return m_F_map;
+    } else if constexpr (D == 3) {
+        return m_T_map;
+    }
+}
+template <int D>
+const std::vector<std::array<int64_t, D + 1>>& IndexSimplexMapper::simplices() const
+{
+    if constexpr (D == 0) {
+        return m_V;
+    } else if constexpr (D == 1) {
+        return m_E;
+    } else if constexpr (D == 2) {
+        return m_F;
+    } else if constexpr (D == 3) {
+        return m_T;
+    }
+}
+
+template int64_t IndexSimplexMapper::get_index<0>(const std::array<int64_t, 1>& s) const;
+template int64_t IndexSimplexMapper::get_index<1>(const std::array<int64_t, 2>& s) const;
+template int64_t IndexSimplexMapper::get_index<2>(const std::array<int64_t, 3>& s) const;
+template int64_t IndexSimplexMapper::get_index<3>(const std::array<int64_t, 4>& s) const;
 } // namespace wmtk::utils::internal
