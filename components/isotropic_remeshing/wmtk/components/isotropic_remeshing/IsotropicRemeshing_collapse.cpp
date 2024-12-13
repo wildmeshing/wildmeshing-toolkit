a2d8f7fdd6f246dabac4d93bafb798518d9ed5f9
Michael Tao <mtao@dgp.toronto.edu>
2024-12-13 13:13:54 -0500

    adding extra isotropic remeshing files

---
+++ components/isotropic_remeshing/wmtk/components/isotropic_remeshing/IsotropicRemeshing_collapse.cpp
@@ -0,0 +1,32 @@
+
+#include <wmtk/invariants/InvariantCollection.hpp>
+#include <wmtk/invariants/MultiMeshLinkConditionInvariant.hpp>
+#include <wmtk/invariants/MultiMeshMapValidInvariant.hpp>
+#include <wmtk/invariants/SimplexInversionInvariant.hpp>
+#include <wmtk/multimesh/MultiMeshVisitor.hpp>
+#include <wmtk/operations/EdgeCollapse.hpp>
+#include <wmtk/utils/Logger.hpp>
+#include "IsotropicRemeshing.hpp"
+
+
+#include <Eigen/Geometry>
+#include <wmtk/invariants/InvariantCollection.hpp>
+
+#include <wmtk/Mesh.hpp>
+#include "internal/configure_collapse.hpp"
+
+namespace wmtk::components::isotropic_remeshing {
+void IsotropicRemeshing::configure_collapse()
+{
+    wmtk::logger().debug("Configure isotropic remeshing collapse");
+    wmtk::Mesh& mesh = m_options.position_attribute.mesh();
+    auto op = std::make_shared<operations::EdgeCollapse>(mesh);
+    internal::configure_collapse(*op, mesh, m_options);
+
+    if (m_envelope_invariants) {
+        op->add_invariant(m_envelope_invariants);
+    }
+    assert(op->attribute_new_all_configured());
+    m_collapse = op;
+}
+} // namespace wmtk::components::isotropic_remeshing
---
+++ components/isotropic_remeshing/wmtk/components/isotropic_remeshing/IsotropicRemeshing_swap.cpp
@@ -0,0 +1,41 @@
+#include <wmtk/operations/EdgeCollapse.hpp>
+#include <wmtk/operations/EdgeSplit.hpp>
+#include <wmtk/operations/composite/EdgeSwap.hpp>
+#include <wmtk/operations/composite/TetEdgeSwap.hpp>
+#include <wmtk/operations/composite/TriEdgeSwap.hpp>
+#include <wmtk/utils/Logger.hpp>
+#include "IsotropicRemeshing.hpp"
+#include "internal/configure_collapse.hpp"
+#include "internal/configure_split.hpp"
+#include "internal/configure_swap.hpp"
+namespace wmtk::components::isotropic_remeshing {
+
+void IsotropicRemeshing::configure_swap()
+{
+    // adds common invariants like inversion check and asserts taht the swap is ready for prime time
+    wmtk::logger().debug("Configure isotropic remeshing swap");
+    wmtk::Mesh& mesh = m_options.position_attribute.mesh();
+    switch (mesh.top_simplex_type()) {
+    case PrimitiveType::Triangle:
+        m_swap = std::make_shared<operations::composite::TriEdgeSwap>(static_cast<TriMesh&>(mesh));
+        break;
+    case PrimitiveType::Tetrahedron:
+        m_swap = std::make_shared<operations::composite::TetEdgeSwap>(static_cast<TetMesh&>(mesh));
+        break;
+    case PrimitiveType::Vertex:
+    case PrimitiveType::Edge:
+    default: assert(false); break;
+    }
+
+    internal::configure_split(m_swap->split(), mesh, m_options);
+    internal::configure_collapse(m_swap->collapse(), mesh, m_options);
+
+    if (m_envelope_invariants) {
+        m_swap->collapse().add_invariant(m_envelope_invariants);
+    }
+
+    internal::configure_swap_transfer(*m_swap, m_options.position_attribute);
+    internal::finalize_swap(*m_swap, m_options);
+    // m_swap = op;
+}
+} // namespace wmtk::components::isotropic_remeshing
