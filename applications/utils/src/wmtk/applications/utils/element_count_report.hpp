29858d750b7a74f7afa112f677f950cea84fb00f
Michael Tao <mtao@dgp.toronto.edu>
2024-12-13 13:33:38 -0500

    adding comments to application utils

--- applications/utils/src/wmtk/applications/utils/element_count_report.hpp
+++ applications/utils/src/wmtk/applications/utils/element_count_report.hpp
@@ -11,8 +11,17 @@ namespace wmtk {
 
 namespace wmtk::applications::utils {
 
-    std::vector<int64_t> element_count_report(const Mesh& m);
+
+// Generates size statistics for each mesh in a mesh collection. Useful for "reports" used in integratino tests
+// Something like:
+// {"pos": {"vertices": 3, "edges": 24}, "pos.uv": {"vertices": 0, "edges": 23}}
+    nlohmann::json element_count_report_named(const components::multimesh::MeshCollection& m);
+
+// produces a json file corresponding colloquial simplex type names to the counts 
+// Soemthing like: {"vertices": 3, "edges": 24}
     nlohmann::json element_count_report_named(const Mesh& m);
 
-    nlohmann::json element_count_report_named(const components::multimesh::MeshCollection& m);
+
+// produces the number of simplices of each type in a mesh - useful for generating json reports on the elements in a mesh
+    std::vector<int64_t> element_count_report(const Mesh& m);
 }
