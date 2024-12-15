f58b053d188803bcec86e58ee57622af54d9af25
Michael Tao <mtao@dgp.toronto.edu>
2024-12-14 20:29:46 -0500

    converting string_view to string in catch2

--- tests/attributes/attribute_type.cpp
+++ tests/attributes/attribute_type.cpp
@@ -9,10 +9,11 @@ using namespace wmtk::attribute;
 TEST_CASE("test_attribute_type_names", "[attributes]")
 {
     using AT = AttributeType;
-    CHECK(attribute_type_name(AT::Char) == "Char");
-    CHECK(attribute_type_name(AT::Double) == "Double");
-    CHECK(attribute_type_name(AT::Int64) == "Int64");
-    CHECK(attribute_type_name(AT::Rational) == "Rational");
+    // converting to string because some compilers fail with this combo of catch + string_view comparisons?
+    CHECK(std::string(attribute_type_name(AT::Char)) == "Char");
+    CHECK(std::string(attribute_type_name(AT::Double)) == "Double");
+    CHECK(std::string(attribute_type_name(AT::Int64)) == "Int64");
+    CHECK(std::string(attribute_type_name(AT::Rational)) == "Rational");
 
 }
 
