dea6a9e582b4d3cbb850345fb0bfa1b3504d49d6
Michael Tao <mtao@dgp.toronto.edu>
2025-01-11 14:42:51 -0500

    minor changes

--- components/mesh_info/src/wmtk/components/mesh_info/simplex/operators/DihedralAngleOperator.hpp
+++ components/mesh_info/src/wmtk/components/mesh_info/simplex/operators/DihedralAngleOperator.hpp
@@ -1,5 +1,7 @@
 #p
+#include <wmtk/TriMesh.hpp>
 #include <wmtk/operations/attribute_update/AttributeTransferStrategy.hpp>
+#include <wmtk/simplex/Simplex.hpp>
 #include <wmtk/utils/triangle_areas.hpp>
 namespace wmtk {
 class Mesh;
@@ -11,14 +13,16 @@ namespace wmtk::components::mesh_info::simplex::operators {
 
 template <typename MyType, typename ParentType, int ParentDim = Eigen::Dynamic>
 class DihedralAngleOperator
-    : public wmtk::operations::AttributeTransferStrategy<ParentType>
+    : public wmtk::operations::SingleAttributeTransferStrategyBase<MyType, ParentType>
 {
 public:
-    using BaseType =
-        wmtk::operations::AttributeTransferStrategy<ParentType>;
-    using MyVecType = typename BaseType::MyVecType;
-    using ParentMatType = typename BaseType::ParentMatType;
+    using BaseType = wmtk::operations::SingleAttributeTransferStrategyBase<MyType, ParentType>;
 
+    using BaseType::handle;
+    using BaseType::mesh;
+    using BaseType::parent_handle;
+    using BaseType::primitive_type;
+    /*
     static MyVecType convert(const ParentMatType& v)
     {
         constexpr static std::array<int, 4> quot = {{1, 1, 2, 6}};
@@ -71,14 +75,71 @@ public:
             }
         }
     }
+    */
+
+    void run(const wmtk::simplex::Simplex& s) const final
+    {
+        assert(handle().mesh().is_valid(s.tuple()));
+        if (s.primitive_type() != primitive_type()) {
+            // TODO: is this an error out or silent fail
+            return;
+        }
+        // position mesh
+        const TriMesh& pmesh = static_cast<const TriMesh&>(parent_handle().mesh());
+
+        auto ang_acc =
+            parent_handle().mesh().create_const_accessor(parent_handle().template as<ParentType>());
+        auto acc = const_cast<Mesh&>(mesh()).create_accessor(handle().template as<MyType>());
+
+        auto& angle = acc.scalar_attribute(s.tuple());
+        if (pmesh.is_boundary(s)) {
+            angle = 0;
+        } else {
+            auto pos_acc = parent_handle().mesh().template create_const_accessor<double, 3>(
+                parent_handle().template as<ParentType>());
+
+
+            // edge a -> b; input triangle has edge c, opp has d
+            //      /c
+            //     / |
+            //    /t |
+            //   /   |
+            //   a---b
+            //   \   |
+            //    \  |
+            //     \ |
+            //      \d
+            auto a = pos_acc.const_vector_attribute(s.tuple());
+
+            auto b = pos_acc.const_vector_attribute(
+                pmesh.switch_tuples(s.tuple(), {PrimitiveType::Vertex}));
+
+            auto c = pos_acc.const_vector_attribute(
+                pmesh.switch_tuples(s.tuple(), {PrimitiveType::Edge}));
+            auto d = pos_acc.const_vector_attribute(
+                pmesh.switch_tuples(s.tuple(), {PrimitiveType::Triangle, PrimitiveType::Edge}));
+
+            auto ban = (b - a).normalized().eval();
+            auto ca = (c - a).eval();
+            auto da = (d - a).eval();
+            ca.noalias() -= ca.dot(ban) * ban;
+            da.noalias() -= da.dot(ban) * ban;
+
+            return std::atan2(ca.cross(da).norm(), ca.dot(da));
+        }
+    }
 
     DihedralAngleOperator(
         const attribute::MeshAttributeHandle& my_handle,
         const attribute::MeshAttributeHandle& parent_handle)
-        : BaseType(my_handle, parent_handle, &convert)
+        : BaseType(my_handle, parent_handle)
     {
+        assert(parent_handle.mesh().top_simplex_type() == PrimitiveType::Triangle);
         assert(parent_handle.primitive_type() == PrimitiveType::Vertex);
+        assert(parent_handle.dimension() == 3);
+        assert(my_handle.primitive_type() == PrimitiveType::Edge);
     }
+
 private:
     attribute::MeshAttributeHandle m_parent_handle;
 };
