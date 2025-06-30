a8ab65e708a09e3c2ba20da8cf2cd330f6a903bc
Daniel Zint <daniel.zint@nyu.edu>
2025-06-30 03:00:16 -0400

    Update geogram.

--- cmake/recipes/geogram.cmake
+++ cmake/recipes/geogram.cmake
@@ -1,5 +1,5 @@
-# Geogram
-# License: (BSD)
+# Geogram (https://github.com/polyfem/geogram)
+# License: BSD
 
 if(TARGET geogram)
     return()
@@ -21,7 +21,7 @@ endif()
 
 option(GEOGRAM_WITH_GRAPHICS "Viewers and geogram_gfx library" OFF)
 option(GEOGRAM_WITH_LEGACY_NUMERICS "Legacy numerical libraries" OFF)
-option(GEOGRAM_WITH_HLBFGS "Non-linear solver (Yang Liu's HLBFGS)" OFF)
+option(GEOGRAM_WITH_HLBFGS "Non-linear solver (Yang Liu's HLBFGS)" ON)
 option(GEOGRAM_WITH_TETGEN "Tetrahedral mesher (Hang Si's TetGen)" OFF)
 option(GEOGRAM_WITH_TRIANGLE "Triangle mesher (Jonathan Shewchuk's triangle)" OFF)
 option(GEOGRAM_WITH_EXPLORAGRAM "Experimental code (hexahedral meshing vpipeline and optimal transport)" OFF)
@@ -37,18 +37,12 @@ endif()
 
 ################################################################################
 
-include(FetchContent)
-FetchContent_Declare(
-    polyfem_geogram
-	GIT_REPOSITORY https://github.com/polyfem/geogram.git
-	GIT_TAG        516c151c244d9019a9076a1a468d52a0f6dd195d
-    GIT_SHALLOW FALSE
-)
-FetchContent_MakeAvailable(polyfem_geogram)
+include(CPM)
+CPMAddPackage("gh:polyfem/geogram#3191ff581d4087ebddff735a21bf3ca2415f53be")
 
 find_path(GEOGRAM_SOURCE_INCLUDE_DIR
 		geogram/basic/common.h
-		PATHS ${polyfem_geogram_SOURCE_DIR}
+		PATHS ${geogram_SOURCE_DIR}
 		PATH_SUFFIXES src/lib
 		NO_DEFAULT_PATH
 )
