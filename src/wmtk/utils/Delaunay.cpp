6c77e9d8ffa47bc466a7b30418d5d4b8f6fcce09
Daniel Zint <daniel.zint@mail.de>
2025-10-31 17:09:04 -0400

    Make sure all tets from Delaunay have positive orientation.

--- src/wmtk/utils/Delaunay.cpp
+++ src/wmtk/utils/Delaunay.cpp
@@ -6,9 +6,39 @@
 #include <wmtk/utils/EnableWarnings.hpp>
 // clang-format on
 
+#include <igl/predicates/predicates.h>
+
 
 #include <cassert>
 #include <mutex>
+
+namespace {
+bool is_inverted(
+    const wmtk::delaunay::Point3D& p0,
+    const wmtk::delaunay::Point3D& p1,
+    const wmtk::delaunay::Point3D& p2,
+    const wmtk::delaunay::Point3D& p3)
+{
+    const Eigen::Vector3d v0(p0[0], p0[1], p0[2]);
+    const Eigen::Vector3d v1(p1[0], p1[1], p1[2]);
+    const Eigen::Vector3d v2(p2[0], p2[1], p2[2]);
+    const Eigen::Vector3d v3(p3[0], p3[1], p3[2]);
+
+    igl::predicates::exactinit();
+    auto res = igl::predicates::orient3d(v0, v1, v2, v3);
+    int result;
+    if (res == igl::predicates::Orientation::POSITIVE)
+        result = 1;
+    else if (res == igl::predicates::Orientation::NEGATIVE)
+        result = -1;
+    else
+        result = 0;
+
+    if (result < 0) // neg result == pos tet (tet origin from geogram delaunay)
+        return false;
+    return true;
+}
+} // namespace
 
 namespace wmtk::delaunay {
 
@@ -51,6 +81,17 @@ auto delaunay3D(const std::vector<Point3D>& points)
         std::sort(tet.begin(), tet.end());
     }
     std::sort(tets.begin(), tets.end());
+
+    for (auto& tet : tets) {
+        const Point3D p0 = points[tet[0]];
+        const Point3D p1 = points[tet[1]];
+        const Point3D p2 = points[tet[2]];
+        const Point3D p3 = points[tet[3]];
+        if (is_inverted(p0, p1, p2, p3)) {
+            // std::cout << "Inverted tet found" << std::endl;
+            std::swap(tet[2], tet[3]); // invert tet
+        }
+    }
 
     return {vertices, tets};
 }
