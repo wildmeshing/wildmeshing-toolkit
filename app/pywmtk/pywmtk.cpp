1e53a6311a2ae2b012d67b0ec49f812da1a86d0e
Daniel Zint <daniel.zint@mail.de>
2026-02-02 20:56:20 +0100

    Fix position independent code issues.

--- app/pywmtk/pywmtk.cpp
+++ app/pywmtk/pywmtk.cpp
@@ -8,14 +8,6 @@
 #include <nlohmann/json.hpp>
 #include <wmtk/utils/Logger.hpp>
 
-#include <spdlog/async.h>
-#include <spdlog/pattern_formatter.h>
-#include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/sinks/null_sink.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/sinks/stdout_sinks.h>
-#include <spdlog/spdlog.h>
-
 // components
 #include "../components_include.hpp"
 
@@ -31,27 +23,10 @@ void wmtk_wrapper(const py::dict& obj)
 {
     const nlohmann::json j = obj;
 
-    std::cout << "Hello" << std::endl;
-    std::cerr << "Hello ERR" << std::endl;
-
     using namespace wmtk;
     std::map<std::string, std::function<void(nlohmann::json)>> components_map;
     // include auto-generated map
 #include "../components_map.hpp"
-
-    std::cout << "Before set logger" << std::endl;
-    std::cout << "CHECK 3" << std::endl;
-    {
-        auto l = spdlog::stdout_color_mt("wmtk");
-        std::cout << "Before set logger 2" << std::endl;
-        // auto l = spdlog::stdout_logger_mt("wmtk");
-        // auto null_sink = std::make_shared<spdlog::sinks::null_sink_mt>();
-        // auto l = std::make_shared<spdlog::logger>("wmtk-pywmtk", null_sink);
-        //  auto l = spdlog::null_logger_mt("wmtk-pywmtk");
-        // l->set_level(spdlog::level::info);
-        set_logger(l);
-    }
-    std::cout << "After set logger" << std::endl;
 
     // make sure input file contains the application name
     if (!j.contains("application")) {
@@ -64,9 +39,6 @@ void wmtk_wrapper(const py::dict& obj)
     if (components_map.count(app_str) == 0) {
         log_and_throw_error("Applictaion {} unknown", app_str);
     }
-
-    logger().info("Input type: {}", j["input"].type_name());
-    // logger().info("Input: {}", j["input"]);
 
     // execute
     components_map[app_str](j);
@@ -86,20 +58,5 @@ PYBIND11_MODULE(pywmtk, m, py::mod_gil_not_used())
     //     "get_relative_edge_lenghts",
     //     &meme::get_relative_edge_lengths,
     //     "Get edge lengths realtive to bbox diagonal");
-    m.def(
-        "wmtk",
-        [](const py::dict& obj) {
-            // py::scoped_ostream_redirect ostream(
-            //     std::cout,
-            //     py::module_::import("sys").attr("stdout"));
-            // py::scoped_estream_redirect estream(
-            //     std::cerr,
-            //     py::module_::import("sys").attr("stderr"));
-            try {
-                wmtk_wrapper(obj);
-            } catch (const std::exception& e) {
-                std::cout << "Error: " << e.what() << std::endl;
-            }
-        },
-        "Wildmeshing-Toolkit application");
+    m.def("wmtk", &wmtk_wrapper, "Wildmeshing-Toolkit application");
 }
