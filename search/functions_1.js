83de45f86208fdcbeb79fdd69ea8a6eacccc6115
daniel-zint <daniel.zint@mail.de>
2026-05-06 15:08:18 +0000

    Deploying to gh-pages from @ wildmeshing/wildmeshing-toolkit@82010d9f62eaa8b9dda0afb1ffbfe55933e44905 🚀

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['barrierenergy2d_0',['BarrierEnergy2D',['../classwmtk_1_1optimization_1_1_barrier_energy2_d.html#a9c89316f34ed6730f47df708206e9879',1,'wmtk::optimization::BarrierEnergy2D']]],
-  ['barrierenergy3d_1',['BarrierEnergy3D',['../classwmtk_1_1optimization_1_1_barrier_energy3_d.html#a83d84dbfe772646c77d97d8f7ac9f06a',1,'wmtk::optimization::BarrierEnergy3D']]],
+  ['barrierenergy3d_1',['BarrierEnergy3D',['../classwmtk_1_1optimization_1_1_barrier_energy3_d.html#ab3c6e78773ebb5276098fcb00e03b99a',1,'wmtk::optimization::BarrierEnergy3D']]],
   ['begin_5fprotect_2',['begin_protect',['../structwmtk_1_1_attribute_collection.html#ae0505eb2677a9eb03e9a76fe03ccf770',1,'wmtk::AttributeCollection']]],
   ['biharmonicenergy2d_3',['BiharmonicEnergy2D',['../classwmtk_1_1optimization_1_1_biharmonic_energy2_d.html#ab51dd8588beb68812381e95dc6da2d22',1,'wmtk::optimization::BiharmonicEnergy2D']]],
   ['biharmonicenergy3d_4',['BiharmonicEnergy3D',['../classwmtk_1_1optimization_1_1_biharmonic_energy3_d.html#ac0cab2a77a5c37bc37fa126b02e62d76',1,'wmtk::optimization::BiharmonicEnergy3D']]]
