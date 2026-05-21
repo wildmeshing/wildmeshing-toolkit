2c0188d156da11eb8a8518d49cad5c30c4057b33
daniel-zint <daniel.zint@mail.de>
2026-05-21 06:33:31 +0000

    Deploying to gh-pages from @ wildmeshing/wildmeshing-toolkit@f71dee1097a682fe66cfcfab2a3a1fbe322cdaf2 🚀

--- search/classes_1.js
+++ search/classes_1.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['barrierenergy2d_0',['BarrierEnergy2D',['../classwmtk_1_1optimization_1_1_barrier_energy2_d.html',1,'wmtk::optimization']]],
-  ['barrierenergy3d_1',['BarrierEnergy3D',['../classwmtk_1_1optimization_1_1_barrier_energy3_d.html',1,'wmtk::optimization']]],
-  ['biharmonicenergy2d_2',['BiharmonicEnergy2D',['../classwmtk_1_1optimization_1_1_biharmonic_energy2_d.html',1,'wmtk::optimization']]],
-  ['biharmonicenergy3d_3',['BiharmonicEnergy3D',['../classwmtk_1_1optimization_1_1_biharmonic_energy3_d.html',1,'wmtk::optimization']]]
+  ['biharmonicenergy2d_0',['BiharmonicEnergy2D',['../classwmtk_1_1optimization_1_1_biharmonic_energy2_d.html',1,'wmtk::optimization']]],
+  ['biharmonicenergy3d_1',['BiharmonicEnergy3D',['../classwmtk_1_1optimization_1_1_biharmonic_energy3_d.html',1,'wmtk::optimization']]]
 ];
