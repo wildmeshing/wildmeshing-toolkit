8767c42c5a5a5aa4a22b9e2b0f8616d290d8d2e4
Daniel Zint <daniel.zint@mail.de>
2026-05-26 17:56:37 +0200

    Update geogram dependency in fast-envelope. (Trying to fix Windows compilation issue)

--- cmake/recipes/fenvelope.cmake
+++ cmake/recipes/fenvelope.cmake
@@ -24,7 +24,7 @@ message(STATUS "Third-party: creating target 'FastEnvelope::FastEnvelope'")
 
 
 include(CPM)
-CPMAddPackage("gh:daniel-zint/fast-envelope#0046a216171237aab654dd1580aa8a5eaef675ee")
+CPMAddPackage("gh:daniel-zint/fast-envelope#9b5ae87785b1677adf50a90ae53006af1a59dda1")
 
 set_target_properties(FastEnvelope PROPERTIES FOLDER third_party)
 add_library(FastEnvelope::FastEnvelope ALIAS FastEnvelope)
