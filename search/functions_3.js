46bbc7d35cf3d9a8197d65dfe99c534621545b6f
daniel-zint <daniel.zint@mail.de>
2026-07-23 13:50:39 +0000

    Deploying to gh-pages from @ wildmeshing/wildmeshing-toolkit@169e1369ecc850e28a270152cbc05bbbc7acf298 🚀

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['dirichletenergy2d_0',['DirichletEnergy2D',['../classwmtk_1_1optimization_1_1_dirichlet_energy2_d.html#a2b7dc2a306ad9a529f4cb62256755413',1,'wmtk::optimization::DirichletEnergy2D']]]
+  ['data_0',['data',['../classwmtk_1_1threading_1_1collector.html#a641ee7ff44b9fd25dd59f6e7c31a587a',1,'wmtk::threading::collector']]],
+  ['dirichletenergy2d_1',['DirichletEnergy2D',['../classwmtk_1_1optimization_1_1_dirichlet_energy2_d.html#a2b7dc2a306ad9a529f4cb62256755413',1,'wmtk::optimization::DirichletEnergy2D']]]
 ];
