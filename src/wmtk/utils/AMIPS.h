1e822fd9f0af5e6f9939ca078d37ba46ef5e2586
Daniel Zint <daniel.zint@mail.de>
2026-07-28 12:24:10 +0200

    Remove comment.

--- src/wmtk/utils/AMIPS.h
+++ src/wmtk/utils/AMIPS.h
@@ -60,7 +60,7 @@ double AMIPS_energy_stable_p3(const std::array<double, 12>& T)
     auto res = AMIPS_energy(T);
 
     if (res < 1e8 && res > 2) {
-        return res * res * res; // deterministic (std::pow(res, 3) is not guaranteed bit-identical)
+        return res * res * res;
     } else {
         return AMIPS_energy_rational_p3<rational>(T);
     }
