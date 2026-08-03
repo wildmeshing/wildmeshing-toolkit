215326639818eb06877e32b4ad50c578c701ca87
daniel-zint <daniel.zint@mail.de>
2026-08-03 12:45:33 +0000

    Deploying to gh-pages from @ wildmeshing/wildmeshing-toolkit@98c4f9c9c404c47ac87209a0ac6b9e42d9141e3f 🚀

--- search/functions_c.js
+++ search/functions_c.js
@@ -1,14 +1,15 @@
 var searchData=
 [
-  ['parametrize_5fpatch_5fboundaries_0',['parametrize_patch_boundaries',['../class_clough_tocher_patch.html#a05505a01d244070dc1ff742631e499a1',1,'CloughTocherPatch']]],
-  ['parse_1',['parse',['../classwmtk_1_1components_1_1simwild_1_1expression__parser_1_1_parser.html#aaba02e4fb4af504d54d6699913fa17b4',1,'wmtk::components::simwild::expression_parser::Parser']]],
-  ['parseand_2',['parseAnd',['../classwmtk_1_1components_1_1simwild_1_1expression__parser_1_1_parser.html#a733c057427c2360482f9eda07f930338',1,'wmtk::components::simwild::expression_parser::Parser']]],
-  ['parseidentifier_3',['parseIdentifier',['../classwmtk_1_1components_1_1simwild_1_1expression__parser_1_1_parser.html#aa1c18f3a3ebbd2eb75fa9dffd1f08feb',1,'wmtk::components::simwild::expression_parser::Parser']]],
-  ['parseor_4',['parseOr',['../classwmtk_1_1components_1_1simwild_1_1expression__parser_1_1_parser.html#adad686b711f1ff3f8312fda1fdcc2b3a',1,'wmtk::components::simwild::expression_parser::Parser']]],
-  ['parseprimary_5',['parsePrimary',['../classwmtk_1_1components_1_1simwild_1_1expression__parser_1_1_parser.html#a722aa2c61115c15e8ea473ee11a383e4',1,'wmtk::components::simwild::expression_parser::Parser']]],
-  ['parser_6',['Parser',['../classwmtk_1_1components_1_1simwild_1_1expression__parser_1_1_parser.html#a27f77ed4e0ebde1bda8c653688b6939e',1,'wmtk::components::simwild::expression_parser::Parser']]],
-  ['parseunary_7',['parseUnary',['../classwmtk_1_1components_1_1simwild_1_1expression__parser_1_1_parser.html#a9c1626b174ac406bdee40f06f35f872c',1,'wmtk::components::simwild::expression_parser::Parser']]],
-  ['prepare_5fsurface_5fflip_5f32_8',['prepare_surface_flip_32',['../classwmtk_1_1components_1_1simwild_1_1_sim_wild_mesh.html#ada4b8c34dfe58e3b3f47c7c8343247e9',1,'wmtk::components::simwild::SimWildMesh::prepare_surface_flip_32()'],['../classwmtk_1_1components_1_1tetwild_1_1_tet_wild_mesh.html#aebbcb64194c670f1f08256ee1f9a4282',1,'wmtk::components::tetwild::TetWildMesh::prepare_surface_flip_32()']]],
-  ['print_5finfo_9',['print_info',['../classwmtk_1_1_tet_mesh_1_1_tuple.html#a49fd88de6c7178861506864665c4eaa3',1,'wmtk::TetMesh::Tuple::print_info() const'],['../classwmtk_1_1_tet_mesh_1_1_tuple.html#a6b102f37bc5870ed46df2630f412394b',1,'wmtk::TetMesh::Tuple::print_info(const TetMesh &amp;m) const']]],
-  ['push_5fback_10',['push_back',['../classwmtk_1_1threading_1_1collector.html#ad97029619feeb1a4acee0b954a17f4d8',1,'wmtk::threading::collector']]]
+  ['parameters_0',['Parameters',['../structwmtk_1_1components_1_1triwild_1_1_parameters.html#a28f57ac286ea9a1dab06783b4b194933',1,'wmtk::components::triwild::Parameters']]],
+  ['parametrize_5fpatch_5fboundaries_1',['parametrize_patch_boundaries',['../class_clough_tocher_patch.html#a05505a01d244070dc1ff742631e499a1',1,'CloughTocherPatch']]],
+  ['parse_2',['parse',['../classwmtk_1_1components_1_1simwild_1_1expression__parser_1_1_parser.html#aaba02e4fb4af504d54d6699913fa17b4',1,'wmtk::components::simwild::expression_parser::Parser']]],
+  ['parseand_3',['parseAnd',['../classwmtk_1_1components_1_1simwild_1_1expression__parser_1_1_parser.html#a733c057427c2360482f9eda07f930338',1,'wmtk::components::simwild::expression_parser::Parser']]],
+  ['parseidentifier_4',['parseIdentifier',['../classwmtk_1_1components_1_1simwild_1_1expression__parser_1_1_parser.html#aa1c18f3a3ebbd2eb75fa9dffd1f08feb',1,'wmtk::components::simwild::expression_parser::Parser']]],
+  ['parseor_5',['parseOr',['../classwmtk_1_1components_1_1simwild_1_1expression__parser_1_1_parser.html#adad686b711f1ff3f8312fda1fdcc2b3a',1,'wmtk::components::simwild::expression_parser::Parser']]],
+  ['parseprimary_6',['parsePrimary',['../classwmtk_1_1components_1_1simwild_1_1expression__parser_1_1_parser.html#a722aa2c61115c15e8ea473ee11a383e4',1,'wmtk::components::simwild::expression_parser::Parser']]],
+  ['parser_7',['Parser',['../classwmtk_1_1components_1_1simwild_1_1expression__parser_1_1_parser.html#a27f77ed4e0ebde1bda8c653688b6939e',1,'wmtk::components::simwild::expression_parser::Parser']]],
+  ['parseunary_8',['parseUnary',['../classwmtk_1_1components_1_1simwild_1_1expression__parser_1_1_parser.html#a9c1626b174ac406bdee40f06f35f872c',1,'wmtk::components::simwild::expression_parser::Parser']]],
+  ['prepare_5fsurface_5fflip_5f32_9',['prepare_surface_flip_32',['../classwmtk_1_1components_1_1simwild_1_1_sim_wild_mesh.html#ada4b8c34dfe58e3b3f47c7c8343247e9',1,'wmtk::components::simwild::SimWildMesh::prepare_surface_flip_32()'],['../classwmtk_1_1components_1_1tetwild_1_1_tet_wild_mesh.html#aebbcb64194c670f1f08256ee1f9a4282',1,'wmtk::components::tetwild::TetWildMesh::prepare_surface_flip_32()']]],
+  ['print_5finfo_10',['print_info',['../classwmtk_1_1_tet_mesh_1_1_tuple.html#a49fd88de6c7178861506864665c4eaa3',1,'wmtk::TetMesh::Tuple::print_info() const'],['../classwmtk_1_1_tet_mesh_1_1_tuple.html#a6b102f37bc5870ed46df2630f412394b',1,'wmtk::TetMesh::Tuple::print_info(const TetMesh &amp;m) const']]],
+  ['push_5fback_11',['push_back',['../classwmtk_1_1threading_1_1collector.html#ad97029619feeb1a4acee0b954a17f4d8',1,'wmtk::threading::collector']]]
 ];
--- search/functions_d.js
+++ search/functions_d.js
@@ -1,15 +1,16 @@
 var searchData=
 [
-  ['radius_0',['radius',['../classwmtk_1_1components_1_1topological__offset_1_1_circle.html#ac2b183a3d597b7b3b04b91561ecd5356',1,'wmtk::components::topological_offset::Circle::radius()'],['../classwmtk_1_1components_1_1topological__offset_1_1_sphere.html#ad3f9995766aaf03297fa77e017040e27',1,'wmtk::components::topological_offset::Sphere::radius()']]],
+  ['radius_0',['radius',['../classwmtk_1_1components_1_1topological__offset_1_1_sphere.html#ad3f9995766aaf03297fa77e017040e27',1,'wmtk::components::topological_offset::Sphere::radius()'],['../classwmtk_1_1components_1_1topological__offset_1_1_circle.html#ac2b183a3d597b7b3b04b91561ecd5356',1,'wmtk::components::topological_offset::Circle::radius()']]],
   ['refine_1',['refine',['../classwmtk_1_1components_1_1tetwild_1_1orig_1_1_mesh_refinement.html#aa397d8680a4adf352a3b4291419a3e0e',1,'wmtk::components::tetwild::orig::MeshRefinement::refine()'],['../classwmtk_1_1components_1_1topological__offset_1_1_circle.html#a65ade4734c0b2bb6bf34b4ddaa82d91d',1,'wmtk::components::topological_offset::Circle::refine()'],['../classwmtk_1_1components_1_1topological__offset_1_1_sphere.html#a890c4e19a85657073e33a9584586b752',1,'wmtk::components::topological_offset::Sphere::refine()']]],
-  ['refine_5fsizing_5faround_5fworst_2',['refine_sizing_around_worst',['../classwmtk_1_1components_1_1simwild_1_1_sim_wild_mesh.html#a1436bc586077e4103e0a21061801831a',1,'wmtk::components::simwild::SimWildMesh::refine_sizing_around_worst()'],['../classwmtk_1_1components_1_1tetwild_1_1_tet_wild_mesh.html#aedf7d89c22f50f1116d09fd846ad097a',1,'wmtk::components::tetwild::TetWildMesh::refine_sizing_around_worst()']]],
+  ['refine_5fsizing_5faround_5fworst_2',['refine_sizing_around_worst',['../classwmtk_1_1components_1_1simwild_1_1_sim_wild_mesh.html#a1436bc586077e4103e0a21061801831a',1,'wmtk::components::simwild::SimWildMesh::refine_sizing_around_worst()'],['../classwmtk_1_1components_1_1tetwild_1_1_tet_wild_mesh.html#aedf7d89c22f50f1116d09fd846ad097a',1,'wmtk::components::tetwild::TetWildMesh::refine_sizing_around_worst()'],['../classwmtk_1_1components_1_1triwild_1_1_tri_wild_mesh.html#a13a33222d56844c0f9eae86b3daef5ba',1,'wmtk::components::triwild::TriWildMesh::refine_sizing_around_worst()']]],
   ['release_5fprotect_5fattributes_3',['release_protect_attributes',['../classwmtk_1_1_tri_mesh.html#a225083671f68132708976d88be0e2de4',1,'wmtk::TriMesh']]],
   ['remove_5fduplicates_4',['remove_duplicates',['../classwmtk_1_1components_1_1simwild_1_1_embed_surface.html#aa74c124f5249f70b274105df856a699a',1,'wmtk::components::simwild::EmbedSurface']]],
   ['remove_5ftets_5fby_5fids_5',['remove_tets_by_ids',['../classwmtk_1_1_tet_mesh.html#afcb8ea0a43e2a2063af41b6a2ba8c91f',1,'wmtk::TetMesh']]],
-  ['reserve_6',['reserve',['../classwmtk_1_1threading_1_1collector.html#ad62cad66dc5f9b2d33a90339029bfa24',1,'wmtk::threading::collector']]],
-  ['reset_5fconnected_5fcomponents_7',['reset_connected_components',['../classwmtk_1_1components_1_1topological__offset_1_1_topo_offset_tet_mesh.html#a3830922378a6f040f5f242af381c57a8',1,'wmtk::components::topological_offset::TopoOffsetTetMesh']]],
-  ['resize_8',['resize',['../classwmtk_1_1threading_1_1collector.html#ae42cf15f595eef9a39c83ff189759995',1,'wmtk::threading::collector']]],
-  ['rollback_9',['rollback',['../structwmtk_1_1_attribute_collection.html#a15d5ca94f6995d3e1363804f79d7c287',1,'wmtk::AttributeCollection']]],
-  ['rollback_5fprotected_5fattributes_10',['rollback_protected_attributes',['../classwmtk_1_1_tri_mesh.html#a6419d01660badf33195e0c3ad52d07f1',1,'wmtk::TriMesh']]],
-  ['round_11',['round',['../classwmtk_1_1components_1_1simwild_1_1_sim_wild_mesh.html#ad7558616aa2479ba9c410d55911e8bdb',1,'wmtk::components::simwild::SimWildMesh']]]
+  ['remove_5ftris_5fby_5fids_6',['remove_tris_by_ids',['../classwmtk_1_1_tri_mesh.html#a90be1ec37c03c2ed25ee11d65784e627',1,'wmtk::TriMesh']]],
+  ['reserve_7',['reserve',['../classwmtk_1_1threading_1_1collector.html#ad62cad66dc5f9b2d33a90339029bfa24',1,'wmtk::threading::collector']]],
+  ['reset_5fconnected_5fcomponents_8',['reset_connected_components',['../classwmtk_1_1components_1_1topological__offset_1_1_topo_offset_tet_mesh.html#a3830922378a6f040f5f242af381c57a8',1,'wmtk::components::topological_offset::TopoOffsetTetMesh']]],
+  ['resize_9',['resize',['../classwmtk_1_1threading_1_1collector.html#ae42cf15f595eef9a39c83ff189759995',1,'wmtk::threading::collector']]],
+  ['rollback_10',['rollback',['../structwmtk_1_1_attribute_collection.html#a15d5ca94f6995d3e1363804f79d7c287',1,'wmtk::AttributeCollection']]],
+  ['rollback_5fprotected_5fattributes_11',['rollback_protected_attributes',['../classwmtk_1_1_tri_mesh.html#a6419d01660badf33195e0c3ad52d07f1',1,'wmtk::TriMesh']]],
+  ['round_12',['round',['../classwmtk_1_1components_1_1simwild_1_1_sim_wild_mesh.html#ad7558616aa2479ba9c410d55911e8bdb',1,'wmtk::components::simwild::SimWildMesh']]]
 ];
