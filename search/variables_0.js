05267767aa9bfd821fb08e5be46b2652b764a259
danielepanozzo <daniele.panozzo@gmail.com>
2026-08-10 17:27:55 +0000

    Deploying to gh-pages from @ wildmeshing/wildmeshing-toolkit@a18692b92c43ff93df535153a121e8b432118350 🚀

--- search/variables_0.js
+++ search/variables_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['already_5finverted_0',['already_inverted',['../structwmtk_1_1optimization_1_1_smooth_reject_counters.html#a7ee86b9e24d37dc5af641555cbbf6a44',1,'wmtk::optimization::SmoothRejectCounters']]]
+  ['allow_5fjunction_5fcleanup_0',['allow_junction_cleanup',['../structwmtk_1_1components_1_1triwild_1_1_parameters.html#a37a8f0cf2a46fca6c01bbf93cad53b5a',1,'wmtk::components::triwild::Parameters']]],
+  ['already_5finverted_1',['already_inverted',['../structwmtk_1_1optimization_1_1_smooth_reject_counters.html#a7ee86b9e24d37dc5af641555cbbf6a44',1,'wmtk::optimization::SmoothRejectCounters']]]
 ];
