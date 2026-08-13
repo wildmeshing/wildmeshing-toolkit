d2ddfabbe830cddce3ffacb85ad85879cff71097
udaykusupati <kusupatiuday@gmail.com>
2026-08-13 20:23:47 +0000

    Deploying to gh-pages from @ wildmeshing/wildmeshing-toolkit@8fc6f37d8e1536e5425f7892bf01989402619a4e 🚀

--- search/variables_f.js
+++ search/variables_f.js
@@ -4,6 +4,5 @@ var searchData=
   ['smoothing_5fmode_1',['smoothing_mode',['../structwmtk_1_1_optimizer_parameters.html#a1a0481012b6c764801ae2d120076c6d3',1,'wmtk::OptimizerParameters']]],
   ['split_5fhigh_5fvalence_5fthreshold_2',['split_high_valence_threshold',['../structwmtk_1_1_optimizer_parameters.html#a99fbd3031d9726ffa1af82b6977acfae',1,'wmtk::OptimizerParameters']]],
   ['stopping_5fcriterion_3',['stopping_criterion',['../structwmtk_1_1_execute_pass.html#af20086623771fe01053a53b98a086b9b',1,'wmtk::ExecutePass']]],
-  ['stopping_5fcriterion_5fchecking_5ffrequency_4',['stopping_criterion_checking_frequency',['../structwmtk_1_1_execute_pass.html#a821948f937feea93d9de593b525cde58',1,'wmtk::ExecutePass']]],
-  ['stuck_5frefine_5fforce_5fsplit_5foversized_5fonly_5',['stuck_refine_force_split_oversized_only',['../structwmtk_1_1_optimizer_parameters.html#a1e6c67ca56cc3353330788e62b0b88cb',1,'wmtk::OptimizerParameters']]]
+  ['stopping_5fcriterion_5fchecking_5ffrequency_4',['stopping_criterion_checking_frequency',['../structwmtk_1_1_execute_pass.html#a821948f937feea93d9de593b525cde58',1,'wmtk::ExecutePass']]]
 ];
