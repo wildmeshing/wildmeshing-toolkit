abaa03a23b16db3535a1339aab54636721c6f789
udaykusupati <kusupatiuday@gmail.com>
2026-08-14 19:36:07 +0000

    Deploying to gh-pages from @ wildmeshing/wildmeshing-toolkit@6646f94eb0ccf1e9b234e02931cdd3384247d7b0 🚀

--- search/classes_e.js
+++ search/classes_e.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['range_0',['range',['../classwmtk_1_1threading_1_1range.html',1,'wmtk::threading']]],
   ['rational_1',['Rational',['../classwmtk_1_1_rational.html',1,'wmtk']]],
-  ['rationalpositions_2',['RationalPositions',['../classwmtk_1_1_rational_positions.html',1,'wmtk']]]
+  ['rationalpositions_2',['RationalPositions',['../classwmtk_1_1_rational_positions.html',1,'wmtk']]],
+  ['ringlockscratch_3',['ringlockscratch',['../structwmtk_1_1_tet_mesh_1_1_ring_lock_scratch.html',1,'wmtk::TetMesh::RingLockScratch'],['../structwmtk_1_1_tri_mesh_1_1_ring_lock_scratch.html',1,'wmtk::TriMesh::RingLockScratch']]]
 ];
--- search/variables_e.js
+++ search/variables_e.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['region_5fattempted_0',['region_attempted',['../structwmtk_1_1components_1_1topological__offset_1_1_topo_offset_tri_mesh_1_1_smooth_trace.html#a536baff935d6ad20587f743b2a92a5df',1,'wmtk::components::topological_offset::TopoOffsetTriMesh::SmoothTrace']]],
-  ['renew_5fneighbor_5ftuples_1',['renew_neighbor_tuples',['../structwmtk_1_1_execute_pass.html#a76cbf1fcebd336fd5d7afb36f2f1091c',1,'wmtk::ExecutePass']]]
+  ['region_5fmax_5frel_5fbefore_1',['region_max_rel_before',['../structwmtk_1_1_tet_optimizer_mesh_1_1_collapse_info_cache.html#a95de59c32a3f9c04a0e803b279b570db',1,'wmtk::TetOptimizerMesh::CollapseInfoCache::region_max_rel_before'],['../structwmtk_1_1_tri_optimizer_mesh_1_1_collapse_info_cache.html#adf866a2a50dc69423cbe15f6af2648c0',1,'wmtk::TriOptimizerMesh::CollapseInfoCache::region_max_rel_before']]],
+  ['renew_5fneighbor_5ftuples_2',['renew_neighbor_tuples',['../structwmtk_1_1_execute_pass.html#a76cbf1fcebd336fd5d7afb36f2f1091c',1,'wmtk::ExecutePass']]]
 ];
