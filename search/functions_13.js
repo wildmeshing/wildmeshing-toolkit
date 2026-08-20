5d5a7d4f11102832f681efaa5affe6b94aec3700
danielepanozzo <daniele.panozzo@gmail.com>
2026-08-20 14:27:42 +0000

    Deploying to gh-pages from @ wildmeshing/wildmeshing-toolkit@23a4165bfbb689147f2555d1a510220a88b0cc14 🚀

--- search/functions_13.js
+++ search/functions_13.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['update_5fattributes_0',['update_attributes',['../classwmtk_1_1_tet_optimizer_mesh.html#a37d8e2f8dc1fa0e283ed3a0378ca660f',1,'wmtk::TetOptimizerMesh::update_attributes()'],['../classwmtk_1_1components_1_1simwild_1_1_sim_wild_mesh.html#ad6ffd03cc2c06bb56c85c29c8f089deb',1,'wmtk::components::simwild::SimWildMesh::update_attributes()']]],
+  ['update_5fattributes_0',['update_attributes',['../classwmtk_1_1_tet_optimizer_mesh.html#a37d8e2f8dc1fa0e283ed3a0378ca660f',1,'wmtk::TetOptimizerMesh::update_attributes()'],['../classwmtk_1_1_tri_optimizer_mesh.html#af708f3e42232d74ffde9abf5b04b85a4',1,'wmtk::TriOptimizerMesh::update_attributes()'],['../classwmtk_1_1components_1_1simwild_1_1_sim_wild_mesh.html#ad6ffd03cc2c06bb56c85c29c8f089deb',1,'wmtk::components::simwild::SimWildMesh::update_attributes()'],['../classwmtk_1_1components_1_1simwild_1_1tri_1_1_sim_wild_mesh_tri.html#a0c2d3701be8243ce4c6ce16c6a723adc',1,'wmtk::components::simwild::tri::SimWildMeshTri::update_attributes()']]],
   ['update_5fsizing_5ffield_1',['update_sizing_field',['../classwmtk_1_1components_1_1topological__offset_1_1_topo_offset_tet_mesh.html#afe3479ab696205d02210128badcd8b76',1,'wmtk::components::topological_offset::TopoOffsetTetMesh']]]
 ];
--- search/variables_2.js
+++ search/variables_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['check_5fcollinear_5finput_0',['check_collinear_input',['../structwmtk_1_1utils_1_1_embed_triangles_options.html#a9b4807ee3e2226337f5eecde6e26e7c0',1,'wmtk::utils::EmbedTrianglesOptions']]],
-  ['check_5fenvelope_5fat_5finit_1',['check_envelope_at_init',['../structwmtk_1_1components_1_1simwild_1_1_parameters.html#aaccb1bb1446e2c4db5b6459717e1650c',1,'wmtk::components::simwild::Parameters::check_envelope_at_init'],['../structwmtk_1_1components_1_1triwild_1_1_parameters.html#ab78031b3b87e66abd0b6c2653c1c8758',1,'wmtk::components::triwild::Parameters::check_envelope_at_init']]],
+  ['check_5fenvelope_5fat_5finit_1',['check_envelope_at_init',['../structwmtk_1_1components_1_1triwild_1_1_parameters.html#ab78031b3b87e66abd0b6c2653c1c8758',1,'wmtk::components::triwild::Parameters']]],
   ['check_5forientation_2',['check_orientation',['../structwmtk_1_1utils_1_1_embed_triangles_options.html#acdb6a348418856ac8c4552ee9f042db9',1,'wmtk::utils::EmbedTrianglesOptions']]],
   ['check_5fsurface_5fprovenance_3',['check_surface_provenance',['../structwmtk_1_1utils_1_1_embed_triangles_options.html#a65bc96b51fcd1f36851767be1fd3ff98',1,'wmtk::utils::EmbedTrianglesOptions']]],
   ['coarsen_5fglobal_5fsmoothing_5fpasses_4',['coarsen_global_smoothing_passes',['../structwmtk_1_1_optimizer_parameters.html#a69fa78c2ef764cf8a35a91bc0cd169b1',1,'wmtk::OptimizerParameters']]],
