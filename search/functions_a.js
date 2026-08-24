584550a40c3ccbf35dc0b2f26ba9e0da966c8a3e
daniel-zint <daniel.zint@mail.de>
2026-08-24 08:39:32 +0000

    Deploying to gh-pages from @ wildmeshing/wildmeshing-toolkit@8c063bde6914b8c65f90fd7e5c097facdd252194 🚀

--- search/functions_a.js
+++ search/functions_a.js
@@ -4,11 +4,12 @@ var searchData=
   ['label_5fnon_5fmanifold_1',['label_non_manifold',['../classwmtk_1_1components_1_1manifold__extraction_1_1_man_extract_tet_mesh.html#a6263061108092e96dd4a7a94d26375a3',1,'wmtk::components::manifold_extraction::ManExtractTetMesh::label_non_manifold()'],['../classwmtk_1_1components_1_1manifold__extraction_1_1_man_extract_tri_mesh.html#a217b40b140c28cef4fb8c42c4b97240b',1,'wmtk::components::manifold_extraction::ManExtractTriMesh::label_non_manifold()']]],
   ['label_5foffset_5fboundary_2',['label_offset_boundary',['../classwmtk_1_1components_1_1topological__offset_1_1_topo_offset_tri_mesh.html#ab7a739e54ba112508dcdd6a00fee1ef8',1,'wmtk::components::topological_offset::TopoOffsetTriMesh']]],
   ['link_5fcondition_3',['link_condition',['../classwmtk_1_1_tet_mesh.html#a5dde52f8c3642ed037019ff31daf6330',1,'wmtk::TetMesh']]],
-  ['load_5fmesh_4',['load_mesh',['../namespacesimwild_1_1polyfem__ops_1_1constraints.html#aaa04c45c11e33c6a7e4b19c8bc84cdbe',1,'simwild::polyfem_ops::constraints']]],
-  ['load_5fspec_5',['load_spec',['../namespacesimwild_1_1polyfem__ops_1_1spec.html#a504b7be205f90a4bc6b999dbc0fe2e65',1,'simwild::polyfem_ops::spec']]],
-  ['local_5feid_6',['local_eid',['../classwmtk_1_1_tri_mesh_1_1_tuple.html#af74485b73b9169fc3978dc11264b2b1b',1,'wmtk::TriMesh::Tuple']]],
-  ['lock_5fvertex_5fball_7',['lock_vertex_ball',['../classwmtk_1_1_tet_mesh.html#a6fddcac3db6744e7a8da0b0f8c041d5c',1,'wmtk::TetMesh::lock_vertex_ball()'],['../classwmtk_1_1_tri_mesh.html#a276b16a28db1c00a4cd8f33f7a5c9abc',1,'wmtk::TriMesh::lock_vertex_ball()']]],
-  ['log_5fcontention_8',['log_contention',['../structwmtk_1_1_execute_pass.html#a406de64b79dfcc15aa5a1ea627309e18',1,'wmtk::ExecutePass']]],
-  ['log_5ftotal_5fsurface_5fenergy_9',['log_total_surface_energy',['../classwmtk_1_1components_1_1simwild_1_1tri_1_1_sim_wild_mesh_tri.html#a42414110473cbf3b104e02c3105ecee5',1,'wmtk::components::simwild::tri::SimWildMeshTri']]],
-  ['lowest_5fcommon_5ftet_10',['lowest_common_tet',['../classwmtk_1_1_tet_mesh.html#a110594e16e75a527eedde84b1d4d3133',1,'wmtk::TetMesh']]]
+  ['live_4',['live',['../classwmtk_1_1_slot_pool.html#ad31b543781963ff6b8adc725e1f206cd',1,'wmtk::SlotPool']]],
+  ['load_5fmesh_5',['load_mesh',['../namespacesimwild_1_1polyfem__ops_1_1constraints.html#aaa04c45c11e33c6a7e4b19c8bc84cdbe',1,'simwild::polyfem_ops::constraints']]],
+  ['load_5fspec_6',['load_spec',['../namespacesimwild_1_1polyfem__ops_1_1spec.html#a504b7be205f90a4bc6b999dbc0fe2e65',1,'simwild::polyfem_ops::spec']]],
+  ['local_5feid_7',['local_eid',['../classwmtk_1_1_tri_mesh_1_1_tuple.html#af74485b73b9169fc3978dc11264b2b1b',1,'wmtk::TriMesh::Tuple']]],
+  ['lock_5fvertex_5fball_8',['lock_vertex_ball',['../classwmtk_1_1_tet_mesh.html#a6fddcac3db6744e7a8da0b0f8c041d5c',1,'wmtk::TetMesh::lock_vertex_ball()'],['../classwmtk_1_1_tri_mesh.html#a276b16a28db1c00a4cd8f33f7a5c9abc',1,'wmtk::TriMesh::lock_vertex_ball()']]],
+  ['log_5fcontention_9',['log_contention',['../structwmtk_1_1_execute_pass.html#a406de64b79dfcc15aa5a1ea627309e18',1,'wmtk::ExecutePass']]],
+  ['log_5ftotal_5fsurface_5fenergy_10',['log_total_surface_energy',['../classwmtk_1_1components_1_1simwild_1_1tri_1_1_sim_wild_mesh_tri.html#a42414110473cbf3b104e02c3105ecee5',1,'wmtk::components::simwild::tri::SimWildMeshTri']]],
+  ['lowest_5fcommon_5ftet_11',['lowest_common_tet',['../classwmtk_1_1_tet_mesh.html#a110594e16e75a527eedde84b1d4d3133',1,'wmtk::TetMesh']]]
 ];
--- search/variables_7.js
+++ search/variables_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['hash_0',['hash',['../classwmtk_1_1_tri_mesh_1_1_triangle_connectivity.html#ae80c31e0e64bcec276d7688027dc83bb',1,'wmtk::TriMesh::TriangleConnectivity']]]
+  ['hash_0',['hash',['../classwmtk_1_1_tet_mesh_1_1_tetrahedron_connectivity.html#a1c2c9175fd8dba2046fff31f735bfa37',1,'wmtk::TetMesh::TetrahedronConnectivity::hash'],['../classwmtk_1_1_tri_mesh_1_1_triangle_connectivity.html#ae80c31e0e64bcec276d7688027dc83bb',1,'wmtk::TriMesh::TriangleConnectivity::hash']]]
 ];
