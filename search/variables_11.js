885587d4995be09b80850c0d57b558e28e7bddce
Spenbert02 <Spenbert02@gmail.com>
2026-09-10 01:21:23 +0000

    Deploying to gh-pages from @ wildmeshing/wildmeshing-toolkit@80656b5a1701ffde0ffeda7f55213fbbf5f2c465 🚀

--- search/variables_11.js
+++ search/variables_11.js
@@ -6,7 +6,9 @@ var searchData=
   ['sizing_5fgradation_5fmode_3',['sizing_gradation_mode',['../structwmtk_1_1components_1_1topological__offset_1_1_parameters.html#ad0a001d723b1c20967459145c227f31e',1,'wmtk::components::topological_offset::Parameters']]],
   ['smooth_5fquality_5fveto_4',['smooth_quality_veto',['../structwmtk_1_1_optimizer_parameters.html#aea3b29f9fdf05a585f3ec305d55a7d5c',1,'wmtk::OptimizerParameters']]],
   ['smoothing_5fmode_5',['smoothing_mode',['../structwmtk_1_1_optimizer_parameters.html#a1a0481012b6c764801ae2d120076c6d3',1,'wmtk::OptimizerParameters']]],
-  ['split_5fhigh_5fvalence_5fthreshold_6',['split_high_valence_threshold',['../structwmtk_1_1_optimizer_parameters.html#a99fbd3031d9726ffa1af82b6977acfae',1,'wmtk::OptimizerParameters']]],
-  ['stopping_5fcriterion_7',['stopping_criterion',['../structwmtk_1_1_execute_pass.html#af20086623771fe01053a53b98a086b9b',1,'wmtk::ExecutePass']]],
-  ['stopping_5fcriterion_5fchecking_5ffrequency_8',['stopping_criterion_checking_frequency',['../structwmtk_1_1_execute_pass.html#a821948f937feea93d9de593b525cde58',1,'wmtk::ExecutePass']]]
+  ['sphere_5ftrace_5finitialization_6',['sphere_trace_initialization',['../structwmtk_1_1components_1_1topological__offset_1_1_parameters.html#a1f60de909647416df3cd79bd92df9352',1,'wmtk::components::topological_offset::Parameters']]],
+  ['sphere_5ftrace_5ftarget_5frel_5ftol_7',['sphere_trace_target_rel_tol',['../structwmtk_1_1components_1_1topological__offset_1_1_parameters.html#aec4223bf61e4f0dc7a7218f92811ab6b',1,'wmtk::components::topological_offset::Parameters']]],
+  ['split_5fhigh_5fvalence_5fthreshold_8',['split_high_valence_threshold',['../structwmtk_1_1_optimizer_parameters.html#a99fbd3031d9726ffa1af82b6977acfae',1,'wmtk::OptimizerParameters']]],
+  ['stopping_5fcriterion_9',['stopping_criterion',['../structwmtk_1_1_execute_pass.html#af20086623771fe01053a53b98a086b9b',1,'wmtk::ExecutePass']]],
+  ['stopping_5fcriterion_5fchecking_5ffrequency_10',['stopping_criterion_checking_frequency',['../structwmtk_1_1_execute_pass.html#a821948f937feea93d9de593b525cde58',1,'wmtk::ExecutePass']]]
 ];
