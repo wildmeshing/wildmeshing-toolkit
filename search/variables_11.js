292e99e169686e0c68a5b5468d069c065ce83618
Spenbert02 <Spenbert02@gmail.com>
2026-09-11 16:38:40 +0000

    Deploying to gh-pages from @ wildmeshing/wildmeshing-toolkit@1ca148700a93384cde7f1e3940007012f7f926dc 🚀

--- search/variables_11.js
+++ search/variables_11.js
@@ -1,14 +1,15 @@
 var searchData=
 [
-  ['should_5frenew_0',['should_renew',['../structwmtk_1_1_execute_pass.html#addd10a3e42d324239dc3c970580d373f',1,'wmtk::ExecutePass']]],
-  ['sigma_1',['sigma',['../structwmtk_1_1components_1_1topological__offset_1_1_align_energy2_d_1_1_edge.html#acf6ed3065fd1c1031ee28b34a34dc1e7',1,'wmtk::components::topological_offset::AlignEnergy2D::Edge::sigma'],['../structwmtk_1_1components_1_1topological__offset_1_1_align_energy3_d_1_1_face.html#a2360a12fc6c249ee139eb0fc1c6d9797',1,'wmtk::components::topological_offset::AlignEnergy3D::Face::sigma']]],
-  ['sizing_5fcollapse_5fmin_2',['sizing_collapse_min',['../structwmtk_1_1components_1_1topological__offset_1_1_parameters.html#a75cefadd552bf38dd047b45d1fdcf7be',1,'wmtk::components::topological_offset::Parameters']]],
-  ['sizing_5fgradation_5fmode_3',['sizing_gradation_mode',['../structwmtk_1_1components_1_1topological__offset_1_1_parameters.html#ad0a001d723b1c20967459145c227f31e',1,'wmtk::components::topological_offset::Parameters']]],
-  ['smooth_5fquality_5fveto_4',['smooth_quality_veto',['../structwmtk_1_1_optimizer_parameters.html#aea3b29f9fdf05a585f3ec305d55a7d5c',1,'wmtk::OptimizerParameters']]],
-  ['smoothing_5fmode_5',['smoothing_mode',['../structwmtk_1_1_optimizer_parameters.html#a1a0481012b6c764801ae2d120076c6d3',1,'wmtk::OptimizerParameters']]],
-  ['sphere_5ftrace_5finitialization_6',['sphere_trace_initialization',['../structwmtk_1_1components_1_1topological__offset_1_1_parameters.html#a1f60de909647416df3cd79bd92df9352',1,'wmtk::components::topological_offset::Parameters']]],
-  ['sphere_5ftrace_5ftarget_5frel_5ftol_7',['sphere_trace_target_rel_tol',['../structwmtk_1_1components_1_1topological__offset_1_1_parameters.html#aec4223bf61e4f0dc7a7218f92811ab6b',1,'wmtk::components::topological_offset::Parameters']]],
-  ['split_5fhigh_5fvalence_5fthreshold_8',['split_high_valence_threshold',['../structwmtk_1_1_optimizer_parameters.html#a99fbd3031d9726ffa1af82b6977acfae',1,'wmtk::OptimizerParameters']]],
-  ['stopping_5fcriterion_9',['stopping_criterion',['../structwmtk_1_1_execute_pass.html#af20086623771fe01053a53b98a086b9b',1,'wmtk::ExecutePass']]],
-  ['stopping_5fcriterion_5fchecking_5ffrequency_10',['stopping_criterion_checking_frequency',['../structwmtk_1_1_execute_pass.html#a821948f937feea93d9de593b525cde58',1,'wmtk::ExecutePass']]]
+  ['sag_5fhalve_5frefinement_0',['sag_halve_refinement',['../structwmtk_1_1components_1_1topological__offset_1_1_parameters.html#aeb0e014e54d31b9b0e1ee0fcfac44135',1,'wmtk::components::topological_offset::Parameters']]],
+  ['should_5frenew_1',['should_renew',['../structwmtk_1_1_execute_pass.html#addd10a3e42d324239dc3c970580d373f',1,'wmtk::ExecutePass']]],
+  ['sigma_2',['sigma',['../structwmtk_1_1components_1_1topological__offset_1_1_align_energy2_d_1_1_edge.html#acf6ed3065fd1c1031ee28b34a34dc1e7',1,'wmtk::components::topological_offset::AlignEnergy2D::Edge::sigma'],['../structwmtk_1_1components_1_1topological__offset_1_1_align_energy3_d_1_1_face.html#a2360a12fc6c249ee139eb0fc1c6d9797',1,'wmtk::components::topological_offset::AlignEnergy3D::Face::sigma']]],
+  ['sizing_5fcollapse_5fmin_3',['sizing_collapse_min',['../structwmtk_1_1components_1_1topological__offset_1_1_parameters.html#a75cefadd552bf38dd047b45d1fdcf7be',1,'wmtk::components::topological_offset::Parameters']]],
+  ['sizing_5fgradation_5fmode_4',['sizing_gradation_mode',['../structwmtk_1_1components_1_1topological__offset_1_1_parameters.html#ad0a001d723b1c20967459145c227f31e',1,'wmtk::components::topological_offset::Parameters']]],
+  ['smooth_5fquality_5fveto_5',['smooth_quality_veto',['../structwmtk_1_1_optimizer_parameters.html#aea3b29f9fdf05a585f3ec305d55a7d5c',1,'wmtk::OptimizerParameters']]],
+  ['smoothing_5fmode_6',['smoothing_mode',['../structwmtk_1_1_optimizer_parameters.html#a1a0481012b6c764801ae2d120076c6d3',1,'wmtk::OptimizerParameters']]],
+  ['sphere_5ftrace_5finitialization_7',['sphere_trace_initialization',['../structwmtk_1_1components_1_1topological__offset_1_1_parameters.html#a1f60de909647416df3cd79bd92df9352',1,'wmtk::components::topological_offset::Parameters']]],
+  ['sphere_5ftrace_5ftarget_5frel_5ftol_8',['sphere_trace_target_rel_tol',['../structwmtk_1_1components_1_1topological__offset_1_1_parameters.html#aec4223bf61e4f0dc7a7218f92811ab6b',1,'wmtk::components::topological_offset::Parameters']]],
+  ['split_5fhigh_5fvalence_5fthreshold_9',['split_high_valence_threshold',['../structwmtk_1_1_optimizer_parameters.html#a99fbd3031d9726ffa1af82b6977acfae',1,'wmtk::OptimizerParameters']]],
+  ['stopping_5fcriterion_10',['stopping_criterion',['../structwmtk_1_1_execute_pass.html#af20086623771fe01053a53b98a086b9b',1,'wmtk::ExecutePass']]],
+  ['stopping_5fcriterion_5fchecking_5ffrequency_11',['stopping_criterion_checking_frequency',['../structwmtk_1_1_execute_pass.html#a821948f937feea93d9de593b525cde58',1,'wmtk::ExecutePass']]]
 ];
--- search/variables_15.js
+++ search/variables_15.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['w_5famips_0',['w_amips',['../structwmtk_1_1_optimizer_parameters.html#a0d5ae5fc83380e897e37a7b7b0673f5b',1,'wmtk::OptimizerParameters']]],
-  ['worst_5fface_5flen_1',['worst_face_len',['../structwmtk_1_1components_1_1topological__offset_1_1_topo_offset_tet_mesh_1_1_energy_criterion.html#a0e062d5beaeabd323884b5f89dbadccd',1,'wmtk::components::topological_offset::TopoOffsetTetMesh::EnergyCriterion']]],
-  ['worst_5fvid_2',['worst_vid',['../structwmtk_1_1components_1_1topological__offset_1_1_topo_offset_tri_mesh_1_1_gradient_split.html#ad7c682b4cf4199a38a50d1c4af01f26f',1,'wmtk::components::topological_offset::TopoOffsetTriMesh::GradientSplit']]]
+  ['worst_5fedge_5flen_1',['worst_edge_len',['../structwmtk_1_1components_1_1topological__offset_1_1_topo_offset_tri_mesh_1_1_energy_criterion.html#a0c6a3bacfe1b2f2f2f89932d582753ab',1,'wmtk::components::topological_offset::TopoOffsetTriMesh::EnergyCriterion']]],
+  ['worst_5fface_5flen_2',['worst_face_len',['../structwmtk_1_1components_1_1topological__offset_1_1_topo_offset_tet_mesh_1_1_energy_criterion.html#a0e062d5beaeabd323884b5f89dbadccd',1,'wmtk::components::topological_offset::TopoOffsetTetMesh::EnergyCriterion']]],
+  ['worst_5fvid_3',['worst_vid',['../structwmtk_1_1components_1_1topological__offset_1_1_topo_offset_tri_mesh_1_1_gradient_split.html#ad7c682b4cf4199a38a50d1c4af01f26f',1,'wmtk::components::topological_offset::TopoOffsetTriMesh::GradientSplit']]]
 ];
