55d258f1be1ed9ae6ae2beb6a75c5e3cb3164fab
Spenbert02 <Spenbert02@gmail.com>
2026-09-15 18:45:44 +0000

    Deploying to gh-pages from @ wildmeshing/wildmeshing-toolkit@e33dde5136dd5aeba4c31d554e6d0f0ce70abd35 🚀

--- search/variables_3.js
+++ search/variables_3.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['deferral_5fwindow_0',['deferral_window',['../structwmtk_1_1_execute_pass.html#ad528fecedb797a7ebe3b1bb7ac882bf4',1,'wmtk::ExecutePass']]],
-  ['deform_5fothers_1',['deform_others',['../structwmtk_1_1components_1_1topological__offset_1_1_parameters.html#a9fe8648b4000c5ec8e9d449bdf38534c',1,'wmtk::components::topological_offset::Parameters']]],
-  ['disabled_2',['disabled',['../classwmtk_1_1_sample_envelope.html#a02b553658ee2d7a77bbdc652321f7191',1,'wmtk::SampleEnvelope']]]
+  ['debug_5fmanual_5fdhat_0',['debug_manual_dhat',['../structwmtk_1_1components_1_1topological__offset_1_1_parameters.html#aeaef365c38235567b1a5b7acf85baad6',1,'wmtk::components::topological_offset::Parameters']]],
+  ['deferral_5fwindow_1',['deferral_window',['../structwmtk_1_1_execute_pass.html#ad528fecedb797a7ebe3b1bb7ac882bf4',1,'wmtk::ExecutePass']]],
+  ['deform_5fothers_2',['deform_others',['../structwmtk_1_1components_1_1topological__offset_1_1_parameters.html#a9fe8648b4000c5ec8e9d449bdf38534c',1,'wmtk::components::topological_offset::Parameters']]],
+  ['disabled_3',['disabled',['../classwmtk_1_1_sample_envelope.html#a02b553658ee2d7a77bbdc652321f7191',1,'wmtk::SampleEnvelope']]]
 ];
